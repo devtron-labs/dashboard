c10a761cbd6012d6183baca0aa1b6956b8a27c36
Rashmi Rai <rashmi.rai@devtron.ai>
2020-11-27 12:10:34 +0530

    added missing files

---
+++ src/components/common/navigation/Navigation.tsx
@@ -0,0 +1,180 @@
+import React, { Component } from 'react';
+import { NavLink, RouteComponentProps } from 'react-router-dom';
+import { URLS } from '../../../config';
+import { ReactComponent as Documentation } from '../../../assets/icons/ic-document.svg'
+import { getLoginInfo } from '../index';
+import { getRandomColor } from '../helpers/Helpers';
+import NavSprite from '../../../assets/icons/navigation-sprite.svg';
+import TextLogo from '../../../assets/icons/ic-nav-devtron.svg';
+import TagManager from 'react-gtm-module';
+import ReactDOM from 'react-dom';
+import { Command } from '../../command/Command';
+import { COMMAND } from '../../command/command.util';
+import './navigation.scss';
+
+const navigationList = [
+	{
+		title: 'Search',
+		type: 'button',
+		iconClass: 'nav-short-search',
+		href: URLS.APP,
+	},
+	{
+		title: 'Applications',
+		type: 'link',
+		iconClass: 'nav-short-apps',
+		href: URLS.APP,
+	},
+	{
+		title: 'Charts',
+		type: 'link',
+		iconClass: 'nav-short-helm',
+		href: URLS.CHARTS,
+	},
+	{
+		title: 'Deployment Groups',
+		type: 'link',
+		iconClass: 'nav-short-bulk-actions',
+		href: URLS.DEPLOYMENT_GROUPS,
+	},
+	{
+		title: 'Security',
+		type: 'link',
+		href: URLS.SECURITY,
+		iconClass: 'nav-security',
+	},
+	{
+		title: 'Global Configurations',
+		type: 'link',
+		href: `${URLS.GLOBAL_CONFIG}`,
+		iconClass: 'nav-short-global'
+	},
+];
+
+export default class Navigation extends Component<RouteComponentProps<{}>, { loginInfo: any; showLogoutCard: boolean; isCommandBarActive: boolean; }> {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			loginInfo: getLoginInfo(),
+			showLogoutCard: false,
+			isCommandBarActive: false,
+		}
+		this.deleteCookie = this.deleteCookie.bind(this);
+		this.toggleLogoutCard = this.toggleLogoutCard.bind(this);
+		this.toggleCommandBar = this.toggleCommandBar.bind(this);
+	}
+
+	componentDidMount() {
+		const tagManagerArgs = {
+			gtmId: 'GTM-59Q5GDK'
+		}
+		TagManager.initialize(tagManagerArgs)
+	}
+
+	toggleLogoutCard() {
+		this.setState({ showLogoutCard: !this.state.showLogoutCard })
+	}
+
+	toggleCommandBar(flag: boolean): void {
+		this.setState({ isCommandBarActive: flag });
+	}
+
+	deleteCookie(): void {
+		document.cookie = `argocd.token=; expires=Thu, 01-Jan-1970 00:00:01 GMT;path=/`;
+		this.props.history.push('/login');
+	}
+
+	renderLogout() {
+		let email: string = this.state.loginInfo ? this.state.loginInfo['email'] || this.state.loginInfo['sub'] : "";
+		return ReactDOM.createPortal(<div className="transparent-div" onClick={this.toggleLogoutCard}>
+			<div className="logout-card">
+				<div className="flexbox flex-justify p-16">
+					<div className="logout-card-user ">
+						<p className="logout-card__name ellipsis-right">{email}</p>
+						<p className="logout-card__email ellipsis-right">{email}</p>
+					</div>
+					<p className="logout-card__initial mb-0" style={{ backgroundColor: getRandomColor(email) }}>{email[0]}</p>
+				</div>
+				<div className="logout-card__logout cursor" onClick={this.deleteCookie}>Logout</div>
+			</div>
+		</div>, document.getElementById('root'))
+	}
+
+	render() {
+		let email: string = this.state.loginInfo ? this.state.loginInfo['email'] || this.state.loginInfo['sub'] : "";
+		return <>
+			<Command location={this.props.location}
+				match={this.props.match}
+				history={this.props.history}
+				isTabMode={true}
+				isCommandBarActive={this.state.isCommandBarActive}
+				toggleCommandBar={this.toggleCommandBar}
+				defaultArguments={[
+					{ value: COMMAND.APPLICATIONS, data: { isValid: true, isEOC: false } },
+				]}
+			/>
+			<nav>
+				<aside className="short-nav main-nav">
+					<NavLink to={URLS.APP} className="flex">
+						<svg className="short-nav-icon logo" viewBox="0 0 40 40">
+							<use href={`${NavSprite}#nav-short-devtron-logo`}></use>
+						</svg>
+					</NavLink>
+					{navigationList.map((item, index) => {
+						if (item.type === "button") return <button type="button"
+							className={this.state.isCommandBarActive ? "transparent active-nav" : "transparent"}
+							onClick={(e) => this.toggleCommandBar(!this.state.isCommandBarActive)}>
+							<div className="svg-container flex">
+								<svg className="short-nav-icon icon-dim-24" viewBox="0 0 24 24">
+									<use href={`${NavSprite}#${item.iconClass}`}></use>
+								</svg>
+							</div>
+						</button>
+						else return <NavLink to={item.href} key={index} className="flex" activeClassName="active-nav">
+							<div className="svg-container flex">
+								<svg className="short-nav-icon" viewBox="0 0 24 24">
+									<use href={`${NavSprite}#${item.iconClass}`}></use>
+								</svg>
+							</div>
+						</NavLink>
+					}
+					)}
+					<div className="flex bottom column bottom-nav">
+						<a rel="noreferrer noopener" className="flex icon-dim-40 mb-16 br-8" href="https://docs.devtron.ai/" target="_blank"><Documentation className="icon-dim-24 fcn-0 cursor" /></a>
+						<div className="logout-card__initial logout-card__initial--nav mb-16" onClick={this.toggleLogoutCard} style={{ backgroundColor: getRandomColor(email) }}>
+							{email[0]}
+						</div>
+						{this.state.showLogoutCard ? this.renderLogout() : null}
+						<div className="hubspot-placeholder"></div>
+					</div>
+				</aside>
+				<aside className="expanded-nav main-nav">
+					<NavLink to={URLS.APP} className="flex left">
+						<img src={TextLogo} alt="devtron" className="text-logo" />
+					</NavLink>
+					{navigationList.map((item, index) => {
+						if (item.type === "button") return <button type="button"
+							className="transparent"
+							onClick={(e) => this.toggleCommandBar(!this.state.isCommandBarActive)}>
+							<div className="title-container flex left">
+								{item.title}
+							</div>
+						</button>
+						else return <NavLink to={item.href} key={index} className="flex left" activeClassName="active-nav">
+							<div className="title-container flex left">
+								{item.title}
+							</div>
+						</NavLink>
+					}
+					)}
+					<div className="flex left bottom column bottom-nav">
+						<a rel="noreferrer noopener" className="flex left icon-dim-40 mb-16 title-container" href="https://docs.devtron.ai/" target="_blank">Documentation</a>
+						<button type="button" className="transparent mb-16 ellipsis-right title-container" onClick={this.toggleLogoutCard}>{email}</button>
+						<div className="hubspot-placeholder"></div>
+					</div>
+				</aside>
+			</nav>
+		</>
+	}
+}
---
+++ src/components/common/navigation/NavigationRoutes.tsx
@@ -0,0 +1,113 @@
+import React, { lazy, Suspense, useEffect, useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { URLS } from '../../../config';
+import { ErrorBoundary, Progressing, getLoginInfo, AppContext } from '../../common';
+import Navigation from './Navigation';
+import { useRouteMatch, useHistory, useLocation } from 'react-router';
+import * as Sentry from '@sentry/browser';
+import ReactGA from 'react-ga';
+import { Security } from '../../security/Security';
+
+const Charts = lazy(() => import('../../charts/Charts'));
+const AppCompose = lazy(() => import('../../appCompose/AppCompose'));
+const AppDetailsPage = lazy(() => import('../../app/details/main'));
+const AppListContainer = lazy(() => import('../../app/list/AppListContainer'));
+const GlobalConfig = lazy(() => import('../../globalConfigurations/GlobalConfiguration'));
+const BulkActions = lazy(() => import('../../deploymentGroups/BulkActions'));
+
+export default function NavigationRoutes() {
+    const history = useHistory()
+    const location = useLocation()
+    const loginInfo = getLoginInfo()
+    const match = useRouteMatch()
+    useEffect(() => {
+        if (!loginInfo) return
+        if (process.env.NODE_ENV !== 'production' || !window._env_ || (window._env_ && !window._env_.SENTRY_ENABLED)) return
+        Sentry.configureScope(function (scope) {
+            scope.setUser({ email: loginInfo['email'] || loginInfo['sub'] });
+        });
+    }, [])
+
+    useEffect(() => {
+        if (process.env.NODE_ENV === 'production' && window._env_ && window._env_.GA_ENABLED) {
+            let email = loginInfo ? loginInfo['email'] || loginInfo['sub'] : "";
+            let path = location.pathname;
+            ReactGA.initialize(window._env_.GA_TRACKING_ID, {
+                debug: false,
+                titleCase: false,
+                gaOptions: {
+                    userId: `${email}`
+                }
+            });
+            ReactGA.pageview(`${path}`);
+            ReactGA.event({
+                category: `Page ${path}`,
+                action: 'First Land'
+            });
+            history.listen((location) => {
+                let path = location.pathname;
+                path = path.replace(new RegExp("[0-9]", "g"), "");
+                path = path.replace(new RegExp("//", "g"), "/");
+                ReactGA.pageview(`${path}`);
+                ReactGA.event({
+                    category: `Page ${path}`,
+                    action: 'First Land'
+                });
+            })
+        }
+    }, [])
+
+    return (
+        <main>
+            <Navigation history={history} match={match} location={location} />
+            <div className="main">
+                <Suspense fallback={<Progressing pageLoader />}>
+                    <ErrorBoundary>
+                        <Switch>
+                            <Route path={URLS.APP} render={() => <AppRouter />} />
+                            <Route path={URLS.CHARTS} render={() => <Charts />} />
+                            <Route path={URLS.GLOBAL_CONFIG} render={props => <GlobalConfig {...props} />} />
+                            <Route path={URLS.DEPLOYMENT_GROUPS} render={props => <BulkActions {...props} />} />
+                            <Route path={URLS.SECURITY} render={(props) => <Security {...props} />} />
+                            <Route>
+                                <RedirectWithSentry />
+                            </Route>
+                        </Switch>
+                    </ErrorBoundary>
+                </Suspense>
+            </div>
+        </main>
+    )
+}
+
+export function AppRouter() {
+    const { path } = useRouteMatch()
+    const [environmentId, setEnvironmentId] = useState(null)
+    return (
+        <ErrorBoundary>
+            <AppContext.Provider value={{ environmentId, setEnvironmentId }}>
+                <Switch>
+                    <Route path={`${path}/:appId(\\d+)/edit`} render={() => <AppCompose />} />
+                    <Route path={`${path}/:appId(\\d+)/material-info`} render={() => <AppListContainer />} />
+                    <Route path={`${path}/:appId(\\d+)`} render={() => <AppDetailsPage />} />
+                    <Route exact path="">
+                        <AppListContainer />
+                    </Route>
+                    <Route>
+                        <RedirectWithSentry />
+                    </Route>
+                </Switch>
+            </AppContext.Provider>
+        </ErrorBoundary>
+    );
+}
+
+export function RedirectWithSentry() {
+    const { push } = useHistory()
+    const { pathname } = useLocation()
+    useEffect(() => {
+        if (pathname && pathname !== '/') Sentry.captureMessage(`redirecting to app-list from ${pathname}`, Sentry.Severity.Warning)
+        push(`${URLS.APP}`)
+    }, [])
+    return null
+}
