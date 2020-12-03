d4bfe77a7e7ede9d837eebdf3da1410da036905d
Rashmi Rai <rashmi.rai@devtron.ai>
2020-12-03 23:27:55 +0530

    refactor app details page

--- src/components/app/details/main.tsx
+++ src/components/app/details/main.tsx
@@ -16,37 +16,59 @@ const CDDetails = lazy(() => import('./cdDetails/CDDetails'));
 const TestRunList = lazy(() => import('./testViewer/TestRunList'));
 
 export default function AppDetailsPage() {
-    const { url, path } = useRouteMatch()
-    const { appId } = useParams()
-    return (
-        <div className="app-details-page">
-            <AppHeader />
-            <ErrorBoundary>
-                <Suspense fallback={<Progressing pageLoader />}>
-                    <Switch>
-                        <Route path={`${path}/${URLS.APP_DETAILS}/:envId(\\d+)?`} render={() => <AppDetails />} />
-                        <Route path={`${path}/${URLS.APP_TRIGGER}`} render={() => <TriggerView />} />
-                        <Route path={`${path}/${URLS.APP_CI_DETAILS}/:pipelineId(\\d+)?`}>
-                            <CIDetails key={appId} />
-                        </Route>
-                        <Route path={`${path}/${URLS.APP_DEPLOYMENT_METRICS}/:envId(\\d+)?`} component={DeploymentMetrics} />
-                        <Route path={`${path}/${URLS.APP_CD_DETAILS}/:envId(\\d+)?/:pipelineId(\\d+)?/:triggerId(\\d+)?`}>
-                            <CDDetails key={appId} />
-                        </Route>
-                        {/* commented for time being */}
-                        {/* <Route path={`${path}/tests/:pipelineId(\\d+)?/:triggerId(\\d+)?`}
-                            render={() => <TestRunList />}
-                        /> */}
-                        <Redirect to={`${path}/${URLS.APP_DETAILS}/:envId(\\d+)?`} />
-                    </Switch>
-                </Suspense>
-            </ErrorBoundary>
-        </div>
-    );
+    const { url, path } = useRouteMatch();
+    const { appId } = useParams<{ appId }>();
+
+    return <div className="app-details-page">
+        <ErrorBoundary>
+            <Suspense fallback={<Progressing pageLoader />}>
+                <Switch>
+                    <Route path={`${path}/${URLS.APP_DETAILS}/:envId(\\d+)?`} render={() => {
+                        return <>
+                            <AppHeader />
+                            <AppDetails />
+                        </>
+                    }} />
+                    <Route path={`${path}/${URLS.APP_TRIGGER}`} render={() => {
+                        return <>
+                            <AppHeader />
+                            <TriggerView />
+                        </>
+                    }} />
+                    <Route path={`${path}/${URLS.APP_CI_DETAILS}/:pipelineId(\\d+)?`}>
+                        <AppHeader />
+                        <CIDetails key={appId} />
+                    </Route>
+                    <Route path={`${path}/${URLS.APP_DEPLOYMENT_METRICS}/:envId(\\d+)?`}
+                        render={(props) => {
+                            return <>
+                                <AppHeader />
+                                <DeploymentMetrics {...props} />
+                            </>
+                        }}
+                    />
+                    <Route path={`${path}/${URLS.APP_CD_DETAILS}/:envId(\\d+)?/:pipelineId(\\d+)?/:triggerId(\\d+)?`}>
+                        <AppHeader />
+                        <CDDetails key={appId} />
+                    </Route>
+                    {/* commented for time being */}
+                    <Route path={`${path}/tests/:pipelineId(\\d+)?/:triggerId(\\d+)?`}
+                        render={() => {
+                            return <>
+                                <AppHeader />
+                                <TestRunList />
+                            </>
+                        }}
+                    />
+                    <Redirect to={`${path}/${URLS.APP_DETAILS}/:envId(\\d+)?`} />
+                </Switch>
+            </Suspense>
+        </ErrorBoundary>
+    </div>
 }
 
 export function AppHeader() {
-    const { appId } = useParams();
+    const { appId } = useParams<{ appId }>();
     const { url, path } = useRouteMatch();
     const { push } = useHistory();
     const [configStatusLoading, setConfigStatusLoading] = useState(false);
@@ -59,7 +81,7 @@ export function AppHeader() {
     const handleAppChange = useCallback(({ label, value }) => {
         const tab = currentPathname.current.replace(url, "").split("/")[1]
         const newUrl = generatePath(path, { appId: value })
-        push(`${newUrl}/${tab}`)
+        push(`${newUrl}`);
         ReactGA.event({
             category: 'App Selector',
             action: 'App Selection Changed',
@@ -67,30 +89,31 @@ export function AppHeader() {
         });
     }, [pathname])
 
-    const { breadcrumbs } = useBreadcrumb(
-        {
-            alias: {
-                ':appId(\\d+)': {
-                    component: (
-                        <AppSelector
-                            primaryKey="appId"
-                            primaryValue="name"
-                            matchedKeys={[]}
-                            api={getAppListMin}
-                            apiPrimaryKey="id"
-                            onChange={handleAppChange}
-                        />
-                    ),
-                    linked: false,
-                },
-                app: {
-                    component: <span className="cn-5 fs-18 lowercase">apps</span>,
-                    linked: true,
-                },
-            },
-        },
-        [appId],
-    );
+    // const { breadcrumbs } = useBreadcrumb(
+    //     {
+    //         alias: {
+    //             ':appId(\\d+)': {
+    //                 component: (
+    //                     <AppSelector
+    //                         primaryKey="appId"
+    //                         primaryValue="name"
+    //                         matchedKeys={[]}
+    //                         api={getAppListMin}
+    //                         apiPrimaryKey="id"
+    //                         onChange={handleAppChange}
+    //                     />
+    //                 ),
+    //                 linked: false,
+    //             },
+    //             app: {
+    //                 component: <span className="cn-5 fs-18 lowercase">apps</span>,
+    //                 linked: true,
+    //             },
+    //         },
+    //     },
+    //     [appId],
+    // );
+
 
     async function handleEditApp(e) {
         setConfigStatusLoading(true);
@@ -102,66 +125,69 @@ export function AppHeader() {
         const newUrl = getNextStageURL(result.result, appId);
         push(newUrl);
     }
-    return (
-        <div className="page-header page-header--tabs">
-            <h1 className="page-header__title flex left fs-18 cn-9">
-                <BreadCrumb breadcrumbs={breadcrumbs} />
-            </h1>
-            <ul role="tablist" className="tab-list">
-                <li className="tab-list__tab ellipsis-right">
-                    <NavLink activeClassName="active" to={`${url}/${URLS.APP_DETAILS}`} className="tab-list__tab-link">
-                        App Details
-                    </NavLink>
-                </li>
-                <li className="tab-list__tab">
-                    <NavLink activeClassName="active" to={`${url}/${URLS.APP_TRIGGER}`} className="tab-list__tab-link">
-                        Trigger
-                    </NavLink>
-                </li>
-                <li className="tab-list__tab">
-                    <NavLink
-                        activeClassName="active"
-                        to={`${url}/${URLS.APP_CI_DETAILS}`}
-                        className="tab-list__tab-link"
-                    >
-                        Build History
-                    </NavLink>
-                </li>
-                <li className="tab-list__tab">
-                    <NavLink
-                        activeClassName="active"
-                        to={`${url}/${URLS.APP_CD_DETAILS}`}
-                        className="tab-list__tab-link"
-                    >
-                        Deployment History
-                    </NavLink>
-                </li>
-                <li className="tab-list__tab">
-                    <NavLink
-                        activeClassName="active"
-                        to={`${url}/${URLS.APP_DEPLOYMENT_METRICS}`}
-                        className="tab-list__tab-link"
-                    >
-                        Deployment Metrics
-                    </NavLink>
-                </li>
-                {/* commented for time being */}
-                {/* <li className="tab-list__tab">
-                    <NavLink activeClassName="active" to={`${url}/tests`} className="tab-list__tab-link">
-                        Tests
-                    </NavLink>
-                </li> */}
-            </ul>
-            <div className="flex page-header__cta-container">
-                <button type="button"
-                    className="cta-with-img flex cancel"
-                    onClick={handleEditApp}
-                    disabled={configStatusLoading}>
-                    {configStatusLoading ? <Progressing /> : (
-                        <><Settings className="icon-dim-20 mr-5" /> Configure</>
-                    )}
-                </button>
-            </div>
+
+    return <div className="page-header page-header--tabs">
+        <h1 className="page-header__title flex left fs-18 cn-9">
+            {/* <BreadCrumb breadcrumbs={breadcrumbs} /> */}
+            <span className="cn-5 fs-18 lowercase" onClick={() => {
+                push('/app');
+            }}>apps</span>
+             <span className="cn-9 fs-18">/</span>
+            <AppSelector
+                primaryKey="appId"
+                primaryValue="name"
+                matchedKeys={[]}
+                api={getAppListMin}
+                apiPrimaryKey="id"
+                onChange={handleAppChange}
+            />
+        </h1>
+        <ul role="tablist" className="tab-list">
+            <li className="tab-list__tab ellipsis-right">
+                <NavLink activeClassName="active"
+                    to={`/app/${appId}/${URLS.APP_DETAILS}`}
+                    className="tab-list__tab-link">App Details
+                </NavLink>
+            </li>
+            <li className="tab-list__tab">
+                <NavLink activeClassName="active"
+                    to={`/app/${appId}/${URLS.APP_TRIGGER}`}
+                    className="tab-list__tab-link">Trigger
+                </NavLink>
+            </li>
+            <li className="tab-list__tab">
+                <NavLink activeClassName="active"
+                    to={`/app/${appId}/${URLS.APP_CI_DETAILS}`}
+                    className="tab-list__tab-link">Build History
+                </NavLink>
+            </li>
+            <li className="tab-list__tab">
+                <NavLink activeClassName="active"
+                    to={`/app/${appId}/${URLS.APP_CD_DETAILS}`}
+                    className="tab-list__tab-link">Deployment History
+                </NavLink>
+            </li>
+            <li className="tab-list__tab">
+                <NavLink activeClassName="active"
+                    to={`/app/${appId}/${URLS.APP_DEPLOYMENT_METRICS}`}
+                    className="tab-list__tab-link">Deployment Metrics
+                </NavLink>
+            </li>
+            {/* commented for time being */}
+            <li className="tab-list__tab">
+                <NavLink activeClassName="active"
+                    to={`/app/${appId}/tests`}
+                    className="tab-list__tab-link"> Tests
+                </NavLink>
+            </li>
+        </ul>
+        <div className="flex page-header__cta-container">
+            <button type="button"
+                className="cta flex cancel"
+                onClick={handleEditApp}
+                disabled={configStatusLoading}>
+                {configStatusLoading ? <Progressing /> : <><Settings className="icon-dim-20 mr-5" /> Configure</>}
+            </button>
         </div>
-    );
+    </div>
 }
