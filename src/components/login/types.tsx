a3549197a24c5a6991102d0b146dc1b68c2bad9e
Rashmi Rai <rashmi.rai@devtron.ai>
2020-12-30 11:58:33 +0530

    types fixed

--- src/components/login/types.tsx
+++ src/components/login/types.tsx
@@ -1,7 +1,6 @@
 import { RouteComponentProps } from 'react-router';
 import { ServerError } from '../../modals/commonTypes';
 
-
 export interface SSOLogin {
     id: number;
     name: string;
@@ -9,33 +8,32 @@ export interface SSOLogin {
     active: boolean;
 }
 
-
-
-export interface SSOLoginState{
-    
+export interface SSOLoginState {
     sso: string;
     configMap: string;
     showToggling: boolean
-    loginList: SSOLogin[];
-    configList: {
-                switch: string;
-                name: string;
-                url:string;
-                config:{
-                    type: string;
-                    id: string;
-                    name: string;
-                    config: {
-                        issuer: string;
-                        clientID: string;
-                        clientSecret: string;
-                        redirectURI: string;
-                        hostedDomains: [];
-                }
-            }
-    };
+    // loginList: SSOLogin[];
+    configList: SSOConfigType[];
 }
 
-export interface SSOLoginProps{
+interface SSOConfigType {
+    switch: string;
+    name: string;
+    url: string;
+    config: {
+        type: string;
+        id: string;
+        name: string;
+        config: {
+            issuer: string;
+            clientID: string;
+            clientSecret: string;
+            redirectURI: string;
+            hostedDomains: any[];
+        }
+    }
+}
+
+export interface SSOLoginProps {
     close: () => void;
 }
