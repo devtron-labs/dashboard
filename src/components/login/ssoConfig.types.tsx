7b35035279301e0ef3e56a65a55fa1eb7a23cf2a
Rashmi Rai <rashmi.rai@devtron.ai>
2021-01-07 12:24:28 +0530

    sso config types

---
+++ src/components/login/ssoConfig.types.tsx
@@ -0,0 +1,31 @@
+export interface SSOLogin {
+    id: number;
+    name: string;
+    label: string;
+    active: boolean;
+}
+
+export interface SSOLoginState {
+    isLoading: boolean;
+    sso: string;
+    lastActiveSSO: string;
+    configMap: string;
+    showToggling: boolean;
+    ssoConfig: SSOConfigType;
+}
+
+interface SSOConfigType {
+    name: string;
+    url: string;
+    config: {
+        type: string;
+        id: string;
+        name: string;
+        config: string; //YAML string
+    },
+    active: boolean;
+}
+
+export interface SSOLoginProps {
+    close: () => void;
+}
