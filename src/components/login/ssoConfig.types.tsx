634985f144a6b0b56ed36707340d6ab9d0c4bf9d
Rashmi Rai <rashmi.rai@devtron.ai>
2021-01-13 16:10:08 +0530

    merged with sso-configuration

---
+++ src/components/login/ssoConfig.types.tsx
@@ -0,0 +1,33 @@
+export interface SSOLogin {
+    id: number;
+    name: string;
+    // label: string;
+    active: boolean;
+}
+
+export interface SSOLoginState {
+    isLoading: boolean;
+    saveLoading: boolean;
+    sso: string; //lowercase
+    lastActiveSSO: undefined | SSOLogin
+    configMap: string;
+    showToggling: boolean;
+    ssoConfig: SSOConfigType;
+}
+
+export interface SSOConfigType {
+    name?: string;
+    id?: number;
+    url?: string;
+    config: {
+        type: string;
+        id: string;
+        name: string;
+        config: string; //YAML string
+    },
+    active?: boolean;
+}
+
+export interface SSOLoginProps {
+    close: () => void;
+}
