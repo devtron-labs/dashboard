4d76c264545a03a4d89e7b3afa738ae14a75e579
Rashmi Rai <rashmi.rai@devtron.ai>
2021-01-14 19:21:52 +0530

    dropdown provider text

--- src/components/artifacts/Artifacts.tsx
+++ src/components/artifacts/Artifacts.tsx
@@ -337,7 +337,7 @@ class Artifacts extends Component<ArtifactsProps, ArtifactsState> {
                                     {props.children}
                                     <NavLink to={`${URLS.GLOBAL_CONFIG_GIT}`} className="react-select__bottom p-10 cb-5 block fw-5 anchor cursor no-decor">
                                         <Add className="icon-dim-20 mr-5 fcb-5 mr-12 vertical-align-bottom" />
-                                        Add Git Material
+                                        Add Git Provider
                                     </NavLink>
                                 </components.MenuList>
                             },
