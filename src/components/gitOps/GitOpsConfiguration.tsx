23cee8b73029b7f9583966fcd15631aa75b18a50
shivani170 <shivani@devtron.ai>
2021-02-04 03:00:04 +0530

    gitops css

--- src/components/gitOps/GitOpsConfiguration.tsx
+++ src/components/gitOps/GitOpsConfiguration.tsx
@@ -1,29 +1,36 @@
 import React, { Component } from "react";
 import { ViewType } from '../../config'
+import '../login/login.css'
+import './gitops.css';
+import { List, CustomInput, ProtectedInput } from '../globalConfigurations/GlobalConfiguration'
 
-export default class GitOpsConfiguration extends Component{
+export default class GitOpsConfiguration extends Component<{}> {
     constructor(props) {
         super(props)
         this.state = {
             view: ViewType.LOADING,
             statusCode: 0,
-            lastActiveSSO: undefined,
-            saveLoading: false,
-            showToggling: false,
-            ssoConfig: undefined,
+            githost:"",
         }
     }
-    render(){
+    render() {
         return <section className="git-page">
-            <h2 className="form__title"></h2>
-            <h5 className="form__subtitle">&nbsp;
+            <h2 className="form__title">GitOps configuration</h2>
+            <h5 className="form__subtitle">Devtron uses Gitops&nbsp;
             </h5>
-            <div className="login__sso-wrapper">
-                <div className="login__sso-flex">
-                    </div>
-                 </div>       
-             </section >
-    
+            <div className="gitops login__sso-wrapper pt-20 pl-20">
+                
+                   <div className="gitops__id">Git host*</div> 
+              {/* <CustomInput autoComplete="off" value={this.state.githost}  onChange={handleOnChange} name="name" error={state.name.error} label="Name*"  />*/}
+
+                   <div className="gitops__id">GitLab organisation ID*</div>
+                   <div>Git access credentials</div>
+                   <div className="gitops__id">GitLab username*</div>
+                   <div className="gitops__id">GitLab token*</div>
+                
+            </div>
+        </section >
+
     }
-        
+
 }
