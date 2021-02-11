f8df4de671bb1ece2bbe769d588b822f39d135bd
shivani170 <shivani@devtron.ai>
2021-02-12 01:43:48 +0530

    auto detection div fixed

--- src/components/hostURL/HostURL.tsx
+++ src/components/hostURL/HostURL.tsx
@@ -14,7 +14,7 @@ export default class HostURL extends Component<HostURLState> {
            value: ""
         })
     }
-    handleOnChange(event){
+    handleChange(event){
      let newURL = event.target.value
     }
 
@@ -26,17 +26,24 @@ render(){
             <div className="white-wrapper">
                 <div className="hosturl__description">
                     <div>
-                        <div className= "hosturl__icon">
+                        <div className= "flex left">
                             <Info className="icon-dim-20 mr-8 " />
                             <div>Host URL is the domain address at which your devtron dashboard can be reached.</div>
                         </div>
                         <div className="ml-30">It is used to reach your devtron dashboard from external sources like configured webhooks, e-mail or slack notifications, grafana dashboard, etc.</div>
                    </div>
                 </div>
-                <div className="hosturl__container m-24">
-                    <div className="form__row  ">
-                            <CustomInput autoComplete="off" value={""} onChange={this.handleOnChange} error={"error"} name="name" label="Name*" />
-                    </div>
+                <div className="pl-20 pr-20">
+                    <div className="flex column left top ">
+                        <div className="gitops__id fw-5 fs-13 mb-8">Name*</div>
+                        <input value={""} type="text" name="name" className="form__input" placeholder={ "Enter Host URL"}
+                        onChange={(event) => this.handleChange(event)} />
+                   </div>
+                   <div className="hosturl__autodetection flex left pt-4">
+                       <Info className="icon-dim-16  mr-8 "/>
+                        Auto-detected from your browser: &nbsp;
+                        <div className="hosturl__url"> http://cd.devtron.ai:32080/</div>
+                   </div>
                 </div>
            </div>
            
