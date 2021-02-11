82a9b71b98cd7d9abcd5a67c345ad222141ceb47
Rashmi Rai <rashmi.rai@devtron.ai>
2021-02-11 16:03:44 +0530

    added url in ssoconfig

--- src/components/login/SSOLogin.tsx
+++ src/components/login/SSOLogin.tsx
@@ -1,6 +1,5 @@
 
 import React, { Component } from 'react'
-import './login.css'
 import { Progressing, ConfirmationDialog, DevtronSwitch as Switch, DevtronSwitchItem as SwitchItem, showError, ErrorScreenManager, } from '../common'
 import Google from '../../assets/icons/ic-google.svg'
 import Check from '../../assets/icons/ic-outline-check.svg'
@@ -11,15 +10,16 @@ import LDAP from '../../assets/icons/ic-ldap.svg'
 import SAML from '../../assets/icons/ic-saml.svg'
 import OIDC from '../../assets/icons/ic-oidc.svg'
 import Openshift from '../../assets/icons/ic-openshift.svg'
-import { SSOLoginProps, SSOLoginState } from './ssoConfig.types'
 import warn from '../../assets/icons/ic-warning.svg';
+import CodeEditor from '../CodeEditor/CodeEditor';
+import { SSOLoginProps, SSOLoginState } from './ssoConfig.types'
 import { toast } from 'react-toastify';
 import { getSSOConfig, createSSOList, updateSSOList, getSSOConfigList } from './login.service'
-import CodeEditor from '../CodeEditor/CodeEditor';
-import yamlJsParser from 'yaml';
-import sample from './sampleConfig.json';
 import { SSOConfigType } from './ssoConfig.types'
 import { ViewType } from '../../config'
+import yamlJsParser from 'yaml';
+import sample from './sampleConfig.json';
+import './login.css'
 
 export const SwitchItemValues = {
     Sample: 'sample',
@@ -48,9 +48,13 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
             configMap: SwitchItemValues.Configuration,
             showToggling: false,
             ssoConfig: undefined,
+            isError: {
+                url: "",
+            }
         }
         this.handleSSOClick = this.handleSSOClick.bind(this);
         this.toggleWarningModal = this.toggleWarningModal.bind(this);
+        this.handleURLChange = this.handleURLChange.bind(this);
         this.handleConfigChange = this.handleConfigChange.bind(this);
         this.saveNewSSO = this.saveNewSSO.bind(this);
     }
@@ -113,6 +117,9 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
             ssoConfig: {
                 ...this.state.ssoConfig,
                 url: event.target.value,
+            },
+            isError: {
+                url: event.target.value.length === 0 ? "This is required field" : ""
             }
         })
     }
@@ -137,8 +144,12 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
     }
 
     saveNewSSO(): void {
+        if (this.state.ssoConfig.url) {
+            toast.error("Some required field are missing");
+            return;
+        }
+        
         this.setState({ saveLoading: true });
-
         let configJSON: any = {};
         try {
             configJSON = yamlJsParser.parse(this.state.ssoConfig.config.config);
@@ -181,8 +192,12 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
     }
 
     onLoginConfigSave(): void {
+        if (!this.state.ssoConfig.url) {
+            toast.error("Some required field are missing");
+            return;
+        }
+        
         this.setState({ saveLoading: true });
-
         let configJSON: any = {};
         try {
             configJSON = yamlJsParser.parse(this.state.ssoConfig.config.config);
@@ -191,21 +206,22 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
             toast.error("Invalid Yaml");
             this.setState({ saveLoading: false });
         }
+        let payload = {
+            id: this.state.ssoConfig.id,
+            name: this.state.sso,
+            url: this.state.ssoConfig.url,
+            config: {
+                id: this.state.ssoConfig.config.id,
+                type: this.state.ssoConfig.config.type,
+                name: this.state.ssoConfig.config.name,
+                config: configJSON
+            },
+            active: true,
+        }
 
         //Create SSO
         if (!this.state.lastActiveSSO) {
-            let payload = {
-                id: this.state.ssoConfig.id,
-                name: this.state.sso,
-                url: this.state.ssoConfig.url,
-                config: {
-                    type: this.state.ssoConfig.config.type,
-                    id: this.state.ssoConfig.config.id,
-                    name: this.state.ssoConfig.config.name,
-                    config: configJSON
-                },
-                active: true,
-            }
+
             createSSOList(payload).then((response) => {
                 let ssoConfig = response.result;
                 this.setState({
@@ -227,18 +243,7 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
         //Update the same SSO
         else if (this.state.lastActiveSSO) {
             if (this.state.sso === this.state.lastActiveSSO?.name) {
-                let payload = {
-                    id: this.state.ssoConfig.id,
-                    name: this.state.sso,
-                    url: this.state.ssoConfig.url,
-                    config: {
-                        id: this.state.ssoConfig.config.id,
-                        type: this.state.ssoConfig.config.type,
-                        name: this.state.ssoConfig.config.name,
-                        config: configJSON
-                    },
-                    active: true,
-                }
+
                 updateSSOList(payload).then((response) => {
                     let ssoConfig = response.result;
                     this.setState({
@@ -292,7 +297,7 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
             <p className="m-0">&nbsp;&nbsp;id: {this.state.ssoConfig.config.id}</p>
             <p className="m-0">&nbsp;&nbsp;config:</p>
         </div> : null;
-        return <div className="sso__code-editor-wrap">
+        return <div className="mt-0 ml-24 mr-24 mb-24">
             <div className="code-editor-container">
                 <CodeEditor value={codeEditorBody}
                     height={300}
@@ -324,8 +329,7 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
         }
         return <section className="git-page">
             <h2 className="form__title">SSO Login Services</h2>
-            <h5 className="form__subtitle">Configure and manage login service for your organization. &nbsp;
-            </h5>
+            <h5 className="form__subtitle">Configure and manage login service for your organization. &nbsp;</h5>
             <div className="login__sso-wrapper">
                 <div className="login__sso-flex">
                     <div>
@@ -420,17 +424,18 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
                         <a rel="noreferrer noopener" href={`${ssoMap[this.state.sso]}`} target="_blank" className="login__auth-link"> Authentication through {this.state.sso}</a></div>
                     </div>
                 </div>
-
-                {this.renderSSOCodeEditor()}
-                <label className="form__row mr-24 ml-24">
-                    <span className="form__label">URL</span>
+                <label className="form__row mr-24 ml-24 mb-24">
+                    <span className="form__label">URL*</span>
                     <input type="text" className="form__input" value={this.state.ssoConfig.url} onChange={this.handleURLChange} />
+                    {this.state.isError.url && <div className="form__error">
+                        {this.state.isError.url}
+                    </div>}
                 </label>
-                <div className="form__buttons mr-24">
+                {this.renderSSOCodeEditor()}
+                <div className="form__buttons mt-32 mr-24">
                     <button onClick={(e) => { e.preventDefault(); this.onLoginConfigSave() }} tabIndex={5} type="submit" disabled={this.state.saveLoading} className={`cta`}>{this.state.saveLoading ? <Progressing /> : this.state.ssoConfig.id ? 'Update' : 'Save'}</button>
                 </div>
             </div>
-
             {this.state.showToggling ? <ConfirmationDialog>
                 <ConfirmationDialog.Icon src={warn} />
                 <div className="modal__title sso__warn-title">Use '{this.state.sso}' instead of '{this.state.lastActiveSSO?.name}' for login?</div>
