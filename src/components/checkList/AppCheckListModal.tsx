86d2c32bbeec7bf588ab0102da117af3c9bd48b7
shivani170 <shivani@devtron.ai>
2021-03-01 17:06:49 +0530

    deleted unnecessary import

--- src/components/checkList/AppCheckListModal.tsx
+++ src/components/checkList/AppCheckListModal.tsx
@@ -1,19 +1,8 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
 import Checklist from '../../assets/img/ic-empty-checklist.png';
-import { ReactComponent as Check } from '../../assets/icons/ic-outline-check.svg';
-import { URLS } from '../../config';
-import img from '../../assets/img/ic-checklist-chart@2x.png';
 import './checklist.css';
 import { ViewType } from '../../config'
-import Complete from '../../assets/img/ic-empty-done@2x.png';
-import Sample from '../../assets/img/ic-checklist-sample-app@2x.png';
-import Deploy from '../../assets/img/ic-checklist-app@2x.png';
-import { ReactComponent as Dropdown } from '../../assets/icons/appstatus/ic-dropdown.svg';
 import { AppCheckListModalState, AppCheckListModalProps } from './checklist.type';
-import { getAppCheckList } from './checklist.service';
-import { Progressing, showError } from '../common';
-import  Uncheck from '../../assets/img/ic-success@2x.png';
 import { AppCheckList } from './AppCheckList';
 import { ChartCheckList }from './ChartCheckList'
 
