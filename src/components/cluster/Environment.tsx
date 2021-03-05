a05a719cec280798b4a594885577f4d16c1f1afa
shivani170 <shivani@devtron.ai>
2021-03-05 13:31:20 +0530

    cluster added

---
+++ src/components/cluster/Environment.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Environment extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
