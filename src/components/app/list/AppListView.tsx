d124fc3d313dc10ff0512ff3dea6c9654c3f8e43
shivani170 <shivani@devtron.ai>
2021-04-13 14:11:08 +0530

    card position fixed to left

--- src/components/app/list/AppListView.tsx
+++ src/components/app/list/AppListView.tsx
@@ -15,6 +15,7 @@ import { ReactComponent as Clear } from '../../../assets/icons/ic-error.svg';
 import { ReactComponent as Dropdown } from '../../../assets/icons/appstatus/ic-dropdown.svg'
 import { TriggerInfoModal } from './TriggerInfo';
 import { AppCheckListModal } from '../../checkList/AppCheckModal';
+import { ReactComponent as Check } from '../../../assets/icons/ic-check.svg';
 
 const APP_LIST_PARAM = {
     createApp: 'create-app',
@@ -63,10 +64,35 @@ export class AppListView extends Component<AppListViewProps>{
     renderPageHeader() {
         return <div className="app-header">
             <div className="app-header__title">
-                <h1 className="app-header__text">Applications({this.props.size})
-                <Dropdown onClick={this.props.toggleHeaderName} className="icon-dim-24 rotate" style={{ ['--rotateBy' as any]:  this.props.collapsed ? '180deg' : '0deg'  }} />
+                <h1 className="app-header__text flex">Applications({this.props.size})
+                <Dropdown onClick={this.props.toggleHeaderName} className="icon-dim-24 rotate ml-4" style={{ ['--rotateBy' as any]: this.props.collapsed ? '180deg' : '0deg' }} />
                 </h1>
-                {this.props.collapsed? "hi" : ""}
+                {this.props.collapsed ? <>
+                    <div className="app-list-card bcn-0 br-4 en-1 bw-1 pt-8 pr-8 pb-8 pl-8">
+                        <div className="flex left pt-8 pr-8 pb-8 pl-8">
+                        <Check className="scb-5 mr-8 icon-dim-16" />
+                            <div>
+                                <div className="cn-9 fs-13">Devtron Apps & Charts</div>
+                                <div className="cn-5">Apps & charts deployed using Devtron</div>
+                            </div>
+                        </div>
+                        <div className="flex left pt-8 pr-8 pb-8 pl-8">
+                        <Check className="scb-5 mr-8 icon-dim-16" />
+                            <div>
+                                <div className="cn-9 fs-13">External Apps</div>
+                                <div className="cn-5">Helm charts, Argocd objects</div>
+                            </div>
+                        </div>
+                        <div className="flex left pt-8 pr-8 pb-8 pl-8">
+                        <Check className="scb-5 mr-8 icon-dim-16" />
+                            <div>
+                                <div className="cn-9 fs-13">K8s Objects</div>
+                                <div className="cn-5">All objects for which you have direct access</div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </> : ""}
                 {this.props.view != AppListViewType.EMPTY ? <button type="button" className="cta"
                     onClick={this.openCreateModal}>
                     <span className="round-button__icon"><i className="fa fa-plus" aria-hidden="true"></i></span>
