fc3ec745027b6c3d237148fd1815ef318d1596e5
shivani170 <shivani@devtron.ai>
2021-05-27 19:03:31 +0530

    [FIX] create app removed template input error

--- src/components/app/create/CreateApp.tsx
+++ src/components/app/create/CreateApp.tsx
@@ -160,7 +160,7 @@ export class AddNewApp extends Component<AddNewAppProps, AddNewAppState> {
                             : null}
                     </span>
                     <span className="form__text-field-info">
-                        <Info className="form__icon form__icon--info" style={{height: "none"}} />
+                        <Info className="form__icon form__icon--info" style={{ height: "none" }} />
                         Apps are NOT env specific and can be used to deploy to multiple environments.
                     </span>
                 </label>
@@ -191,11 +191,6 @@ export class AddNewApp extends Component<AddNewAppProps, AddNewAppState> {
                             </Select.Option>
                         })}
                     </Select>
-                    <span className="form__error">
-                        {showError && !this.state.isValid.projectId
-                            ? <><Error className="form__icon form__icon--error" />{errorObject[1].message}</>
-                            : null}
-                    </span>
                 </div>
                 {this.state.form.cloneId > 0 && <div className="info__container info__container--create-app">
                     <Info />
