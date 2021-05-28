a15e7cc99afb33fc700839e2894d26b391620607
shivani170 <shivani@devtron.ai>
2021-05-28 11:20:38 +0530

    [INTERNAL] check after use effect

--- src/components/app/details/appDetails/AppMetrics.tsx
+++ src/components/app/details/appDetails/AppMetrics.tsx
@@ -138,13 +138,6 @@ export const AppMetrics: React.FC<{ appName: string, environment, podMap: Map<st
     function getNewGraphs(newTab): void {
         if (!datasource.isHealthy) return;
 
-        if (!isK8sVersionValid(k8sVersion)) {
-            k8sVersion = DEFAULTK8SVERSION;
-            toast.warn(<div className="toast">
-                <div className="toast__title">Error Parsing K8sVersion</div>
-                <div className="toast__subtitle">Showing Graphs for {DEFAULTK8SVERSION} and above</div>
-            </div>)
-        }
         let appInfo = {
             appId: appId,
             envId: envId,
@@ -178,15 +171,23 @@ export const AppMetrics: React.FC<{ appName: string, environment, podMap: Map<st
         getNewGraphs(tab);
     }, [calendarValue])
 
+    if (!isK8sVersionValid(k8sVersion)) {
+        k8sVersion = DEFAULTK8SVERSION;
+        toast.warn(<div className="toast">
+            <div className="toast__title">Error Parsing K8sVersion</div>
+            <div className="toast__subtitle">Showing Graphs for {DEFAULTK8SVERSION} and above</div>
+        </div>)
+    }
+
     //@ts-ignore
     if (!datasource.isConfigured || !datasource.isHealthy || !hostURLConfig || hostURLConfig.value !== window.location.origin) {
         return <>
-        {console.log()}
-        <AppMetricsEmptyState isLoading={datasource.isLoading}
-            isConfigured={datasource.isConfigured}
-            isHealthy={datasource.isHealthy}
-            hostURLConfig={hostURLConfig} />
-            </>
+            {console.log()}
+            <AppMetricsEmptyState isLoading={datasource.isLoading}
+                isConfigured={datasource.isConfigured}
+                isHealthy={datasource.isHealthy}
+                hostURLConfig={hostURLConfig} />
+        </>
     }
     else {
         return <section className={`app-summary bcn-0 pl-24 pr-24 pb-20 w-100`}
