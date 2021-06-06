cdc0c14f56de2f449e621be2a18a19c13f50e312
shivani170 <shivani@devtron.ai>
2021-06-06 15:53:48 +0530

    handle all filter with one function

--- src/components/charts/ChartHeaderFilters.tsx
+++ src/components/charts/ChartHeaderFilters.tsx
@@ -17,62 +17,65 @@ function ChartHeaderFilter({ selectedChartRepo, handleCloseFilter, includeDeprec
     const history = useHistory()
     const location = useLocation()
     const { url } = match
-    const searchParams = new URLSearchParams(location.search);
-    const app = searchParams.get(QueryParams.AppStoreName);
-    const deprecate = searchParams.get(QueryParams.IncludeDeprecated);
-    const chartRepoId = searchParams.get(QueryParams.ChartRepoId);
 
     const MenuList = (props) => {
         return (
             <components.MenuList {...props}>
                 {props.children}
                 <div className="chart-list-apply-filter flex bcn-0 pt-10 pb-10">
-                    <button type="button" className="cta flex cta--chart-store" disabled={false} onClick={(selected: any) => { handleChartRepoChange(selectedChartRepo) }}>Apply Filter</button>
+                    <button type="button" className="cta flex cta--chart-store" disabled={false} onClick={(selected: any) => { handleFilterChanges(selectedChartRepo, "chart-repo") }}>Apply Filter</button>
                 </div>
             </components.MenuList>
         );
     };
 
-    function handleChartRepoChange(selected): void {
-        let chartRepoId = selected?.map((e) => { return e.value }).join(",");
-        let qs = `${QueryParams.ChartRepoId}=${chartRepoId}`;
-        if (app) qs = `${qs}&${QueryParams.AppStoreName}=${app}`;
-        if (deprecate) qs = `${qs}&${QueryParams.IncludeDeprecated}=${deprecate}`;
-        history.push(`${url}?${qs}`);
-    }
+    function handleFilterChanges(selected, key): void {
+        const searchParams = new URLSearchParams(location.search);
+        const app = searchParams.get(QueryParams.AppStoreName);
+        const deprecate = searchParams.get(QueryParams.IncludeDeprecated);
+        const chartRepoId = searchParams.get(QueryParams.ChartRepoId);
 
-    function handleDeprecateChange(deprecated): void {
-        let qs = `${QueryParams.IncludeDeprecated}=${deprecated}`;
-        if (app) qs = `${qs}&${QueryParams.AppStoreName}=${app}`;
-        if (chartRepoId) qs = `${qs}&${QueryParams.ChartRepoId}=${chartRepoId}`
-        history.push(`${url}?${qs}`);
-    }
+        if (key == "chart-repo") {
+            let chartRepoId = selected?.map((e) => { return e.value }).join(",");
+            let qs = `${QueryParams.ChartRepoId}=${chartRepoId}`;
+            if (app) qs = `${qs}&${QueryParams.AppStoreName}=${app}`;
+            if (deprecate) qs = `${qs}&${QueryParams.IncludeDeprecated}=${deprecate}`;
+            history.push(`${url}?${qs}`)
+        };
 
-    function handleAppStoreChange(event): void {
-        event.preventDefault();
-        let qs = `${QueryParams.AppStoreName}=${appStoreName}`;
-        if (deprecate) qs = `${qs}&${QueryParams.IncludeDeprecated}=${deprecate}`;
-        if (chartRepoId) qs = `${qs}&${QueryParams.ChartRepoId}=${chartRepoId}`;
-        history.push(`${url}?${qs}`);
-    }
+        if (key == "deprecated") {
+            let qs = `${QueryParams.IncludeDeprecated}=${selected}`;
+            if (app) qs = `${qs}&${QueryParams.AppStoreName}=${app}`;
+            if (chartRepoId) qs = `${qs}&${QueryParams.ChartRepoId}=${chartRepoId}`
+            history.push(`${url}?${qs}`);
+        }
 
-    function clearSearch(event): void {
-        let qs: string = "";
-        if (deprecate) qs = `${qs}&${QueryParams.IncludeDeprecated}=${deprecate}`;
-        if (chartRepoId) qs = `${qs}&${QueryParams.ChartRepoId}=${chartRepoId}`;
-        history.push(`${url}?${qs}`);
+        if (key == "search") {
+            selected.preventDefault();
+            let qs = `${QueryParams.AppStoreName}=${appStoreName}`;
+            if (deprecate) qs = `${qs}&${QueryParams.IncludeDeprecated}=${deprecate}`;
+            if (chartRepoId) qs = `${qs}&${QueryParams.ChartRepoId}=${chartRepoId}`;
+            history.push(`${url}?${qs}`);
+        }
+
+        if (key == "clear") {
+            let qs: string = "";
+            if (deprecate) qs = `${qs}&${QueryParams.IncludeDeprecated}=${deprecate}`;
+            if (chartRepoId) qs = `${qs}&${QueryParams.ChartRepoId}=${chartRepoId}`;
+            history.push(`${url}?${qs}`);
+        }
     }
 
     return (<div className="flexbox flex-justify mt-16 ml-20 mr-20">
         <form
-            onSubmit={handleAppStoreChange}
+            onSubmit={(e) => handleFilterChanges(e, "search")}
             className="search position-rel" >
             <Search className="search__icon icon-dim-18" />
             <input type="text" placeholder="Search charts"
                 value={appStoreName}
                 className="search__input bcn-0"
                 onChange={(event) => { setAppStoreName(event.target.value) }} />
-            {searchApplied ? <button className="search__clear-button" type="button" onClick={clearSearch}>
+            {searchApplied ? <button className="search__clear-button" type="button" onClick={(e) => handleFilterChanges(e, "clear")}>
                 <Clear className="icon-dim-18 icon-n4 vertical-align-middle" />
             </button> : null}
         </form>
@@ -102,7 +105,7 @@ function ChartHeaderFilter({ selectedChartRepo, handleCloseFilter, includeDeprec
             <Checkbox rootClassName="ml-16 mb-0 fs-14 cursor bcn-0 pt-8 pb-8 pr-12 date-align-left--deprecate"
                 isChecked={includeDeprecated === 1}
                 value={"CHECKED"}
-                onChange={(event) => { let value = (includeDeprecated + 1) % 2; handleDeprecateChange(value) }}>
+                onChange={(event) => { let value = (includeDeprecated + 1) % 2; handleFilterChanges(value, "deprecated") }}>
                 <div className="ml-5"> Show deprecated</div>
             </Checkbox>
         </div>
