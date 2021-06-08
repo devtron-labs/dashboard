22c52b29b4bae0a18aadb1bd6a23d0e6d938d4a9
shivani170 <shivani@devtron.ai>
2021-06-08 14:22:26 +0530

    [INTERNAL] removed commented out code and spacing

--- src/components/login/SSOLogin.tsx
+++ src/components/login/SSOLogin.tsx
@@ -315,8 +315,6 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
         this.setState({ configMap: value })
     }
 
-
-
     renderSSOCodeEditor() {
         let ssoConfig = this.state.ssoConfig.config.config || yamlJsParser.stringify({}, { indent: 2 });
         let codeEditorBody = this.state.configMap === SwitchItemValues.Configuration ? ssoConfig : yamlJsParser.stringify(sample[this.state.sso], { indent: 2 });
@@ -358,8 +356,6 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
         })
     }
 
-
-
     getSSOLoginTabsArr() {
         let SSOLoginTabsArr = [
             { provider: SSOProvider.google, SSOName: "Google" }, 
@@ -371,7 +367,6 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
         ]
         return SSOLoginTabsArr;
     }
-
 
     render() {
         if (this.state.view === ViewType.LOADING) {
@@ -390,12 +385,6 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
                 <div className="login__sso-flex pl-24">
                     {this.getSSOLoginTabsArr().map((item) => {
                         return <SSOLoginTab value={item.provider} SSOName={item.SSOName} checked={this.state.sso === item.provider} handleSSOClick={this.handleSSOClick} lastActiveSSO={this.state.lastActiveSSO} /> })}
-                    {/* <SSOLoginTab value={SSOProvider.google} SSOName="Google" checked={this.state.sso === SSOProvider.google} handleSSOClick={this.handleSSOClick} lastActiveSSO={this.state.lastActiveSSO} />,
-                    <SSOLoginTab value={SSOProvider.github} SSOName="GitHub" checked={this.state.sso === SSOProvider.github} handleSSOClick={this.handleSSOClick} lastActiveSSO={this.state.lastActiveSSO} />,
-                    <SSOLoginTab value={SSOProvider.microsoft} SSOName="Microsoft" checked={this.state.sso === SSOProvider.microsoft} handleSSOClick={this.handleSSOClick} lastActiveSSO={this.state.lastActiveSSO} />,
-                    <SSOLoginTab value={SSOProvider.ldap} SSOName="LDAP" checked={this.state.sso === SSOProvider.ldap} handleSSOClick={this.handleSSOClick} lastActiveSSO={this.state.lastActiveSSO} />,
-                    <SSOLoginTab value={SSOProvider.oidc} SSOName="OIDC" checked={this.state.sso === SSOProvider.oidc} handleSSOClick={this.handleSSOClick} lastActiveSSO={this.state.lastActiveSSO} />,
-                    <SSOLoginTab value={SSOProvider.openshift} SSOName="OpenShift" checked={this.state.sso === SSOProvider.openshift} handleSSOClick={this.handleSSOClick} lastActiveSSO={this.state.lastActiveSSO} /> */}
                 </div>
                 <div className="sso__description p-16 br-4 fs-14 eb-2 bw-1 mt-20 mb-20 ml-24 mr-24">
                     <div className="flexbox">
