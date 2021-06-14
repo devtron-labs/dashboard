2e70520fea6077dcff1f96f66294966c7df149cb
shivani170 <shivani@devtron.ai>
2021-06-14 11:43:51 +0530

    [CREATE APP] semi colon instead colon

--- src/components/app/create/validationRules.ts
+++ src/components/app/create/validationRules.ts
@@ -12,7 +12,7 @@ export class ValidationRules {
         if (value.length === 0) return { isValid: false, message: 'This is required field' };
         if (value.length < 3) return { isValid: false, message: 'Atleast 3 characters required' };
         if (value.length > 30) return { isValid: false, message: 'max 30 characters allowed' };
-        else if (!test) return { isValid: false, message: 'Min 3 characters; Start with lowercase; Use (a-z), (0-9), (-), (.): Do not use \'spaces\''};
+        else if (!test) return { isValid: false, message: 'Min 3 characters; Start with lowercase; Use (a-z), (0-9), (-), (.); Do not use \'spaces\''};
         else return { isValid: true, message: '' };
     }
 
