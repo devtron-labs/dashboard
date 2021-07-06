352fe933f86b44cecbffca9c18f77b84dc723604
shivani170 <shivani@devtron.ai>
2021-07-06 07:46:49 +0530

    [BULK EDIT] mock data removed

--- src/components/bulkEdits/bulkedits.service.tsx
+++ src/components/bulkEdits/bulkedits.service.tsx
@@ -1,9 +1,5 @@
 import { get, post } from '../../services/api';
 
-// export function getReadme() {
-//     return get(`app-store/application/readme/240`)
-// }
-
 export function updateBulkList(request): Promise<any> {
     const URL = `batch/v1beta1/application `;
     return post(URL, request);
@@ -18,49 +14,3 @@ export function getSeeExample() {
     const URL = 'batch/v1beta1/application/see-example'
     return get(URL)
 }
-
-// export function getOutputListMin(): Promise<{
-//     appNameIncludes: string;
-//     appNameExcludes: string;
-//     envId: number;
-//     isGlobal: boolean;
-//     patchJson: string;
-// }[]> {
-//     return new Promise((resolve, reject) => {
-//         resolve([{
-//             "appNameIncludes": "demo",
-//             "appNameExcludes": "docker-config",
-//             "envId": 5,
-//             "isGlobal": true,
-//             "patchJson": "string"
-//         },
-//         {
-//             "appNameIncludes": "demo-env",
-//             "appNameExcludes": "docker-config",
-//             "envId": 5,
-//             "isGlobal": true,
-//             "patchJson": "string"
-//         },
-//         {
-//             "appNameIncludes": "demo_live",
-//             "appNameExcludes": "docker-config",
-//             "envId": 5,
-//             "isGlobal": true,
-//             "patchJson": "string"
-//         },
-//         {
-//             "appNameIncludes": "demo-env",
-//             "appNameExcludes": "docker-config",
-//             "envId": 5,
-//             "isGlobal": true,
-//             "patchJson": "string"
-//         },
-//     ])
-//     })
-// }
-
-// export function getImpactedObjectsListMin(): Promise<{
-
-// }>{
-
-// }
