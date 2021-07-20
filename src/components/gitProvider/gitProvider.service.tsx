32ab12b6d6cf66342df4d41a505312f18c6ed004
shivani170 <shivani@devtron.ai>
2021-07-20 08:15:57 +0530

    [WEBHOOK] git provider

---
+++ src/components/gitProvider/gitProvider.service.tsx
@@ -0,0 +1,29 @@
+import { Routes } from '../../config';
+import { post, put, get } from '../../services/api';
+
+export const getGitProviderConfig = (id: number): Promise<any> => {
+    const URL = `${Routes.GIT_PROVIDER}/${id}`;
+    return get(URL);
+}
+
+export const updateGitProviderConfig = (request: any, id: number) => {
+    const URL = `${Routes.GIT_PROVIDER}`;
+    return put(URL, request);
+}
+
+export const saveGitProviderConfig = (request: any, id: any) => {
+    const URL = `${Routes.GIT_PROVIDER}`;
+    return post(URL, request);
+}
+
+
+export function getGitHost(id: number | string): Promise<any> {
+    const URL = `${Routes.GIT_HOST}/${id}`;
+    return get(URL);
+    
+}
+
+export function saveGitHost(payload): Promise<any> {
+    const URL = `${Routes.GIT_HOST}`;
+    return post(URL, payload);
+}
