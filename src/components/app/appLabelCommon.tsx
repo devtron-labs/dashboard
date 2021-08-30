411c85ef86f09745503347013ef3bdbced7d7760
shivani170 <shivani@devtron.ai>
2021-08-30 21:42:19 +0530

    app-tags- validated

--- src/components/app/appLabelCommon.tsx
+++ src/components/app/appLabelCommon.tsx
@@ -25,7 +25,6 @@ export function handleKeyDown(labelTags, setAppTagLabel, event) {
         case 'Enter':
         case 'Tab':
         case ',':
-        case ' ': // space
             if (labelTags.inputTagValue) {
                 setAppTagLabel()
             }
