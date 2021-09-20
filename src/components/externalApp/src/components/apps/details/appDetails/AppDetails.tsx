ddcea5522f2b896c906d7588b05d3585f3a38722
shivani170 <shivani@devtron.ai>
2021-09-20 14:38:27 +0530

    EXTERNAL APP: architecture

---
+++ src/components/externalApp/src/components/apps/details/appDetails/AppDetails.tsx
@@ -0,0 +1,9 @@
+import React, { lazy, Suspense, useCallback, useRef, useEffect, useState } from 'react';
+
+export default function AppDetails() {
+    return (
+        <div>
+            hi
+        </div>
+    )
+}
---
+++ src/components/externalApp/src/components/apps/details/appDetails/appDetails.type.ts
@@ -0,0 +1,4 @@
+export enum AppMetricsTab {
+    Aggregate = 'aggregate',
+    Pod = 'pod'
+}
