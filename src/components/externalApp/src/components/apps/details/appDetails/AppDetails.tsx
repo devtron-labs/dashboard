deaef795adf90098c643be8979a6212cb3c7eb5d
shivani170 <shivani@devtron.ai>
2021-09-20 15:29:54 +0530

    EXTERNAL APP: added multiselect

--- src/components/externalApp/src/components/apps/details/appDetails/AppDetails.tsx
+++ src/components/externalApp/src/components/apps/details/appDetails/AppDetails.tsx
@@ -1,9 +1,39 @@
 import React, { lazy, Suspense, useCallback, useRef, useEffect, useState } from 'react';
+import Select, { components } from 'react-select';
+// import { multiSelectStyles } from '../../../common/MultiSelect/MutiSelectCustomisation';
 
 export default function AppDetails() {
     return (
         <div>
-            hi
+             <div style={{ width: 'clamp( 100px, 30%, 200px )', height: '100%', position: 'relative' }}>
+                <svg
+                    viewBox="0 0 200 40"
+                    preserveAspectRatio="none"
+                    style={{ width: '100%', height: '100%', display: 'flex' }}
+                >
+                    <path d="M0 20 L200 20 Z" strokeWidth="1" stroke="#0066cc" />
+                    <path d="M0 10 L0, 30" strokeWidth="2" stroke="#0066cc" />
+                </svg>
+                <div
+                    className="bcb-5 br-10 cn-0 pl-8 pr-8"
+                    style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
+                >
+                    ENV
+                </div>
+            </div>
+            <div style={{ width: '200px' }}>
+                <Select 
+                    placeholder='Select Environment'
+                    closeMenuOnSelect
+                    components={{ IndicatorSeparator: null}}
+                    styles={{
+                        // ...multiSelectStyles,
+                        control: (base, state) => ({ ...base, border: '1px solid #0066cc', backgroundColor: 'transparent' }),
+                        singleValue: (base, state) => ({ ...base, fontWeight: 600, color: '#06c' })
+                    }}
+                    isSearchable={false}
+                />
+            </div>
         </div>
     )
 }
