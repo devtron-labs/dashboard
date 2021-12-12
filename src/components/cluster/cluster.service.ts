541ed3fc58cf7f7508a5b2e1bc68d4f0a7fea925
nishant <nishant@devtron.ai>
2021-12-12 16:01:58 +0530

    external app cluster component url correction

--- src/components/cluster/cluster.service.ts
+++ src/components/cluster/cluster.service.ts
@@ -36,7 +36,7 @@ export function updateCluster(request) {
 }
 
 export function retryClusterInstall(id: number, payload): Promise<ResponseType> {
-    const URL = `${Routes.CLUSTER}/component/install/${id}`;
+    const URL = `app-store/cluster-component/install/${id}`;
     return post(URL, payload);
 }
 
