ad34fd34758c3d7d6526fa995deeee30f0e2fc51
Milind <milind@devtron.ai>
2022-02-18 20:22:30 +0530

    Fomatted file

--- src/components/v2/appDetails/k8Resource/nodeDetail/NodeDetailTabs/CopyToast.tsx
+++ src/components/v2/appDetails/k8Resource/nodeDetail/NodeDetailTabs/CopyToast.tsx
@@ -6,19 +6,22 @@ import '../../../../../LogViewer/LogViewer.scss';
 import  './nodeDetailTab.scss';
 
 
-function CopyToast({ showCopyToast}) {
-    return <AutoSizer>{({ height, width }) =>
-    <div id="xterm-logs" style={{ height, width }}>
-    <span
-        className={`br-8 bcn-0 cn-9 clipboard-toast ${showCopyToast ? 'clipboard-toast--show' : ''}`}
-        style={{ zIndex: 9 }}
-    >
-        <CheckIcon className="icon-dim-24 scn-9" />
-        <div className="">Copied!</div>
-    </span>
-</div>
-     }
-     </AutoSizer>;
+function CopyToast({ showCopyToast }) {
+    return (
+        <AutoSizer>
+            {({ height, width }) => (
+                <div id="xterm-logs" style={{ height, width }}>
+                    <span
+                        className={`br-8 bcn-0 cn-9 clipboard-toast ${showCopyToast ? 'clipboard-toast--show' : ''}`}
+                        style={{ zIndex: 9 }}
+                    >
+                        <CheckIcon className="icon-dim-24 scn-9" />
+                        <div className="">Copied!</div>
+                    </span>
+                </div>
+            )}
+        </AutoSizer>
+    );
 }
 
 export default CopyToast;
