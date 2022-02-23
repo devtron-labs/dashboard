994098b9f7a14c7dda641244004dc551010e51e3
shivani170 <shivani@devtron.ai>
2022-02-23 11:36:05 +0530

    SEQUENTIAL CD: status of pre and post changed

--- src/components/app/details/triggerView/cdMaterial.tsx
+++ src/components/app/details/triggerView/cdMaterial.tsx
@@ -80,13 +80,13 @@ export class CDMaterial extends Component<CDMaterialProps> {
                 <div className="bcv-1 pt-6 pb-6 pl-16 pr-16 br-4">
                     <span className="cn-9 fw-6">Deployed on </span>{' '}
                     <span className="cv-5 fw-6">
-                        {this.props.envName} 
+                        {this.props.parentEnvironmentName} 
                         {this.props.parentEnvironmentName ? (
                             <>
                                 <span className="cn-9 fw-4" style={{ fontStyle: 'italic' }}>
                                 {' '} and  {' '}
                                 </span>
-                                {this.props.parentEnvironmentName}
+                                {this.props.envName}
                             </>
                         ) : (
                             ''
@@ -125,7 +125,7 @@ export class CDMaterial extends Component<CDMaterialProps> {
           onClick={(event) => { event.stopPropagation(); if (!mat.vulnerable) this.props.selectImage(index, this.props.materialType) }}>
           <div>  
            <div className="commit-hash commit-hash--docker"><img src={docker} alt="" className="commit-hash__icon" />{mat.image}</div>
-           {this.props.stageType !== 'CD' && mat.latest  ? <span className="last-deployed-status">Running</span> : null}
+           {this.props.stageType !== 'CD' && mat.latest  ? <span className="last-deployed-status">Last Run</span> : null}
          </div>  
           {this.props.materialType === "none" ? null : <div className="material-history__info">
             <span className="trigger-modal__small-text">Deployed at:</span> <span>{mat.deployedTime}</span>
