5e75dd42adc22018ec683678cce5963c9c86daf3
shivani170 <shivani@devtron.ai>
2022-02-24 23:54:50 +0530

    DEPLOYMENT HISTORY: added selector

--- src/components/app/details/cdDetails/CompareWithBaseConfig.tsx
+++ src/components/app/details/cdDetails/CompareWithBaseConfig.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import ReactSelect from 'react-select';
+import { menuList } from '../../../charts/charts.util';
+import { DropdownIndicator } from '../appDetails/utils';
+import { styles } from '../metrics/deploymentMetrics.util';
 
 function CompareWithBaseConfig() {
-  return <div className='border-bottom'>
-      hi
+  return <div className='border-bottom ml-20 mr-20 mt-12 mb-12'>
+    <div>
+     <div>Compare with</div> 
+							<ReactSelect
+              className='w-200'
+								components={{
+									IndicatorSeparator: null,
+									DropdownIndicator
+								}}
+                value={{label:'tetsinh', value: 'testing'}}
+								placeholder="Select Project"
+								styles={{
+									...styles,
+									...menuList,
+								}}
+							/>
+    </div>
+     
   </div>;
 }
 
