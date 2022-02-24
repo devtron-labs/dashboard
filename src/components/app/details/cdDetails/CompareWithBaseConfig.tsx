4918eacd54b456c7c6e6e3f05a6f53222f034368
shivani170 <shivani@devtron.ai>
2022-02-24 23:58:44 +0530

    HT: added base configuration

--- src/components/app/details/cdDetails/CompareWithBaseConfig.tsx
+++ src/components/app/details/cdDetails/CompareWithBaseConfig.tsx
@@ -5,25 +5,30 @@ import { DropdownIndicator } from '../appDetails/utils';
 import { styles } from '../metrics/deploymentMetrics.util';
 
 function CompareWithBaseConfig() {
-  return <div className='border-bottom ml-20 mr-20 mt-12 mb-12'>
-    <div>
-     <div>Compare with</div> 
-							<ReactSelect
-              className='w-200'
-								components={{
-									IndicatorSeparator: null,
-									DropdownIndicator
-								}}
-                value={{label:'tetsinh', value: 'testing'}}
-								placeholder="Select Project"
-								styles={{
-									...styles,
-									...menuList,
-								}}
-							/>
-    </div>
-     
-  </div>;
+    return (
+        <div className="border-bottom ml-20 mr-20 mt-12 mb-12 flex left">
+            <div>
+                <div>Compare with</div>
+                <ReactSelect
+                    className="w-200"
+                    components={{
+                        IndicatorSeparator: null,
+                        DropdownIndicator,
+                    }}
+                    value={{ label: 'tetsinh', value: 'testing' }}
+                    placeholder="Select Project"
+                    styles={{
+                        ...styles,
+                        ...menuList,
+                    }}
+                />
+            </div>
+            <div className="ml-16">
+                Base configuration
+                <div>Mon, 17 Jun 2019, 11:32 AM</div>
+            </div>
+        </div>
+    );
 }
 
 export default CompareWithBaseConfig;
