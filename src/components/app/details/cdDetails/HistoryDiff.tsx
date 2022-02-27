a7699054346fe1b3c04f9339505ba2d451e7e9c2
shivani170 <shivani@devtron.ai>
2022-02-27 23:38:03 +0530

    DT: added code editor

--- src/components/app/details/cdDetails/HistoryDiff.tsx
+++ src/components/app/details/cdDetails/HistoryDiff.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CodeEditor from '../../../CodeEditor/CodeEditor';
 import './cdDetail.scss';
 import CompareWithBaseConfig from './CompareWithBaseConfig';
 
 function HistoryDiff() {
+    const [tempValue, setTempValue] = useState("")
+
     return (
         <div className='historical-diff__container'>
             <div className="historical-diff__left">
                 Deployment Template
                 <div className="cg-5">2 changes</div>
             </div>
-            <div className="historical-diff__right ci-details__body bcn-1">
-                <div className="en-2 bw-1 br-4 deployment-diff__upper bcn-0 ml-20 mt-20 mb-16 mr-20">
+            <div className="historical-diff__right ci-details__body bcn-1 ">
+                <div className="en-2 bw-1 br-4 deployment-diff__upper bcn-0 mt-20 mb-16 mr-20 ml-20">
                     <div className="pl-16 pr-16 pt-16">
                         <div className="pb-16">
                             <div className="cn-6">Chart version</div>
@@ -40,6 +43,16 @@ function HistoryDiff() {
                         </div>
                     </div>
                 </div>
+                <div className="form__row form__row--code-editor-container en-2 bw-1 br-4 mr-20 ml-20">
+                    <CodeEditor
+                        // value={tempValue? tempValue:state ? state.duplicate ? YAML.stringify(state.duplicate, { indent: 2 }) : YAML.stringify(state.data.globalConfig, { indent: 2 }) : ""}
+                        onChange={ tempValue => {setTempValue(tempValue)}}
+                        // defaultValue={state && state.data && state.duplicate ? YAML.stringify(state.data.globalConfig, { indent: 2 }) : ""}
+                        mode={'yaml'}
+                        // validatorSchema={state.data.schema}
+                        >
+                    </CodeEditor>
+                    </div>
             </div>
         </div>
     );
