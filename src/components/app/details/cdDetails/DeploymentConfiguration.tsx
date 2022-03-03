bdb8fdf34aff6cdcf5d58dc54df396801438435b
shivani170 <shivani@devtron.ai>
2022-03-03 23:57:41 +0530

    bsckgroung color of left tab changes

--- src/components/app/details/cdDetails/DeploymentConfiguration.tsx
+++ src/components/app/details/cdDetails/DeploymentConfiguration.tsx
@@ -20,9 +20,10 @@ function DeploymentConfiguration({ setShowTemplate }: TemplateConfiguration) {
             </div> */}
 
             <div
+                 
                 // to={`${match.url}/deployment-template`}
                 onClick={()=>setShowTemplate(true)}
-                className="no-decor bcn-0 cn-9 pl-16 pr-16 pt-12 pb-12 br-4 en-2 bw-1 mb-20 flex content-space cursor"
+                className="bcb-1 no-decor bcn-0 cn-9 pl-16 pr-16 pt-12 pb-12 br-4 en-2 bw-1 mb-20 flex content-space cursor"
             >
                 <div>Deployment template <span className="cn-6 ml-4">2 changes from previous deployment</span></div>
                 <span> <RightArrow className="rotate icon-dim-20" style={{ ['--rotateBy' as any]: '180deg' }} /></span>
