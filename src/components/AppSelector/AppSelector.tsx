acf148a6fe079aa425e928b2b100d9e5ebc2a514
vivek-devtron <vivek@devtron.ai>
2022-03-08 17:30:01 +0530

    Debounced result

--- src/components/AppSelector/AppSelector.tsx
+++ src/components/AppSelector/AppSelector.tsx
@@ -13,6 +13,7 @@ interface AppSelectorType {
 export default function AppSelector({ onChange, appId, appName }: AppSelectorType) {
     const defaultOption = [{ value: appId, label: appName }];
     let selectedValue = defaultOption[0];
+    let timeoutId;
     function noOptionsMessage(inputObj: { inputValue: string }): string {
         if (inputObj && (inputObj.inputValue === '' || inputObj.inputValue.length < 3)) {
             return 'Type 3 chars to see matching results';
@@ -22,35 +23,39 @@ export default function AppSelector({ onChange, appId, appName }: AppSelectorTyp
 
     const appListOptions = (inputValue: string): Promise<[]> =>
         new Promise((resolve) => {
-            if (inputValue.length < 3) {
-                resolve([]);
-                return;
+            if (timeoutId) {
+                clearTimeout(timeoutId);
             }
-            getAppList({
-                appNameSearch: inputValue,
-                sortBy: 'appNameSort',
-                sortOrder: 'ASC',
-                size: 50,
-            })
-                .then((response) => {
-                    let appList = [];
-                    if (response.result && !!response.result.appContainers) {
-                        appList = response.result.appContainers.map((res) => ({
-                            value: res['appId'],
-                            label: res['appName'],
-                            ...res,
-                        }));
-                    }
-                    resolve(appList as []);
-                })
-                .catch((errors: ServerErrors) => {
+            timeoutId = setTimeout(() => {
+                if (inputValue.length < 3) {
                     resolve([]);
-                    if (errors.code) {
-                        showError(errors);
-                    }
-                });
+                    return;
+                }
+                getAppList({
+                    appNameSearch: inputValue,
+                    sortBy: 'appNameSort',
+                    sortOrder: 'ASC',
+                    size: 50,
+                })
+                    .then((response) => {
+                        let appList = [];
+                        if (response.result && !!response.result.appContainers) {
+                            appList = response.result.appContainers.map((res) => ({
+                                value: res['appId'],
+                                label: res['appName'],
+                                ...res,
+                            }));
+                        }
+                        resolve(appList as []);
+                    })
+                    .catch((errors: ServerErrors) => {
+                        resolve([]);
+                        if (errors.code) {
+                            showError(errors);
+                        }
+                    });
+            }, 300);
         });
-
     return (
         <AsyncSelect
             defaultOption
