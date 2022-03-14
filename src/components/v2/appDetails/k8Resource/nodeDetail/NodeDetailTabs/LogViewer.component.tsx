c90e00ab7f14750d98c4696d33b2c25471f7c772
vivek-devtron <vivek@devtron.ai>
2022-03-14 11:51:25 +0530

    minor fix

--- src/components/v2/appDetails/k8Resource/nodeDetail/NodeDetailTabs/LogViewer.component.tsx
+++ src/components/v2/appDetails/k8Resource/nodeDetail/NodeDetailTabs/LogViewer.component.tsx
@@ -79,7 +79,7 @@ const LogViewerComponent: React.FunctionComponent<logViewerInterface> = ({
             fontFamily: 'Inconsolata',
             // disableStdin: true,
             cursorStyle: 'bar',
-            cursorWidth: 0,
+            cursorWidth: 1,
             theme: {
                 background: '#0b0f22',
                 foreground: '#FFFFFF',
