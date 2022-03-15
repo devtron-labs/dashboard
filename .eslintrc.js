a368295858f92c1f643cda0947799acb72aa60c4
vivek-devtron <vivek@devtron.ai>
2022-03-15 11:54:15 +0530

    Dev dependency updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,6 @@ module.exports = {
     },
     "extends": [
         "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint",
         "plugin:prettier/recommended",
         "plugin:react/recommended"
     ],
