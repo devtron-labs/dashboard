cb3ba2b120388cc32a6f6ea9bd65c74e47c3838d
vivek-devtron <vivek@devtron.ai>
2022-03-22 11:12:33 +0530

    Resolved conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,8 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',\
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
         'plugin:react/recommended',
     ],
