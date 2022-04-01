6208995988631863e6c1140d5f4e901eaab13972
shivani170 <shivani@devtron.ai>
2022-04-01 08:35:24 +0530

    text correction in configmap

--- src/components/configMaps/ConfigMap.tsx
+++ src/components/configMaps/ConfigMap.tsx
@@ -491,7 +491,7 @@ export function ConfigMapForm({ appChartRef, id, appId, name = "", external, dat
                                 subPath
                             </a>
                             for volume mount)<br></br>
-                            {isSubPathChecked ? <span className="mb-0 cn-5 fs-11">{ isExternalValues ? 'Please provide keys of secret to be mounted' : 'Keys will be used as filename for subpath'}</span> : null}
+                            {isSubPathChecked ? <span className="mb-0 cn-5 fs-11">{ isExternalValues ? 'Please provide keys of config map to be mounted' : 'Keys will be used as filename for subpath'}</span> : null}
                            
                             {isChartVersion309OrBelow ? <span className="fs-12 fw-5">
                                 <span className="cr-5">Supported for Chart Versions 3.10 and above.</span>
