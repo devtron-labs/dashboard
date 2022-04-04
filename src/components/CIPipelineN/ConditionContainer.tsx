e61c053b1901ab570dc25046e3aded395348d968
vivek-devtron <vivek@devtron.ai>
2022-04-04 18:45:57 +0530

    minor ui fix

--- src/components/CIPipelineN/ConditionContainer.tsx
+++ src/components/CIPipelineN/ConditionContainer.tsx
@@ -29,39 +29,8 @@ export function ConditionContainer({
         { label: '>', value: '>' },
         { label: '!', value: '!' },
     ]
-    const _conditionList = [
-        {
-            id: 0,
-            conditionOnVariable: 'date',
-            conditionOperator: '=',
-            conditionType: 'SUCCESS',
-            conditionalValue: '50',
-        },
-        {
-            id: 1,
-            ConditionOnVariable: 'time',
-            ConditionOperator: '=>',
-            ConditionType: 'SUCCESS',
-            ConditionalValue: '50',
-        },
-        {
-            id: 2,
-            ConditionOnVariable: 'date',
-            ConditionOperator: '<=',
-            ConditionType: 'TRIGGER',
-            ConditionalValue: '50',
-        },
-        {
-            id: 3,
-            ConditionOnVariable: 'time',
-            ConditionOperator: '!=',
-            ConditionType: 'TRIGGER',
-            ConditionalValue: '50',
-        },
-    ]
     const [collapsedSection, setCollapsedSection] = useState<boolean>(true)
-    const [conditionList, setConditionList] = useState(_conditionList)
-    const [selectedOperator, setSelectedOperator] = useState<{ label: string; value: string }>()
+    const [selectedOperator, setSelectedOperator] = useState<{ label: string; value: string }>(operatorOptions[0])
     const [selectedVariable, setSelectedVariable] = useState<{ label: string; value: number }>()
     const [conditionType, setConditionType] = useState<ConditionType>(
         type === ConditionContainerType.PASS_FAILURE ? ConditionType.SUCCESS : ConditionType.TRIGGER,
@@ -140,9 +109,10 @@ export function ConditionContainer({
                 fontWeight: '500',
                 color: 'var(--N900)',
                 fontSize: '12px',
-                padding: '8px 24px',
+                padding: '5px 10px',
             }
         },
+        dropdownIndicator: (styles) => ({ ...styles, padding: 0 }),
     }
 
     return (
@@ -193,10 +163,10 @@ export function ConditionContainer({
                             .filter((condition) => condition.conditionType === conditionType)
                             .map((conditionDetail, index) => (
                                 <>
-                                    <label className="tp-4 fs-13 fw-4 text-uppercase">
+                                    <label className="tp-4 fs-13 fw-4 text-uppercase mr-10">
                                         {conditionDetail.conditionType} If
                                     </label>
-                                    <label className="tp-4 fs-13 fw-4 text-uppercase">
+                                    <label className="tp-4 fs-13 fw-4 text-uppercase mr-10">
                                         <ReactSelect
                                             autoFocus
                                             defaultValue={selectedVariable}
@@ -216,9 +186,8 @@ export function ConditionContainer({
                                             styles={tempMultiSelectStyles}
                                         />
                                     </label>
-                                    <label className="tp-4 fs-13 fw-4 text-uppercase">
+                                    <label className="tp-4 fs-13 fw-4 text-uppercase mr-10">
                                         <ReactSelect
-                                            autoFocus
                                             defaultValue={selectedOperator}
                                             tabIndex={1}
                                             onChange={(selectedValue) => {
@@ -229,7 +198,7 @@ export function ConditionContainer({
                                             styles={tempMultiSelectStyles}
                                         />
                                     </label>
-                                    <div className="p-4 fs-14">
+                                    <div className="fs-13 mr-10">
                                         <input
                                             className="w-100"
                                             type="text"
