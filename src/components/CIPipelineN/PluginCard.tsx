faf5a69b00fc1f5f9a096d788cb8b15ab7f7f013
vivek-devtron <vivek@devtron.ai>
2022-04-07 15:13:27 +0530

    Minor checks

--- src/components/CIPipelineN/PluginCard.tsx
+++ src/components/CIPipelineN/PluginCard.tsx
@@ -19,13 +19,15 @@ export function PluginCard({
             <div>
                 <div className="fs-13 fw-6 cn-9 mb-4 lh-1-25">{title}</div>
                 <div className="fs-12 fw-4 cn-7 lh-16">{subTitle}</div>
-                <div className="mt-10">
-                    {tags?.map((tag, index) => (
-                        <span key={index} className="tag-container fs-11 br-4 mr-8 cn-7 bcn-1">
-                            {tag}{' '}
-                        </span>
-                    ))}
-                </div>
+                {tags && tags.length > 0 && (
+                    <div className="mt-10">
+                        {tags.map((tag, index) => (
+                            <span key={index} className="tag-container fs-11 br-4 mr-8 cn-7 bcn-1">
+                                {tag}{' '}
+                            </span>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     )
