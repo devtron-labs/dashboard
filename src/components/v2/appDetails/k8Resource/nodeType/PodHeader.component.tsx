e060c77e42f1a0ad90762f356ecc1b4e6781e6e1
Milind <milind@devtron.ai>
2022-05-03 14:47:05 +0530

    Isolated components

--- src/components/v2/appDetails/k8Resource/nodeType/PodHeader.component.tsx
+++ src/components/v2/appDetails/k8Resource/nodeType/PodHeader.component.tsx
@@ -3,6 +3,7 @@ import IndexStore from '../../index.store';
 import { getNodeStatus } from './nodeType.util';
 import './nodeType.scss';
 import { iNode } from '../../appDetails.type';
+import PodTabSection from './PodTabSection';
 
 function PodHeaderComponent({ callBack }) {
     const [podTab, selectPodTab] = useState<'old' | 'new'>('new');
@@ -36,54 +37,8 @@ function PodHeaderComponent({ callBack }) {
 
     return (
         <div className="pod-node-tab__wrapper flex left">
-            <TabSection podTab={podTab} selectPodTab={selectPodTab} podStatus={newPods} isNew={true} />
-            <TabSection podTab={podTab} selectPodTab={selectPodTab} podStatus={oldPods} isNew={false} />
-        </div>
-    )
-}
-
-function TabSection({
-    podTab,
-    selectPodTab,
-    podStatus,
-    isNew,
-}: {
-    podTab: string
-    selectPodTab: (string) => void
-    podStatus: { running; all }
-    isNew: boolean
-}) {
-    const dataTestId = isNew ? 'all-pods-new' : 'all-pods-old'
-    return (
-        <div
-            className={
-                isNew
-                    ? `lh-1-4-33 no-decor pod-tab ${
-                          podTab === 'new' ? 'pod-tab__active' : ''
-                      } border-right flex left column pl-16 pr-16 pointer `
-                    : `pod-tab ${
-                          podTab === 'old' ? 'pod-tab__active border-right' : 'pod-tab__transparent-top'
-                      } no-decor flex left column pl-16 pr-16 pointer  `
-            }
-            onClick={(e) => selectPodTab(isNew ? 'new' : 'old')}
-            data-testid={dataTestId}
-        >
-            <div className="fs-14 fw-6 pt-12 ">
-                {' '}
-                {isNew ? 'New Pods' : 'Old Pods'} ({podStatus.all}){' '}
-            </div>
-            <div className="flex left fs-12 cn-9 pb-12">
-                {Object.keys(podStatus)
-                    .filter((n) => n !== 'all')
-                    .map((status, idx) => (
-                        <React.Fragment key={idx}>
-                            {!!idx && <span className="bullet mr-4 ml-4"></span>}
-                            <span key={idx} data-testid={isNew && `new-pod-status-${status}`}>
-                                {podStatus[status]} {status}
-                            </span>
-                        </React.Fragment>
-                    ))}
-            </div>
+            <PodTabSection podTab={podTab} selectPodTab={selectPodTab} podStatus={newPods} isNew={true} />
+            <PodTabSection podTab={podTab} selectPodTab={selectPodTab} podStatus={oldPods} isNew={false} />
         </div>
     )
 }
---
+++ src/components/v2/appDetails/k8Resource/nodeType/PodTabSection.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './nodeType.scss';
+
+function PodTabSection({
+    podTab,
+    selectPodTab,
+    podStatus,
+    isNew,
+}: {
+    podTab: string
+    selectPodTab: (string) => void
+    podStatus: { running; all }
+    isNew: boolean
+}) {
+    const dataTestId = isNew ? 'all-pods-new' : 'all-pods-old'
+    return (
+        <div
+            className={
+                isNew
+                    ? `lh-1-4-33 no-decor pod-tab ${
+                          podTab === 'new' ? 'pod-tab__active' : ''
+                      } border-right flex left column pl-16 pr-16 pointer `
+                    : `pod-tab ${
+                          podTab === 'old' ? 'pod-tab__active border-right' : 'pod-tab__transparent-top'
+                      } no-decor flex left column pl-16 pr-16 pointer  `
+            }
+            onClick={(e) => selectPodTab(isNew ? 'new' : 'old')}
+            data-testid={dataTestId}
+        >
+            <div className="fs-14 fw-6 pt-12 ">
+                {' '}
+                {isNew ? 'New Pods' : 'Old Pods'} ({podStatus.all}){' '}
+            </div>
+            <div className="flex left fs-12 cn-9 pb-12">
+                {Object.keys(podStatus)
+                    .filter((n) => n !== 'all')
+                    .map((status, idx) => (
+                        <React.Fragment key={idx}>
+                            {!!idx && <span className="bullet mr-4 ml-4"></span>}
+                            <span key={idx} data-testid={isNew && `new-pod-status-${status}`}>
+                                {podStatus[status]} {status}
+                            </span>
+                        </React.Fragment>
+                    ))}
+            </div>
+        </div>
+    )
+}
+
+export default PodTabSection;
