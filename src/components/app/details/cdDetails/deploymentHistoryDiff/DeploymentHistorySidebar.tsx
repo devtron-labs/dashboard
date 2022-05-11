b5cc2c291eafa05d427bc486fcc0318cb9bea29b
shivani170 <shivani@devtron.ai>
2022-05-11 12:21:38 +0530

    SIDEBAR: css fixes

--- src/components/app/details/cdDetails/deploymentHistoryDiff/DeploymentHistorySidebar.tsx
+++ src/components/app/details/cdDetails/deploymentHistoryDiff/DeploymentHistorySidebar.tsx
@@ -10,11 +10,12 @@ function DeploymentHistorySidebar() {
              const url = match.url.includes(DEPLOYMENT_HISTORY_LINK_MAP[elm.label.toLowerCase()]) ? match.url : DEPLOYMENT_HISTORY_LINK_MAP[elm.label.toLowerCase()]
 
                 return (
-                    <div className={`fw-6 cursor pt-12 pb-12`} key={`deployment-history-sidebar-${index}`}>
+                    <div className={`cursor`} key={`deployment-history-sidebar-${index}`}>
                         <NavLink
                             activeClassName="active"
                             to={url}
-                            className={'no-decor pt-12 pb-12 pl-16 pr-12 fs-14 cn-9 node-link w-100'}
+                            className={'inline-block no-decor pt-12 pb-12 pl-16 pr-12 fs-13 cn-9 configuration-link w-280' 
+                        }
                         >
                             {elm.label}
                         </NavLink>
