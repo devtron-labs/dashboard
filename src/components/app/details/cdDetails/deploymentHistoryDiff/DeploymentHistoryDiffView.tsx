99f650e7bcfde59f9233b87b35f7fe1d891b2c77
vivek-devtron <vivek@devtron.ai>
2022-05-13 12:05:51 +0530

    Code cleanup

--- src/components/app/details/cdDetails/deploymentHistoryDiff/DeploymentHistoryDiffView.tsx
+++ src/components/app/details/cdDetails/deploymentHistoryDiff/DeploymentHistoryDiffView.tsx
@@ -1,8 +1,7 @@
 import React, { Fragment } from 'react'
 import CodeEditor from '../../../../CodeEditor/CodeEditor'
-import { DeploymentTemplateHistoryType } from '../cd.type'
+import { DeploymentHistorySingleValue, DeploymentTemplateHistoryType } from '../cd.type'
 import YAML from 'yaml'
-import { Progressing } from '../../../../common'
 
 export default function DeploymentHistoryDiffView({
     currentConfiguration,
@@ -10,27 +9,32 @@ export default function DeploymentHistoryDiffView({
     codeEditorLoading,
 }: DeploymentTemplateHistoryType) {
     const renderDeploymentDiffViaCodeEditor = () => {
-        return codeEditorLoading ? (
-            <div className="w-100 flex" style={{ minHeight: '500px' }}>
-                <Progressing pageLoader />
-            </div>
-        ) : (
-            <>
-                {baseTemplateConfiguration?.['codeEditorValue']?.['value'] &&
-                    currentConfiguration?.['codeEditorValue']?.['value'] && (
-                        <CodeEditor
-                            value={YAML.stringify(JSON.parse(baseTemplateConfiguration.codeEditorValue['value']))}
-                            defaultValue={currentConfiguration.codeEditorValue['value']}
-                            height={600}
-                            mode="yaml"
-                            diffView={true}
-                            readOnly={true}
-                            loading={codeEditorLoading}
-                        ></CodeEditor>
-                    )}
-            </>
+        return (
+            baseTemplateConfiguration?.codeEditorValue?.value &&
+            currentConfiguration?.codeEditorValue?.value && (
+                <CodeEditor
+                    value={YAML.stringify(JSON.parse(baseTemplateConfiguration.codeEditorValue.value))}
+                    defaultValue={currentConfiguration.codeEditorValue.value}
+                    height={600}
+                    mode="yaml"
+                    diffView={true}
+                    readOnly={true}
+                    loading={codeEditorLoading}
+                ></CodeEditor>
+            )
         )
     }
+    const renderValue = (parentClassName: string, value: DeploymentHistorySingleValue) => {
+        const titleStyle = 'cn-6 pt-8 pl-16 pr-16 lh-16'
+        const descriptionStyle = 'cn-9 fs-13 pb-8 pl-16 pr-16 lh-20 mh-28'
+        return (
+            <div className={parentClassName}>
+                <div className={titleStyle}>{value.displayName}</div>
+                <div className={descriptionStyle}>{value.value}</div>
+            </div>
+        )
+    }
+
     return (
         <div>
             <div className="en-2 bw-1 br-4 bcn-0 mt-20 mb-16 mr-20 ml-20 pt-2 pb-2 deployment-diff__upper">
@@ -41,26 +45,15 @@ export default function DeploymentHistoryDiffView({
                             const currentValue = currentConfiguration.values[configKey]
                             const baseValue = baseTemplateConfiguration.values[configKey]
                             const changeBGColor = currentValue?.value !== baseValue?.value
-                            const titleStyle = 'cn-6 pt-8 pl-16 pr-16 lh-16'
-                            const descriptionStyle = 'cn-9 fs-13 pb-8 pl-16 pr-16 lh-20 mh-28'
-                            const baseTemplateBGStyle = changeBGColor ? ' bcg-1' : ''
-                            const currentTemplateBGStyle = changeBGColor ? ' bcr-1' : ''
                             return (
                                 <Fragment key={`deployment-history-diff-view-${index}`}>
                                     {currentValue && currentValue.value ? (
-                                        <div className={currentTemplateBGStyle}>
-                                            <div className={titleStyle}>{currentValue['displayName']}</div>
-                                            <div className={descriptionStyle}>{currentValue['value'] + ''}</div>
-                                        </div>
+                                        renderValue(changeBGColor ? ' bcr-1' : '', currentValue)
                                     ) : (
                                         <div></div>
                                     )}
-
                                     {baseValue && baseValue.value ? (
-                                        <div className={baseTemplateBGStyle}>
-                                            <div className={titleStyle}>{baseValue['displayName']}</div>
-                                            <div className={descriptionStyle}>{baseValue['value'] + ''}</div>
-                                        </div>
+                                        renderValue(changeBGColor ? ' bcg-1' : '', baseValue)
                                     ) : (
                                         <div></div>
                                     )}
