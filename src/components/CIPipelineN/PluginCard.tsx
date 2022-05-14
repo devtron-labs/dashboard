b928f75eb387b5705572393ce2196619441b984e
vivek-devtron <vivek@devtron.ai>
2022-05-14 10:56:38 +0530

    Minor UI fix

--- src/components/CIPipelineN/PluginCard.tsx
+++ src/components/CIPipelineN/PluginCard.tsx
@@ -22,7 +22,7 @@ export function PluginCard({
                 {tags && tags.length > 0 && (
                     <div className="mt-10">
                         {tags.map((tag, index) => (
-                            <span key={index} className="tag-container fs-11 br-4 mr-8 cn-7 bcn-1">
+                            <span key={index} className="fs-11 br-4 mr-8 cn-7 bcn-1 pr-6 pl-6">
                                 {tag}{' '}
                             </span>
                         ))}
