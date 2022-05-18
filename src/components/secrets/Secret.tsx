f7fb9f5be11c1d38fc2439c84d73e475dca0059a
Vivekanand Pandey <95338474+vivek-devtron@users.noreply.github.com>
2022-05-18 13:23:49 +0530

    External_type from constant

--- src/components/secrets/Secret.tsx
+++ src/components/secrets/Secret.tsx
@@ -8,7 +8,7 @@ import { toast } from 'react-toastify';
 import { KeyValueInput, useKeyValueYaml, validateKeyValuePair } from '../configMaps/ConfigMap'
 import { getSecretList } from '../../services/service';
 import CodeEditor from '../CodeEditor/CodeEditor'
-import { DOCUMENTATION, PATTERNS } from '../../config';
+import { DOCUMENTATION, PATTERNS, EXTERNAL_TYPES } from '../../config';
 import YAML from 'yaml'
 import keyIcon from '../../assets/icons/ic-key.svg'
 import addIcon from '../../assets/icons/ic-add.svg'
@@ -235,13 +235,6 @@ export const SecretForm: React.FC<SecretFormProps> = function (props) {
     const [codeEditorRadio, setCodeEditorRadio] = useState("data")
     const isExternalValues = externalType !== "KubernetesSecret";
     const tabs = [{ title: 'Environment Variable' }, { title: 'Data Volume' }].map(data => ({ ...data, active: data.title === selectedTab }))
-    const externalTypes = {
-        "": "Kubernetes Secret",
-        "KubernetesSecret": "Kubernetes External Secret",
-        "AWSSecretsManager": "AWS Secrets Manager",
-        "AWSSystemManager": "AWS System Manager",
-        "HashiCorpVault": "Hashi Corp Vault"
-    }
     const isHashiOrAWS = (externalType === "AWSSecretsManager" || externalType === "AWSSystemManager" || externalType === "HashiCorpVault");
 
     function setKeyValueArray(arr) {
@@ -549,8 +542,8 @@ export const SecretForm: React.FC<SecretFormProps> = function (props) {
             <label className="form__label">Data type</label>
             <div className="form-row__select-external-type flex">
                 <Select value={externalType} onChange={e => { setExternalType(e.target.value) }} disabled={secretMode}>
-                    <Select.Button>{externalType || (externalType === "") ? externalTypes[externalType] : `Select Secret Type`}</Select.Button>
-                    {Object.entries(externalTypes).map(([value, name]) =>
+                    <Select.Button>{externalType || (externalType === "") ? EXTERNAL_TYPES[externalType] : `Select Secret Type`}</Select.Button>
+                    {Object.entries(EXTERNAL_TYPES).map(([value, name]) =>
                         <Select.Option key={value} value={value}>{name}</Select.Option>)}
                 </Select>
             </div>
