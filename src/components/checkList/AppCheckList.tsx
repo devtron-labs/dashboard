52fcb868c09236e00ce4f919f44b5b69f94af9cb
Milind <milind@devtron.ai>
2022-05-25 16:58:01 +0530

    Added changes to remove selected checklist

--- src/components/checkList/AppCheckList.tsx
+++ src/components/checkList/AppCheckList.tsx
@@ -21,24 +21,24 @@ export class AppCheckList extends Component<AppCheckListProps, AppCheckListState
             </div>
             {!this.props.isAppCollapsed ? <div className="fs-13">
                 {this.props.showDivider && <hr className="checklist__divider mt-0 mb-0" />}
-                <NavLink to={`${URLS.GLOBAL_CONFIG_HOST_URL}`} className="no-decor mt-8 flex left" style={{ ['color']: hostUrl ? `#767d84` : `#0066cc` }} >
+                {!hostUrl && <NavLink to={`${URLS.GLOBAL_CONFIG_HOST_URL}`} className="no-decor mt-8 flex left" style={{ ['color']: hostUrl ? `#767d84` : `#0066cc` }} >
                     {!hostUrl ? <img src={Uncheck} className="icon-dim-16 flex mr-8" /> : <Check className="icon-dim-16 mr-8" />}
-                    Add host URL</NavLink>
-                <NavLink to={`${URLS.GLOBAL_CONFIG_GITOPS}`} className="no-decor  mt-8 flex left" style={{ ['color']: gitOps ? `#767d84` : `#0066cc` }}>
+                    Add host URL</NavLink>}
+                {!gitOps && <NavLink to={`${URLS.GLOBAL_CONFIG_GITOPS}`} className="no-decor  mt-8 flex left" style={{ ['color']: gitOps ? `#767d84` : `#0066cc` }}>
                     {!gitOps ? <img src={Uncheck} className="icon-dim-16 mr-8" /> : <Check className="icon-dim-16 mr-8" />}
-                    Configure gitops</NavLink>
-                <NavLink to={`${URLS.GLOBAL_CONFIG_PROJECT}`} className="no-decor  mt-8 flex left" style={{ ['color']: project ? `#767d84` : `#0066cc` }}>
+                    Configure gitops</NavLink>}
+                {!project && <NavLink to={`${URLS.GLOBAL_CONFIG_PROJECT}`} className="no-decor  mt-8 flex left" style={{ ['color']: project ? `#767d84` : `#0066cc` }}>
                     {!project ? <img src={Uncheck} className="icon-dim-16 mr-8" /> : <Check className="icon-dim-16 mr-8" />}
-                    Add project</NavLink>
-                <NavLink to={`${URLS.GLOBAL_CONFIG_CLUSTER}`} className="no-decor mt-8 flex left" style={{ ['color']: environment ? `#767d84` : `#0066cc` }}>
+                    Add project</NavLink>}
+                {!environment && <NavLink to={`${URLS.GLOBAL_CONFIG_CLUSTER}`} className="no-decor mt-8 flex left" style={{ ['color']: environment ? `#767d84` : `#0066cc` }}>
                     {!environment ? <img src={Uncheck} className="icon-dim-16 mr-8" /> : <Check className="icon-dim-16 mr-8" />}
-                    Add cluster & environment</NavLink>
-                <NavLink to={`${URLS.GLOBAL_CONFIG_GIT}`} className="no-decor mt-8 flex left" style={{ ['color']: git ? `#767d84` : `#0066cc` }}>
+                    Add cluster & environment</NavLink>}
+                {!git && <NavLink to={`${URLS.GLOBAL_CONFIG_GIT}`} className="no-decor mt-8 flex left" style={{ ['color']: git ? `#767d84` : `#0066cc` }}>
                     {!git ? <img src={Uncheck} className="icon-dim-16 mr-8" /> : <Check className="icon-dim-16 mr-8" />}
-                    Add git account</NavLink>
-                <NavLink to={`${URLS.GLOBAL_CONFIG_DOCKER}`} className="no-decor mt-8 pb-8 flex left" style={{ ['color']: docker ? `#767d84` : `#0066cc` }}>
+                    Add git account</NavLink>}
+                {!docker && <NavLink to={`${URLS.GLOBAL_CONFIG_DOCKER}`} className="no-decor mt-8 pb-8 flex left" style={{ ['color']: docker ? `#767d84` : `#0066cc` }}>
                     {!docker ? <img src={Uncheck} className="icon-dim-16 mr-8" /> : <Check className="icon-dim-16 mr-8" />}
-                    Add container registry</NavLink>
+                    Add container registry</NavLink>}
             </div> : ''}
         </>
     }
