8f6568c99e5c8938f94b3e9fefca33f629da0770
shivani170 <shivani@devtron.ai>
2022-05-26 23:57:21 +0530

    MAIN: formatting

--- src/components/app/details/main.tsx
+++ src/components/app/details/main.tsx
@@ -1,36 +1,36 @@
-import React, { lazy, Suspense, useCallback, useRef, useEffect, useState } from 'react';
-import { Switch, Route, Redirect, NavLink } from 'react-router-dom';
-import { ErrorBoundary, Progressing, BreadCrumb, useBreadcrumb, useAsync, showError, VisibleModal } from '../../common';
-import { getAppListMin } from '../../../services/service';
-import { useParams, useRouteMatch, useHistory, generatePath, useLocation } from 'react-router';
-import { URLS } from '../../../config';
-import { AppSelector } from '../../AppSelector';
-import ReactGA from 'react-ga';
-import AppConfig from './appConfig/AppConfig';
-import './appDetails/appDetails.scss';
-import './app.css';
-import Tippy from '@tippyjs/react';
-import { getAppMetaInfo, createAppLabels } from '../service';
-import { toast } from 'react-toastify';
-import { OptionType } from '../types';
-import AboutAppInfoModal from './AboutAppInfoModal';
-import { validateTags, TAG_VALIDATION_MESSAGE, createOption, handleKeyDown } from '../appLabelCommon';
-import { ReactComponent as Settings } from '../../../assets/icons/ic-settings.svg';
-import { ReactComponent as Info } from '../../../assets/icons/ic-info-outlined.svg';
-import { EnvType } from '../../v2/appDetails/appDetails.type';
+import React, { lazy, Suspense, useCallback, useRef, useEffect, useState } from 'react'
+import { Switch, Route, Redirect, NavLink } from 'react-router-dom'
+import { ErrorBoundary, Progressing, BreadCrumb, useBreadcrumb, useAsync, showError, VisibleModal } from '../../common'
+import { getAppListMin } from '../../../services/service'
+import { useParams, useRouteMatch, useHistory, generatePath, useLocation } from 'react-router'
+import { URLS } from '../../../config'
+import { AppSelector } from '../../AppSelector'
+import ReactGA from 'react-ga'
+import AppConfig from './appConfig/AppConfig'
+import './appDetails/appDetails.scss'
+import './app.css'
+import Tippy from '@tippyjs/react'
+import { getAppMetaInfo, createAppLabels } from '../service'
+import { toast } from 'react-toastify'
+import { OptionType } from '../types'
+import AboutAppInfoModal from './AboutAppInfoModal'
+import { validateTags, TAG_VALIDATION_MESSAGE, createOption, handleKeyDown } from '../appLabelCommon'
+import { ReactComponent as Settings } from '../../../assets/icons/ic-settings.svg'
+import { ReactComponent as Info } from '../../../assets/icons/ic-info-outlined.svg'
+import { EnvType } from '../../v2/appDetails/appDetails.type'
 
-const TriggerView = lazy(() => import('./triggerView/TriggerView'));
-const DeploymentMetrics = lazy(() => import('./metrics/DeploymentMetrics'));
-const CIDetails = lazy(() => import('./cIDetails/CIDetails'));
-const AppDetails = lazy(() => import('./appDetails/AppDetails'));
-const IndexComponent = lazy(() => import('../../v2/index'));
+const TriggerView = lazy(() => import('./triggerView/TriggerView'))
+const DeploymentMetrics = lazy(() => import('./metrics/DeploymentMetrics'))
+const CIDetails = lazy(() => import('./cIDetails/CIDetails'))
+const AppDetails = lazy(() => import('./appDetails/AppDetails'))
+const IndexComponent = lazy(() => import('../../v2/index'))
 
-const CDDetails = lazy(() => import('./cdDetails/CDDetails'));
-const TestRunList = lazy(() => import('./testViewer/TestRunList'));
+const CDDetails = lazy(() => import('./cdDetails/CDDetails'))
+const TestRunList = lazy(() => import('./testViewer/TestRunList'))
 
 export default function AppDetailsPage({ isV2 }) {
-    const { path } = useRouteMatch();
-    const { appId } = useParams<{ appId }>();
+    const { path } = useRouteMatch()
+    const { appId } = useParams<{ appId }>()
 
     return (
         <div className="app-details-page">
@@ -72,131 +72,131 @@ export default function AppDetailsPage({ isV2 }) {
                 </Suspense>
             </ErrorBoundary>
         </div>
-    );
+    )
 }
 
 export function AppHeader() {
-    const { appId } = useParams<{ appId }>();
-    const match = useRouteMatch();
-    const history = useHistory();
-    const location = useLocation();
-    const currentPathname = useRef('');
-    const [showInfoModal, setShowInfoModal] = useState(false);
-    const [submitting, setSubmitting] = useState(false);
+    const { appId } = useParams<{ appId }>()
+    const match = useRouteMatch()
+    const history = useHistory()
+    const location = useLocation()
+    const currentPathname = useRef('')
+    const [showInfoModal, setShowInfoModal] = useState(false)
+    const [submitting, setSubmitting] = useState(false)
     const [labelTags, setLabelTags] = useState<{ tags: OptionType[]; inputTagValue: string; tagError: string }>({
         tags: [],
         inputTagValue: '',
         tagError: '',
-    });
-    const [result, setResult] = useState(undefined);
-    const [isLoading, setIsLoading] = useState(false);
-    const [appName, setAppName] = useState('');
+    })
+    const [result, setResult] = useState(undefined)
+    const [isLoading, setIsLoading] = useState(false)
+    const [appName, setAppName] = useState('')
 
     const getAppMetaInfoRes = () => {
-        setIsLoading(true);
+        setIsLoading(true)
         const res = getAppMetaInfo(appId).then((_result) => {
-            setAppName(_result?.result?.appName);
+            setAppName(_result?.result?.appName)
             let labelOptionRes = _result?.result?.labels?.map((_label) => {
                 return {
                     label: `${_label.key?.toString()}:${_label.value?.toString()}`,
                     value: `${_label.key?.toString()}:${_label.value?.toString()}`,
-                };
-            });
-            setResult(_result);
-            setIsLoading(false);
-            setLabelTags({ tags: labelOptionRes || [], inputTagValue: '', tagError: '' });
-        });
-    };
+                }
+            })
+            setResult(_result)
+            setIsLoading(false)
+            setLabelTags({ tags: labelOptionRes || [], inputTagValue: '', tagError: '' })
+        })
+    }
 
     useEffect(() => {
         try {
-            getAppMetaInfoRes();
+            getAppMetaInfoRes()
         } catch (err) {
-            showError(err);
+            showError(err)
         }
-    }, [appId]);
+    }, [appId])
 
     function validateForm(): boolean {
         if (
             labelTags.tags.length !== labelTags.tags.map((tag) => tag.value).filter((tag) => validateTags(tag)).length
         ) {
-            setLabelTags((labelTags) => ({ ...labelTags, tagError: TAG_VALIDATION_MESSAGE.error }));
-            return false;
+            setLabelTags((labelTags) => ({ ...labelTags, tagError: TAG_VALIDATION_MESSAGE.error }))
+            return false
         }
-        return true;
+        return true
     }
 
     function handleInputChange(inputTagValue) {
-        setLabelTags((tags) => ({ ...tags, inputTagValue: inputTagValue, tagError: '' }));
+        setLabelTags((tags) => ({ ...tags, inputTagValue: inputTagValue, tagError: '' }))
     }
 
     function handleTagsChange(newValue: any, actionMeta: any) {
-        setLabelTags((tags) => ({ ...tags, tags: newValue || [], tagError: '' }));
+        setLabelTags((tags) => ({ ...tags, tags: newValue || [], tagError: '' }))
     }
 
     function handleCreatableBlur(e) {
-        labelTags.inputTagValue = labelTags.inputTagValue.trim();
-        if (!labelTags.inputTagValue) return;
+        labelTags.inputTagValue = labelTags.inputTagValue.trim()
+        if (!labelTags.inputTagValue) return
         setLabelTags({
             inputTagValue: '',
             tags: [...labelTags.tags, createOption(e.target.value)],
             tagError: '',
-        });
+        })
     }
 
     async function handleSubmit(e) {
-        const validForm = validateForm();
+        const validForm = validateForm()
         if (!validForm) {
-            return;
+            return
         }
-        setSubmitting(true);
+        setSubmitting(true)
 
-        let _optionTypes = [];
+        let _optionTypes = []
         if (labelTags.tags && labelTags.tags.length > 0) {
             labelTags.tags.forEach((_label) => {
-                let colonIndex = _label.value.indexOf(':');
-                let splittedTagBeforeColon = _label.value.substring(0, colonIndex);
-                let splittedTagAfterColon = _label.value.substring(colonIndex + 1);
+                let colonIndex = _label.value.indexOf(':')
+                let splittedTagBeforeColon = _label.value.substring(0, colonIndex)
+                let splittedTagAfterColon = _label.value.substring(colonIndex + 1)
                 _optionTypes.push({
                     key: splittedTagBeforeColon,
                     value: splittedTagAfterColon,
-                });
-            });
+                })
+            })
         }
 
         const payload = {
             appId: parseInt(appId),
             labels: _optionTypes,
-        };
+        }
 
         try {
-            const { result } = await createAppLabels(payload);
-            setShowInfoModal(false);
-            toast.success('Successfully saved.');
+            const { result } = await createAppLabels(payload)
+            setShowInfoModal(false)
+            toast.success('Successfully saved.')
         } catch (err) {
-            showError(err);
+            showError(err)
         } finally {
-            setSubmitting(false);
+            setSubmitting(false)
         }
     }
 
     useEffect(() => {
-        currentPathname.current = location.pathname;
-    }, [location.pathname]);
+        currentPathname.current = location.pathname
+    }, [location.pathname])
 
     const handleAppChange = useCallback(
         ({ label, value }) => {
-            const tab = currentPathname.current.replace(match.url, '').split('/')[1];
-            const newUrl = generatePath(match.path, { appId: value });
-            history.push(`${newUrl}/${tab}`);
+            const tab = currentPathname.current.replace(match.url, '').split('/')[1]
+            const newUrl = generatePath(match.path, { appId: value })
+            history.push(`${newUrl}/${tab}`)
             ReactGA.event({
                 category: 'App Selector',
                 action: 'App Selection Changed',
                 label: tab,
-            });
+            })
         },
         [location.pathname],
-    );
+    )
 
     const { breadcrumbs } = useBreadcrumb(
         {
@@ -212,18 +212,18 @@ export function AppHeader() {
             },
         },
         [appId, appName],
-    );
+    )
 
     let newTag = labelTags.inputTagValue.split(',').map((e) => {
-        e = e.trim();
-        return createOption(e);
-    });
+        e = e.trim()
+        return createOption(e)
+    })
     const setAppTagLabel = () =>
         setLabelTags({
             inputTagValue: '',
             tags: [...labelTags.tags, ...newTag],
             tagError: '',
-        });
+        })
 
     return (
         <div className="page-header" style={{ gridTemplateColumns: 'unset' }}>
@@ -232,7 +232,7 @@ export function AppHeader() {
                 <div
                     className="tab-list__info-icon ml-4 cursor"
                     onClick={() => {
-                        return setShowInfoModal(true), getAppMetaInfoRes();
+                        return setShowInfoModal(true), getAppMetaInfoRes()
                     }}
                 >
                     <Tippy className="default-tt " arrow={false} content={'About app'}>
@@ -269,7 +269,7 @@ export function AppHeader() {
                             ReactGA.event({
                                 category: 'App',
                                 action: 'App Details Clicked',
-                            });
+                            })
                         }}
                     >
                         App Details
@@ -284,7 +284,7 @@ export function AppHeader() {
                             ReactGA.event({
                                 category: 'App',
                                 action: 'Build & Deploy Clicked',
-                            });
+                            })
                         }}
                     >
                         Build & Deploy
@@ -299,7 +299,7 @@ export function AppHeader() {
                             ReactGA.event({
                                 category: 'App',
                                 action: 'Build History Clicked',
-                            });
+                            })
                         }}
                     >
                         Build History
@@ -314,7 +314,7 @@ export function AppHeader() {
                             ReactGA.event({
                                 category: 'App',
                                 action: 'Deployment History Clicked',
-                            });
+                            })
                         }}
                     >
                         Deployment History
@@ -329,7 +329,7 @@ export function AppHeader() {
                             ReactGA.event({
                                 category: 'App',
                                 action: 'Deployment Metrics Clicked',
-                            });
+                            })
                         }}
                     >
                         Deployment Metrics
@@ -345,7 +345,7 @@ export function AppHeader() {
                             ReactGA.event({
                                 category: 'App',
                                 action: 'App Configuration Clicked',
-                            });
+                            })
                         }}
                     >
                         <Settings className="tab-list__icon icon-dim-16 fcn-9 mr-4" />
@@ -360,5 +360,5 @@ export function AppHeader() {
                 </li> */}
             </ul>
         </div>
-    );
+    )
 }
