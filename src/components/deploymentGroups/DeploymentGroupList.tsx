362551a7a520ac8c7a50bc7b858303c934db1d7c
shivani170 <shivani@devtron.ai>
2022-06-02 10:30:16 +0530

    added missing  create group button

--- src/components/deploymentGroups/DeploymentGroupList.tsx
+++ src/components/deploymentGroups/DeploymentGroupList.tsx
@@ -305,17 +305,24 @@ export default class DeploymentGroupList extends Component<BulkActionListProps, 
 
     renderCreateButton = () => {
         return (
-            <button type="button" className="flex cta h-32 lh-n" onClick={() => this.redirectToCreateGroup()}>
-                <Add className="icon-dim-20" />
-                Create Group
-            </button>
+            this.state.view !== ViewType.LOADING &&
+            this.state.view !== ViewType.EMPTY && (
+                <button type="button" className="flex cta h-32 lh-n" onClick={() => this.redirectToCreateGroup()}>
+                    <Add className="icon-dim-20" />
+                    Create Group
+                </button>
+            )
         )
     }
 
     render() {
         return (
             <div>
-                <PageHeader headerName="Deployment Groups" />
+                <PageHeader
+                    headerName="Deployment Groups"
+                    showCreateButton={true}
+                    renderCreateButton={this.renderCreateButton}
+                />
 
                 <div className="deployment-group-list-page__body">
                     {this.state.view === ViewType.LOADING && <Progressing pageLoader />}
