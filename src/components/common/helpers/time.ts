72c178f6bdbd1b9c46e18670468a1b57a355e033
Milind <milind@devtron.ai>
2022-07-14 14:12:16 +0530

    Fixed incorrect timezone

--- src/components/common/helpers/time.ts
+++ src/components/common/helpers/time.ts
@@ -4,7 +4,7 @@ export function ISTTimeModal(ts: string, isRelativeTime = false) {
     let timestamp = "";
     try {
         if (ts && ts.length) {
-            let date = moment(ts).utc(false).tz('Asia/Kolkata');
+            let date = moment(ts)
             if(isRelativeTime){
                 // check for minimum date (zero date) (Invoking an empty time.Time struct literal will return Go's zero date)
                 if(ts != '0001-01-01T00:00:00Z'){
