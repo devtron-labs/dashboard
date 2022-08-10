abcac44496e6507a331d64d96cf8f40f2a0c518f
Milind <milind@devtron.ai>
2022-08-10 19:08:44 +0530

    Minor bug fix

--- src/components/common/infocolourBar/InfoColourbar.tsx
+++ src/components/common/infocolourBar/InfoColourbar.tsx
@@ -11,7 +11,7 @@ interface InfoColourBarType {
 
 function InfoColourBar({ message, classname, Icon, iconClass, renderActionButton }: InfoColourBarType) {
     return (
-        <div className={`${classname} info flex content-space pt-10 pb-10 pl-16 pr-16 br-4 top fs-13 fw-4`}>
+        <div className={`${classname} info_text flex content-space pt-10 pb-10 pl-16 pr-16 br-4 top fs-13 fw-4`}>
             <span className="flex top">
                 <div className="icon-dim-20 mr-10">
                     <Icon className={`${iconClass} icon-dim-20 mr-8`} />
