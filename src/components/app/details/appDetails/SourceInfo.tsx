53a6ba740144c7c853085e88344749a36f60389a
vivek-devtron <vivek@devtron.ai>
2022-09-01 00:28:23 +0530

    Added helm and gitops icon on app details

--- src/components/app/details/appDetails/SourceInfo.tsx
+++ src/components/app/details/appDetails/SourceInfo.tsx
@@ -10,6 +10,8 @@ import { ReactComponent as CommitIcon } from '../../../../assets/icons/ic-code-c
 import { ReactComponent as Question } from '../../../../assets/icons/ic-help-outline.svg'
 import { ReactComponent as Timer } from '../../../../assets/icons/ic-timer.svg'
 import { ReactComponent as CD } from '../../../../assets/icons/ic-CD.svg'
+import { ReactComponent as ArgoCD } from '../../../../assets/icons/argo-cd-app.svg'
+import { ReactComponent as Helm } from '../../../../assets/icons/helm-app.svg'
 import { useParams } from 'react-router'
 import { Nodes } from '../../types'
 import Tippy from '@tippyjs/react'
@@ -26,7 +28,7 @@ export function SourceInfo({
     deploymentStatus = null,
     deploymentStatusText = null,
     deploymentTriggerTime = null,
-    triggeredBy = null
+    triggeredBy = null,
 }) {
     const status = appDetails?.resourceTree?.status || ''
     const params = useParams<{ appId: string; envId?: string }>()
@@ -61,6 +63,13 @@ export function SourceInfo({
         <div className="flex left w-100 column w-100 source-info-container">
             <div className="flex left w-100 mb-16">
                 <EnvSelector environments={environments} disabled={params.envId && !showCommitInfo} />
+                <Tippy className="default-tt" arrow={false} placement="top" content={`Deployed using ${appDetails?.deploymentAppType === DeploymentAppType.helm?`Helm`:`GitOps`}`}>
+                    {appDetails?.deploymentAppType === DeploymentAppType.helm ? (
+                        <Helm className="icon-dim-24 ml-20" />
+                    ) : (
+                      <ArgoCD className="icon-dim-24 ml-20" />
+                    )}
+                </Tippy>
                 <div style={{ marginLeft: 'auto' }} className="flex right fs-12 cn-9">
                     {showCommitInfo && (
                         <button
@@ -128,55 +137,57 @@ export function SourceInfo({
                                 </div>
                             </div>
                         </div>
-                       {appDetails?.deploymentAppType !== DeploymentAppType.helm && <div
-                            onClick={showDeploymentDetailedStatus}
-                            className="pointer flex left bcn-0 p-16 br-4 mw-382 en-2 bw-1"
-                        >
-                            <div className="mw-48 mh-48 bcn-1 flex br-4 mr-16">
-                                <CD className="icon-dim-32" />
-                            </div>
-                            <div className="flex left column pr-16 border-right-n1 mr-16">
-                                <div className="flexbox">
-                                    <span className="fs-12 mr-5 fw-4 cn-9">Deployment status</span>
+                        {appDetails?.deploymentAppType !== DeploymentAppType.helm && (
+                            <div
+                                onClick={showDeploymentDetailedStatus}
+                                className="pointer flex left bcn-0 p-16 br-4 mw-382 en-2 bw-1"
+                            >
+                                <div className="mw-48 mh-48 bcn-1 flex br-4 mr-16">
+                                    <CD className="icon-dim-32" />
+                                </div>
+                                <div className="flex left column pr-16 border-right-n1 mr-16">
+                                    <div className="flexbox">
+                                        <span className="fs-12 mr-5 fw-4 cn-9">Deployment status</span>
 
-                                    <Tippy
-                                        className="default-tt"
-                                        arrow={false}
-                                        placement="top"
-                                        content="Status of last triggered deployment"
-                                    >
-                                        <Question className="icon-dim-16 mt-2" />
-                                    </Tippy>
+                                        <Tippy
+                                            className="default-tt"
+                                            arrow={false}
+                                            placement="top"
+                                            content="Status of last triggered deployment"
+                                        >
+                                            <Question className="icon-dim-16 mt-2" />
+                                        </Tippy>
+                                    </div>
+                                    <div className="flexbox">
+                                        <span
+                                            className={`app-summary__status-name fs-14 mr-8 fw-6 f-${deploymentStatus} ${
+                                                deploymentStatus === 'inprogress' ? 'loading-dots' : ''
+                                            }`}
+                                        >
+                                            {deploymentStatusText}
+                                        </span>
+                                        <div className={`${deploymentStatus} icon-dim-20 mt-2`}></div>
+                                    </div>
+                                    <div>
+                                        <span className="cb-5 fw-6 pointer">Details</span>
+                                    </div>
                                 </div>
-                                <div className="flexbox">
-                                    <span
-                                        className={`app-summary__status-name fs-14 mr-8 fw-6 f-${deploymentStatus} ${
-                                            deploymentStatus === 'inprogress' ? 'loading-dots' : ''
-                                        }`}
-                                    >
-                                        {deploymentStatusText}
-                                    </span>
-                                    <div className={`${deploymentStatus} icon-dim-20 mt-2`}></div>
-                                </div>
-                                <div>
-                                    <span className="cb-5 fw-6 pointer">Details</span>
+                                <div className="flex left column mw-140">
+                                    <div className="fs-12 fw-4 cn-9">Deployment triggered</div>
+                                    <div className="flexbox">
+                                        <span className="fs-13 mr-5 fw-6 cn-9">
+                                            {deploymentTriggerTime
+                                                ? moment(deploymentTriggerTime, 'YYYY-MM-DDTHH:mm:ssZ').fromNow()
+                                                : '-'}
+                                        </span>
+                                        {deploymentStatus === 'inprogress' && <Timer className="icon-dim-16 mt-4" />}
+                                    </div>
+                                    <div className="fw-4 fs-12 cn-9 ellipsis-right" style={{ maxWidth: 'inherit' }}>
+                                        by {triggeredBy || '-'}
+                                    </div>
                                 </div>
                             </div>
-                            <div className="flex left column mw-140">
-                                <div className="fs-12 fw-4 cn-9">Deployment triggered</div>
-                                <div className="flexbox">
-                                    <span className="fs-13 mr-5 fw-6 cn-9">
-                                        {deploymentTriggerTime
-                                            ? moment(deploymentTriggerTime, 'YYYY-MM-DDTHH:mm:ssZ').fromNow()
-                                            : '-'}
-                                    </span>
-                                    {deploymentStatus === 'inprogress' && <Timer className="icon-dim-16 mt-4" />}
-                                </div>
-                                <div className="fw-4 fs-12 cn-9 ellipsis-right" style={{ maxWidth: 'inherit' }}>
-                                    by {triggeredBy || '-'}
-                                </div>
-                            </div>
-                        </div>}
+                        )}
                     </>
                 )}
                 <div style={{ marginLeft: 'auto' }} className="flex right">
