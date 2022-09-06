144ed1b675b2beaa57f361a8b4568804266febc1
shivani170 <shivani@devtron.ai>
2022-09-06 14:56:12 +0530

    FORCE DELETE: break word added for error messaging

---
+++ src/components/common/dialogs/Dialog.type.ts
@@ -0,0 +1,6 @@
+export interface ForceDeleteDialogType {
+    onClickDelete: () => void
+    closeDeleteModal: () => void
+    forceDeleteDialogTitle: string
+    forceDeleteDialogMessage: string
+}
