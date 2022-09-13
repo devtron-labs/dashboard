533b3f9bc52d5ae8fc4e45f6e1a245c1184d2e1a
vivek-devtron <vivek@devtron.ai>
2022-09-13 20:16:20 +0530

    Removed stage url hardcoding

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -189,7 +189,7 @@ export const AppConfigStatus = {
 
 export const FullRoutes = {
     LOGIN: `${Routes.LOGIN}`,
-    CENTRAL: 'https://api-stage.devtron.ai',
+    CENTRAL: 'https://api.devtron.ai',
 }
 
 export const PATTERNS = {
