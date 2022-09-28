f658d717ec570e8b8399fefba4696c6be7b76af8
Sohel Ravankole <sohel@devtron.ai>
2022-09-28 14:19:06 +0530

    Minor changes

--- src/components/cluster/ClusterInfoStepsModal.tsx
+++ src/components/cluster/ClusterInfoStepsModal.tsx
@@ -6,89 +6,89 @@ import { ReactComponent as Help } from '../../assets/icons/ic-help.svg'
 import './cluster.scss'
 import { ReactComponent as ClipboardIcon } from '../../assets/icons/ic-copy.svg'
 import { ClusterStepModal } from './cluster.type'
+import { DOCUMENTATION } from '../../config'
 
-export default function ClusterInfoStepsModal({ subTitle, command, clusterName }: ClusterStepModal) {
+function ClusterInfoSteps({ command, clusterName }: ClusterStepModal) {
     const [copied, setCopied] = useState(false)
     const copyClipboard = (e): void => {
         e.stopPropagation()
         setCopied(true)
         copyToClipboard(command)
     }
-
     const infoItems = [
         {
-            value: (
+            additionalInfo: (
                 <div>
                     <span className="fw-6">Prerequisites:</span> kubectl and Jq should be installed
                 </div>
             ),
         },
         {
-            value: (
-                <>
-                    <div>Run below command on terminal to get server URI & bearer token</div>
-                    <div className="dc__position-rel cluster-modal-container dc__align-left bcn-1 p-10 lh-20 mt-2 br-4">
-                        <div className="fs-13 fw-4 h-100 dc__overflow-scroll" id="command-code">
-                            {command}
-                        </div>
-                        <Tippy
-                            className="default-tt p-4"
-                            arrow={false}
-                            placement="bottom"
-                            content={copied ? 'Copied!' : 'Copy'}
-                            trigger="mouseenter click"
-                            onShow={(_tippy) => {
-                                setTimeout(() => {
-                                    _tippy.hide()
-                                    setCopied(false)
-                                }, 5000)
-                            }}
-                            interactive={true}
-                        >
-                            <div className="cluster-clipboard dc__position-abs cursor" onClick={copyClipboard}>
-                                <ClipboardIcon className="icon-dim-20" />
-                            </div>
-                        </Tippy>
+            info: 'Run below command on terminal to get server URI & bearer token',
+            additionalInfo: (
+                <div className="dc__position-rel cluster-modal-container dc__align-left bcn-1 p-10 lh-20 mt-2 br-4">
+                    <div className="fs-13 fw-4 h-100 dc__overflow-scroll" id="command-code">
+                        {command}
                     </div>
-                </>
+                    <Tippy
+                        className="default-tt p-4"
+                        arrow={false}
+                        placement="bottom"
+                        content={copied ? 'Copied!' : 'Copy'}
+                        trigger="mouseenter click"
+                        onShow={(_tippy) => {
+                            setTimeout(() => {
+                                _tippy.hide()
+                                setCopied(false)
+                            }, 5000)
+                        }}
+                        interactive={true}
+                    >
+                        <div className="cluster-clipboard dc__position-abs cursor" onClick={copyClipboard}>
+                            <ClipboardIcon className="icon-dim-20" />
+                        </div>
+                    </Tippy>
+                </div>
             ),
         },
         {
-            value: 'Copy & paste Server URL & Bearer token from command output',
+            info: 'Copy & paste Server URL & Bearer token from command output',
         },
         {
-            value: `Replace Local IP with public IP at which ${clusterName} cluster api server is accessible`,
+            info: `Replace Local IP with public IP at which ${clusterName} cluster api server is accessible`,
         },
     ]
 
     return (
-        <div className='fs-13 fw-4'>
+        <div className="p-12 fs-13 fw-4">
+            {infoItems.map((item, key) => (
+                <div className="cluster-modal-wrapper ">
+                    <div className="cluster-modal-number flex mr-16 bw-1 bcn-0 en-2 icon-dim-24">{key + 1}</div>
+                    <div
+                        className={`cluster-inner-container flexbox-col dc__border-left pt-2 pr-44 pb-20 pl-28 lh-20 dc__align-start dc__content-start ${
+                            infoItems.length - 1 === key ? 'cluster-last-item' : ''
+                        }`}
+                    >
+                        {item.info && <div>{item.info}</div>}
+                        {item.additionalInfo && item.additionalInfo}
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default function ClusterInfoStepsModal({ subTitle, command, clusterName }: ClusterStepModal) {
+    return (
+        <div className="fs-13 fw-4">
             <h2 className="flex left fs-14 fw-6 p-12 m-0">
                 <Help className="icon-dim-20 fcv-5 mr-12" />
                 Get Server URL & Bearer token
             </h2>
-            <p className="bcn-1 pt-8 pb-8 pl-12 pr-12 m-0">{subTitle}</p>
-            <p className="m-0">
-                <div className="p-12 fs-13 fw-4">
-                    {infoItems.map((item, key) => (
-                        <div className="cluster-modal-wrapper ">
-                            <div className="cluster-modal-number flex mr-16 bw-1 bcn-0 en-2 icon-dim-24">{key + 1}</div>
-                            <div
-                                className={`cluster-inner-container flexbox-col dc__border-left pt-2 pr-44 pb-20 pl-28 lh-20 dc__align-start dc__content-start ${
-                                    infoItems.length === key + 1 ? 'cluster-last-item' : ''
-                                }`}
-                            >
-                                {item.value}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </p>
+            {subTitle && <p className="bcn-1 pt-8 pb-8 pl-12 pr-12 m-0">{subTitle}</p>}
+            <ClusterInfoSteps subTitle={subTitle} command={command} clusterName={clusterName} />
             <div className="p-12">
-                <a
-                    href="https://docs.devtron.ai/getting-started/global-configurations/cluster-and-environments"
-                    target="_blank"
-                >
+                <a href={DOCUMENTATION.CLUSTER_AND_ENVIRONMENTS} target="_blank">
                     View documentation
                 </a>
             </div>
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -266,7 +266,8 @@ export const DOCUMENTATION = {
     DEVTRON_UPGRADE: `${DOCUMENTATION_HOME_PAGE}/v/v0.5/getting-started/upgrade`,
     APP_METRICS: `${DOCUMENTATION_HOME_PAGE}/v/v0.5/usage/applications/app-details/app-metrics`,
     EXTERNAL_SECRET: `${DOCUMENTATION_HOME_PAGE}/v/v0.5/usage/applications/creating-application/secrets#external-secrets`,
-    BLOB_STORAGE: `${DOCUMENTATION_HOME_PAGE}/v/v0.5/getting-started/install/installation-configuration#configuration-of-blob-storage`
+    BLOB_STORAGE: `${DOCUMENTATION_HOME_PAGE}/v/v0.5/getting-started/install/installation-configuration#configuration-of-blob-storage`,
+    CLUSTER_AND_ENVIRONMENTS: `${DOCUMENTATION_HOME_PAGE}/getting-started/global-configurations/cluster-and-environments`,
 }
 
 export const DEVTRON_NODE_DEPLOY_VIDEO = 'https://www.youtube.com/watch?v=9u-pKiWV-tM&t=1s'
