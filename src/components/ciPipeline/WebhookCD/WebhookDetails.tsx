d1af6924b7b9afbd8444bd2d8e6365f977e1dc75
vivek-devtron <vivek@devtron.ai>
2022-10-26 17:10:59 +0530

    minor Ui fix

--- src/components/ciPipeline/WebhookCD/WebhookDetails.tsx
+++ src/components/ciPipeline/WebhookCD/WebhookDetails.tsx
@@ -8,6 +8,7 @@ import { ReactComponent as BotIcon } from '../../../assets/icons/ic-bot.svg'
 import { ReactComponent as PersonIcon } from '../../../assets/icons/ic-person.svg'
 import InfoColourBar from '../../common/infocolourBar/InfoColourbar'
 import { CIPipelineType } from '../types'
+import './webhookDetails.scss'
 
 export function WebhookDetails({ appName, connectCDPipelines, getWorkflows, close, deleteWorkflow }: CIPipelineType) {
     const appStatusDetailRef = useRef<HTMLDivElement>(null)
@@ -42,7 +43,7 @@ export function WebhookDetails({ appName, connectCDPipelines, getWorkflows, clos
     // }, [outsideClickHandler])
     return (
         <Drawer position="right" width="1000px">
-            <div className="dc__window-bg h-100" ref={appStatusDetailRef}>
+            <div className="dc__window-bg h-100 webhook-details-container" ref={appStatusDetailRef}>
                 <div className="flex flex-align-center flex-justify dc__border-bottom bcn-0 pr-20">
                     <h2 className="fs-16 fw-6 lh-1-43 m-0 title-padding">Deploy image from external source</h2>
                     <button
@@ -66,8 +67,17 @@ export function WebhookDetails({ appName, connectCDPipelines, getWorkflows, clos
                         <div className="fw-6 fs-13 cn-9 pb-16">
                             Use API token with below permissions in the cURL request
                         </div>
-                        <div>
+                        <div className="permission-row dc__border-bottom">
                           <span>Project</span>
+                          <span>Environment</span>
+                          <span>Application</span>
+                          <span>Role</span>
+                        </div>
+                        <div className="permission-row">
+                          <span>demo</span>
+                          <span>demo-devtroncd</span>
+                          <span>first-app</span>
+                          <span>Build and deploy</span>
                         </div>
                     </div>
                     <div className="bcn-0 p-16 mb-16 br-4 bw-1 en-2">
