f6c56a94cd01d59861788fe7ee2ba939d5e40b72
vivek-devtron <vivek@devtron.ai>
2022-10-28 13:42:39 +0530

    minor fix added

--- src/components/workflowEditor/nodes/WebhookNode.tsx
+++ src/components/workflowEditor/nodes/WebhookNode.tsx
@@ -1,41 +1,46 @@
 import React, { Component } from 'react'
-import { ReactComponent as Webhook } from '../../../assets/icons/ic-CIWebhook.svg';
+import { Link } from 'react-router-dom'
+import { ReactComponent as Webhook } from '../../../assets/icons/ic-CIWebhook.svg'
+import { ConditionalWrap } from '../../common'
 
 export interface WebhookNodeProps {
-  x: number
-  y: number
-  width: number
-  height: number
-  id: number
-  title: string
-  type: string
-  description: string
-  workflowId?: number
-  triggerType: string
-  isLinkedCI: boolean
-  isExternalCI: boolean
-  isTrigger: boolean
-  linkedCount: number
-  to?: string
-  configDiffView?: boolean
+    x: number
+    y: number
+    width: number
+    height: number
+    id: number
+    title: string
+    type: string
+    description: string
+    workflowId?: number
+    triggerType: string
+    isLinkedCI: boolean
+    isExternalCI: boolean
+    isTrigger: boolean
+    linkedCount: number
+    to?: string
+    configDiffView?: boolean
 }
 
 export class WebhookNode extends Component<WebhookNodeProps> {
-  renderWebhookCard() {
-    return (
-        <div className="workflow-node pl-10">
-            <div className="workflow-node__title flex">
-                <div className="workflow-node__full-width-minus-Icon">
-                    <span className="workflow-node__text-light">
-                       Webhook
-                    </span>
-                    <div className="dc__ellipsis-left">External source</div>
-                </div>
-                <Webhook className="icon-dim-20"/>
+    renderWebhookCard() {
+        return (
+            <div className="workflow-node cursor pl-10">
+                <ConditionalWrap
+                    condition={!!this.props.to}
+                    wrap={(children) => <Link to={this.props.to} className="dc__no-decor">{children}</Link>}
+                >
+                    <div className="workflow-node__title flex">
+                        <div className="workflow-node__full-width-minus-Icon">
+                            <span className="workflow-node__text-light">Webhook</span>
+                            <div className="dc__ellipsis-left">External source</div>
+                        </div>
+                        <Webhook className="icon-dim-20" />
+                    </div>
+                </ConditionalWrap>
             </div>
-        </div>
-    )
-}
+        )
+    }
 
     render() {
         return (
