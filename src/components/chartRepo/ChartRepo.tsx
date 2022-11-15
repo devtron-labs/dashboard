853f9987ce11d4e806cec93dfb942bcd67edfd22
shivani170 <shivani@devtron.ai>
2022-11-15 17:37:17 +0530

    Chart repository name min character from 5 to to 1

--- src/components/chartRepo/ChartRepo.tsx
+++ src/components/chartRepo/ChartRepo.tsx
@@ -134,7 +134,6 @@ function ChartForm({ id = null, name = "", active = false, url = "", authMode = 
                 required: true,
                 validators: [
                   { error: 'Name is required', regex: /^.*$/ },
-                  { error: 'Minimum 1 characters required', regex: /^.{1,}$/ },
               ]
             },
             url: {
