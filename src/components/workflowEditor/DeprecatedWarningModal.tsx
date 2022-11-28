2770eb367f3691261ca66fa55daed2434f0e34e1
vivek-devtron <vivek@devtron.ai>
2022-11-28 21:52:46 +0530

    Review comment fixed

--- src/components/workflowEditor/DeprecatedWarningModal.tsx
+++ src/components/workflowEditor/DeprecatedWarningModal.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { ConfirmationDialog } from '../common'
 import warningIconSrc from '../../assets/icons/ic-warning-y6.svg'
 import { DEPRECATED_EXTERNAL_CI_MESSAGE, DOCUMENTATION } from '../../config'
+import { DeprecatedWarningModalType } from './types'
 
-export default function DeprecatedWarningModal({ closePopup }: { closePopup: () => void }) {
+export default function DeprecatedWarningModal({ closePopup }: DeprecatedWarningModalType) {
     const close = (): void => {
         closePopup()
     }
