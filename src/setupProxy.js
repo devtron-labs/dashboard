a902daf54cf3f8f5dd50f97edad6bb673564a848
Manish Agrawal <manish@devtron.ai>
2022-11-29 11:42:31 +0530

    file revert

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/orchestrator',
         createProxyMiddleware({
-            target: 'http://172.173.174.169:32080/',
+            target: 'http://demo.devtron.info:32080/',
             changeOrigin: true,
             logLevel: 'info',
             secure: false,
