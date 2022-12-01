e74071b09876328bcaebb311710247d376273068
Prakash Kumar <prakash.kumar@devtron.ai>
2022-12-01 13:21:20 +0530

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,7 +5,7 @@ export const DEFAULTK8SVERSION = 'v1.16.0'
 export const TOKEN_COOKIE_NAME = 'argocd.token'
 export const DEVTRON_DEFAULT_RELEASE_NAME = 'devtron'
 export const DEVTRON_DEFAULT_NAMESPACE = 'devtroncd'
-export const DEVTRON_DEFAULT_CLUSTER_ID = 1
+export const DEVTRON_DEFAULT_CLUSTER_ID = '1'
 
 export const Routes = {
     GET: 'get',
