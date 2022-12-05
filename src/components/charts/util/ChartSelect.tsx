373e0a3fcd233b979912918895d095f27c6d5de8
ayushmaheshwari <ayush@devtron.ai>
2022-12-05 16:01:44 +0530

    reverting changes in ChartSelect.tsx

--- src/components/charts/util/ChartSelect.tsx
+++ src/components/charts/util/ChartSelect.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react'
+import React, { useContext } from 'react'
 import Tippy from '@tippyjs/react'
 import { Chart } from '../charts.types'
 import placeHolder from '../../../assets/icons/ic-plc-chart.svg'
@@ -72,8 +72,6 @@ const ChartSelect: React.FC<ChartSelectProps> = ({
     const onClickChartSelect = (): void => {
         onClick(chart.id)
     }
-
-
 
     return (
         <div
