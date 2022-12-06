b8a31bc9feeb856b4b4b15c563c8b06f7ab54d6d
Prakash Kumar <prakash.kumar@devtron.ai>
2022-12-06 11:51:06 +0530

    Update DeleteComponent.tsx

--- src/util/DeleteComponent.tsx
+++ src/util/DeleteComponent.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import { ConfirmationDialog, DeleteDialog, noop, showError } from '../components/common';
 import { ServerErrors } from '../modals/commonTypes';
