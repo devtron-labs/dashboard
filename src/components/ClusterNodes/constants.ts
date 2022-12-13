0fddd120a6265424f7ea774bac71f0d95b40d16a
milindk8 <milind@devtron.ai>
2022-12-13 11:43:16 +0530

    Messaging changes

--- src/components/ClusterNodes/constants.ts
+++ src/components/ClusterNodes/constants.ts
@@ -1,18 +1,18 @@
 export const clusterImages = {
     'quay.io/devtron/ubuntu-k8s-utils:latest': {
         label: 'Ubuntu: Kubernetes utilities',
-        info: 'Ubuntu is a Debian-based Linux distribution.',
+        info: 'kubectl, helm, curl, git, busybox, wget, jq, nslookup, telnet on ubuntu OS',
     },
     'quay.io/devtron/alpine-k8s-utils:latest': {
         label: 'Alpine: Kubernetes utilities',
-        info: 'Alpine Linux contains musl, BusyBox and OpenRC.',
+        info: 'kubectl, helm, curl, git, busybox, wget, jq, nslookup, telnet on alpine OS',
     },
     'quay.io/devtron/centos-k8s-utils:latest': {
         label: 'CentOs: Kubernetes utilities',
-        info: 'CentOS is a Red Hat Enterprise Linux (RHEL) based Linux distribution.',
+        info: 'kubectl, helm, curl, git, busybox, wget, jq, nslookup, telnet on Cent OS',
     },
     'quay.io/devtron/alpine-netshoot:latest': {
         label: 'Alpine: Netshoot',
-        info: 'Busybox comes with many Linux utilities to debug into your cluster.',
+        info: 'Contains Docker + Kubernetes network troubleshooting utilities.',
     },
 }
