f2a40b253c72c841e530d66b0238baea722ad0b7
milindk8 <milind@devtron.ai>
2022-12-13 11:53:27 +0530

    Messaging fix

--- src/components/ClusterNodes/constants.ts
+++ src/components/ClusterNodes/constants.ts
@@ -1,15 +1,15 @@
 export const clusterImages = {
     'quay.io/devtron/ubuntu-k8s-utils:latest': {
         label: 'Ubuntu: Kubernetes utilities',
-        info: 'kubectl, helm, curl, git, busybox, wget, jq, nslookup, telnet on ubuntu OS',
+        info: 'Contains kubectl, helm, curl, git, busybox, wget, jq, nslookup, telnet on ubuntu OS',
     },
     'quay.io/devtron/alpine-k8s-utils:latest': {
         label: 'Alpine: Kubernetes utilities',
-        info: 'kubectl, helm, curl, git, busybox, wget, jq, nslookup, telnet on alpine OS',
+        info: 'Contains kubectl, helm, curl, git, busybox, wget, jq, nslookup, telnet on alpine OS',
     },
     'quay.io/devtron/centos-k8s-utils:latest': {
         label: 'CentOs: Kubernetes utilities',
-        info: 'kubectl, helm, curl, git, busybox, wget, jq, nslookup, telnet on Cent OS',
+        info: 'Contains kubectl, helm, curl, git, busybox, wget, jq, nslookup, telnet on Cent OS',
     },
     'quay.io/devtron/alpine-netshoot:latest': {
         label: 'Alpine: Netshoot',
