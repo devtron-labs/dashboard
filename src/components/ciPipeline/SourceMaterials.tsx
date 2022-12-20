d6d4ec83681254b203f1100cbcc9e807b2e2cf12
Prakash Kumar <prakash.kumar@devtron.ai>
2022-12-20 12:01:07 +0530

    trim whitespaces in git branch name

--- src/components/ciPipeline/SourceMaterials.tsx
+++ src/components/ciPipeline/SourceMaterials.tsx
@@ -183,7 +183,7 @@ export const SourceMaterials: React.FC<SourceMaterialsProps> = function (props) 
                                                 placeholder="Eg. main"
                                                 type="text"
                                                 disabled={!props.handleSourceChange}
-                                                value={mat.value}
+                                                value={mat.value.trim()}
                                                 onChange={(event) => {
                                                     props?.handleSourceChange(
                                                         event,
