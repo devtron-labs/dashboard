53b8a0c42ce9853aec3b070de0e44cb75eb3b69d
Prakash Kumar <prakash.kumar@devtron.ai>
2022-12-21 17:11:25 +0530

    code refactoring

--- src/components/gitOps/GitOpsConfiguration.tsx
+++ src/components/gitOps/GitOpsConfiguration.tsx
@@ -198,7 +198,7 @@ class GitOpsConfiguration extends Component<GitOpsProps, GitOpsState> {
                 [key]: event.target.value.length === 0 ? "This is a required field" : "",
             },
             isFormEdited: false,
-            isUrlValidationError: false
+            isUrlValidationError: key == 'host' ? false : this.state.isUrlValidationError,
         })
     }
 
