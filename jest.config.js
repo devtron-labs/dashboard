6c1547751a2affd95873dfbc9465a4cccb1646b2
Sohel Ravankole <sohel@devtron.ai>
2022-12-22 15:18:09 +0530

    minor changes

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     roots: ['./src'],
     testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
     collectCoverage: true,
-    collectCoverageFrom: ['./src/**/*.{ts,tsx,js,jsx}'],
+    collectCoverageFrom: ['./src/**/*.{ts,tsx,js,jsx}', '!**/node_modules/**', '!**/serviceWorker.ts'],
     coverageReporters: ['html'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     globals: {
@@ -22,6 +22,7 @@ module.exports = {
         '^.+\\.jsx?$': 'babel-jest',
         '^.+\\.(ts|tsx)?$': 'ts-jest',
     },
+    setupFilesAfterEnv: ['jest-extended'],
     // setupFilesAfterEnv: ['./src/setupTests.js'],
     // setupFiles: ['./src/setupTests.js', 'jsdom-worker'],
-};
+}
