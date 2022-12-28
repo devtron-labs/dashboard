bae3c78ddb363f7e5becde76db69bf48fa0b67c3
Sohel Ravankole <sohel@devtron.ai>
2022-12-28 10:02:07 +0530

    added snowstorm script again

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -34,6 +34,16 @@ export default class Login extends Component<LoginProps, LoginFormState> {
         let queryString = new URLSearchParams(this.props.location.search)
         let queryParam = queryString.get('continue')       
         
+        const script = document.createElement("script");
+        script.src = "https://cdn.devtron.ai/js/snowstorm-min.js";
+        script.async = true
+        script.onload = () => {
+            if (window['snowStorm']?.start) {
+                window['snowStorm'].start()
+            }
+        }
+        document.body.appendChild(script);
+
         //1. TOKEN_COOKIE_NAME= 'argocd.token', is the only token unique to a user generated as Cookie when they log in,
             //If a user is still at login page for the first time and getCookie(TOKEN_COOKIE_NAME) becomes false.
             //queryParam is '/' for first time login, queryParam != "/" becomes false at login page. Hence toast won't appear 
