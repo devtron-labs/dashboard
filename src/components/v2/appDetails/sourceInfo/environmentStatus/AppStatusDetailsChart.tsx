58d62334c40276e4b3337b1e11f32f99bac9be40
milindk8 <milind@devtron.ai>
2022-12-30 18:56:35 +0530

    text change

--- src/components/v2/appDetails/sourceInfo/environmentStatus/AppStatusDetailsChart.tsx
+++ src/components/v2/appDetails/sourceInfo/environmentStatus/AppStatusDetailsChart.tsx
@@ -120,7 +120,7 @@ export default function AppStatusDetailsChart({ appStreamData, filterRemoveHealt
                         <div className="flex dc__height-inherit mh-300">
                             <div className="dc__align-center">
                                 <InfoIcon className="icon-dim-20" />
-                                <div>Checking resource status</div>
+                                <div>Checking resources status</div>
                             </div>
                         </div>
                     )}
