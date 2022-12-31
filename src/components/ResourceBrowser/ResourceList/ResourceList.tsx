395161d8847e2810b6aa34bd000f43f4c5521164
vivek-devtron <vivek@devtron.ai>
2022-12-31 17:34:30 +0530

    Cluster selectionlist added

--- src/components/ResourceBrowser/ResourceList/ResourceList.tsx
+++ src/components/ResourceBrowser/ResourceList/ResourceList.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { useHistory, useParams } from 'react-router'
-import { Progressing, showError } from '../../common'
+import { useHistory, useParams, useLocation } from 'react-router'
+import { convertToOptionsList, Progressing, showError } from '../../common'
 import PageHeader from '../../common/header/PageHeader'
 import { GVKType, K8SObjectType, ResourceDetail, resourceListPayloadType } from '../Types'
-import { getResourceGroupList, getResourceList } from '../ResourceBrowser.service'
+import { getResourceGroupList, getResourceList, namespaceListByClusterId } from '../ResourceBrowser.service'
 import { OptionType } from '../../app/types'
 import { ALL_NAMESPACE_OPTION } from '../Constants'
 import { URLS } from '../../../config'
@@ -11,6 +11,8 @@ import { getAggregator } from '../../app/details/appDetails/utils'
 import { Sidebar } from './Sidebar'
 import { K8SResourceList } from './K8SResourceList'
 import '../ResourceBrowser.scss'
+import { ClusterSelectionComponent } from './ClusterSelectionComponent'
+import { getClusterListMinWithoutAuth } from '../../../services/service'
 
 export default function ResourceList() {
     const { clusterId, namespace, kind, node } = useParams<{
@@ -19,7 +21,7 @@ export default function ResourceList() {
         kind: string
         node: string
     }>()
-    const { replace } = useHistory()
+    const { replace, push } = useHistory()
     const [loader, setLoader] = useState(false)
     const [resourceListLoader, setResourceListLoader] = useState(false)
     const [noResults, setNoResults] = useState(false)
@@ -27,10 +29,16 @@ export default function ResourceList() {
     const [k8SObjectListIndexMap, setK8SObjectListIndexMap] = useState<Map<string, number>>()
     const [resourceList, setResourceList] = useState<ResourceDetail[]>([])
     const [filteredResourceList, setFilteredResourceList] = useState<ResourceDetail[]>([])
+    const [clusterOptions, setClusterOptions] = useState<OptionType[]>()
+    const [namespaceOptions, setNamespaceOptions] = useState<OptionType[]>()
     const [selectedCluster, setSelectedCluster] = useState<OptionType>(null)
     const [selectedNamespace, setSelectedNamespace] = useState<OptionType>(null)
     const [selectedResource, setSelectedResource] = useState(kind || '')
     const [selectedGVK, setSelectedGVK] = useState<GVKType>(null)
+
+    useEffect(() => {
+        getClusterList()
+    }, [])
 
     useEffect(() => {
         if (clusterId) {
@@ -42,12 +50,41 @@ export default function ResourceList() {
         if (selectedGVK) {
             getResourceListData()
         }
-    }, [selectedGVK])
+    }, [selectedGVK, namespace])
+
+    const getClusterList = async () => {
+        try {
+            setLoader(true)
+            const { result } = await getClusterListMinWithoutAuth()
+            const _clusterOptions = convertToOptionsList(result, null, 'cluster_name', 'id')
+            setClusterOptions(_clusterOptions)
+            const _selectedCluster = _clusterOptions.find((cluster) => cluster.value == clusterId)
+            if (_selectedCluster) {
+                setSelectedCluster(_selectedCluster || _clusterOptions[0])
+                getNamespaceList(_selectedCluster.value)
+            }
+        } catch (err) {
+            showError(err)
+        } finally {
+            setLoader(false)
+        }
+    }
+
+    const getNamespaceList = async (clusterId: string) => {
+        try {
+            const { result } = await namespaceListByClusterId(clusterId)
+            const _namespaceOptions = [ALL_NAMESPACE_OPTION, ...convertToOptionsList(result)]
+            setNamespaceOptions(convertToOptionsList(result))
+            setSelectedNamespace(_namespaceOptions[0])
+        } catch (err) {
+            showError(err)
+        }
+    }
 
     const getSidebarData = async (): Promise<void> => {
         try {
             setLoader(true)
-            const { result: result1 } = await getResourceGroupList('1')
+            const { result: result1 } = await getResourceGroupList(clusterId)
             const _k8SObjectListIndexMap = new Map<string, number>()
             const _k8SObjectList = []
             let _selectedGVK
@@ -128,37 +165,56 @@ export default function ResourceList() {
         setK8SObjectList(_k8SObjectList)
     }
 
+    const onChangeCluster = (selected, fromClusterSelect?: boolean): void => {
+        setSelectedCluster(selected)
+        getNamespaceList(selected.value)
+        const path = `${URLS.RESOURCE_BROWSER}/${selected.value}/${ALL_NAMESPACE_OPTION.value}`
+        if (fromClusterSelect) {
+            replace({
+                pathname: path,
+            })
+        } else {
+            push({
+                pathname: path,
+            })
+        }
+    }
+
     if (loader) {
         return <Progressing pageLoader />
-    } else if (!clusterId) {
-        return null
     }
 
     return (
-        <div className="bcn-0">
+        <div>
             <PageHeader headerName="Kubernetes object browser" />
-            <div className="resource-browser-container">
-                <Sidebar
-                    k8SObjectList={k8SObjectList}
-                    clusterId={clusterId}
-                    namespace={selectedNamespace?.value}
-                    selectedResource={selectedResource}
-                    setSelectedResource={setSelectedResource}
-                    handleGroupHeadingClick={handleGroupHeadingClick}
-                    setSelectedGVK={setSelectedGVK}
-                />
-                <K8SResourceList
-                    resourceList={resourceList}
-                    filteredResourceList={filteredResourceList}
-                    setFilteredResourceList={setFilteredResourceList}
-                    noResults={noResults}
-                    selectedCluster={selectedCluster}
-                    setSelectedCluster={setSelectedCluster}
-                    selectedNamespace={selectedNamespace}
-                    setSelectedNamespace={setSelectedNamespace}
-                    resourceListLoader={resourceListLoader}
-                />
-            </div>
+            {!selectedCluster?.value ? (
+                <ClusterSelectionComponent clusterOptions={clusterOptions} onChangeCluster={onChangeCluster} />
+            ) : (
+                <div className="resource-browser-container bcn-0">
+                    <Sidebar
+                        k8SObjectList={k8SObjectList}
+                        clusterId={clusterId}
+                        namespace={selectedNamespace?.value || namespace}
+                        selectedResource={selectedResource}
+                        setSelectedResource={setSelectedResource}
+                        handleGroupHeadingClick={handleGroupHeadingClick}
+                        setSelectedGVK={setSelectedGVK}
+                    />
+                    <K8SResourceList
+                        resourceList={resourceList}
+                        filteredResourceList={filteredResourceList}
+                        setFilteredResourceList={setFilteredResourceList}
+                        noResults={noResults}
+                        clusterOptions={clusterOptions}
+                        selectedCluster={selectedCluster}
+                        onChangeCluster={onChangeCluster}
+                        namespaceOptions={namespaceOptions}
+                        selectedNamespace={selectedNamespace}
+                        setSelectedNamespace={setSelectedNamespace}
+                        resourceListLoader={resourceListLoader}
+                    />
+                </div>
+            )}
         </div>
     )
 }
