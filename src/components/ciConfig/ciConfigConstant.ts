7be8df65b2160dc9bd6b53d9b0d0a125426e9b79
Gireesh Naidu <gireesh@devtron.ai>
2023-01-10 16:48:13 +0530

    Minor Ui changes

--- src/components/ciConfig/ciConfigConstant.ts
+++ src/components/ciConfig/ciConfigConstant.ts
@@ -1,5 +1,6 @@
 export const SelectorMessaging = {
      WARNING_WITH_NO_TARGET: 'You have entered a custom target platform, please ensure it is valid.',
+     WARNING_WITH_USING_NO_TARGET:'You are using a custom target platform, please ensure it is valid.',
      PALTFORM_DESCRIPTION: 'If target platform is not set, Devtron will build image for architecture and operating system of the k8s node on which CI is running',
      PALTFORM_DESCRIPTION_WITH_NO_TARGET: 'Target platform is not set',
      TARGET_SELECTOR_MENU: ' Type to enter a target platform. Press Enter to accept.'
