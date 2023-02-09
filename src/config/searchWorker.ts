52f24d9a898cb9caaef19cc52797579ff6af76d3
Sohel Ravankole <sohel@devtron.ai>
2023-02-09 13:17:36 +0530

    addded debounced search

--- src/config/searchWorker.ts
+++ src/config/searchWorker.ts
@@ -1,20 +1,33 @@
 /* eslint-disable no-restricted-globals */
 export default () => {
+    function debounceSearch(callback: (...args: any[]) => void) {
+        let timeout
+        return (...args: any[]): void => {
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+                callback.apply(self, args)
+            }, 300)
+        }
+    }
+
+    function getFilteredList({ searchText, list, searchInKeys }) {
+        const searchTextLowerCased = searchText.toLowerCase()
+        const filteredList = []
+        for (let i = 0; i < list.length; i++) {
+            for (let j = 0; j < searchInKeys.length; j++) {
+                if (list[i][searchInKeys[j]]?.toLowerCase().includes(searchTextLowerCased)) {
+                    filteredList.push(list[i])
+                    break
+                }
+            }
+        }
+        self.postMessage(filteredList)
+    }
+
     self.addEventListener('message', (e) => {
-        const { searchText, list, searchInKeys } = e.data.payload
         switch (e.data.type) {
             case 'start':
-                const searchTextLowerCased = searchText.toLowerCase()
-                const filteredList = []
-                for (let i = 0; i < list.length; i++) {
-                    for (let j = 0; j < searchInKeys.length; j++) {
-                        if (list[i][searchInKeys[j]]?.toLowerCase().includes(searchTextLowerCased)) {
-                            filteredList.push(list[i])
-                            break
-                        }
-                    }
-                }
-                self.postMessage(filteredList)
+                debounceSearch(getFilteredList)(e.data.payload)
                 break
             case 'stop':
                 self.postMessage([])
