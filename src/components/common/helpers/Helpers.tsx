b10172b54ed17e63c9dc2f1456d306da664b7c68
dhananjay0601 <dhananjay@devtron.ai>
2023-02-16 13:31:41 +0530

    removal of unwanted code

--- src/components/common/helpers/Helpers.tsx
+++ src/components/common/helpers/Helpers.tsx
@@ -14,7 +14,7 @@ import { ClusterImageList, ImageList } from '../../ClusterNodes/types'
 import { ApiResourceGroupType, K8SObjectType } from '../../ResourceBrowser/Types'
 import { getAggregator } from '../../app/details/appDetails/utils'
 import { SIDEBAR_KEYS } from '../../ResourceBrowser/Constants'
-import { cursorTo } from 'readline'
+
 const commandLineParser = require('command-line-parser')
 
 export type IntersectionChangeHandler = (entry: IntersectionObserverEntry) => void
