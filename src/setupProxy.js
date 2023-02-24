d48c2b8cb1a982459d073d61aefe1e2f6d7bc6cb
Shivani Bhatt <61836271+shivani170@users.noreply.github.com>
2023-02-24 15:33:16 +0530

    set up proxy fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/orchestrator',
         createProxyMiddleware({
-            target: 'http://20.241.250.55:30425/',
+            target: 'http://demo.devtron.info:32080/',
             changeOrigin: true,
             logLevel: 'info',
             secure: false,
