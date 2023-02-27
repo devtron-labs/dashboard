2d854dca20e45b738eecb3266817f9de6cf5bddc
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2023-02-27 11:40:30 +0530

    Put a check on last run status.

--- src/components/app/Overview/Overview.tsx
+++ src/components/app/Overview/Overview.tsx
@@ -345,10 +345,10 @@ export default function AppOverview({ appMetaInfo, getAppMetaInfoRes, isJobOverv
                 return (
                     <div className="env-deployments-info-wrapper w-100">
                         <div className=" flex dc__border-bottom-n1 dc__uppercase fs-12 fw-6 cn-7 dc__content-space">
-                            <div className="m-tb-3">Pipeline name</div>
+                            <div className="m-tb-8">Pipeline name</div>
                             <div className="flex">
-                                <div className="m-tb-3 mr-16 w-150">Last run status</div>
-                                <div className="w-150 m-tb-3">Last run at</div>
+                                <div className="m-tb-8 mr-16 w-150">Last run status</div>
+                                <div className="w-150 m-tb-8">Last run at</div>
                             </div>
                         </div>
                         {jobPipelines.map((jobPipeline) => (
@@ -356,11 +356,11 @@ export default function AppOverview({ appMetaInfo, getAppMetaInfoRes, isJobOverv
                                 key={jobPipeline.ci_pipeline_id}
                                 className="dc__content-space flex dc__border-bottom-n1"
                             >
-                                <div className="h-20 m-tb-3 ci-pipeline-name-color fs-13">
+                                <div className="h-20 m-tb-8 ci-pipeline-name-color fs-13">
                                     {jobPipeline.ci_pipeline_name}
                                 </div>
                                 <div className="flex">
-                                    <div className="mr-16 w-150 h-20 m-tb-3 fs-13 flex dc__content-start">
+                                    <div className="mr-16 w-150 h-20 m-tb-8 fs-13 flex dc__content-start">
                                         {jobPipeline.status === 'Succeeded' && (
                                             <SucceededIcon className="dc__app-summary__icon icon-dim-20 mr-8" />
                                         )}
@@ -375,7 +375,7 @@ export default function AppOverview({ appMetaInfo, getAppMetaInfoRes, isJobOverv
                                         )}
                                         {jobPipeline.status}
                                     </div>
-                                    <div className="w-150 h-20 m-tb-3 fs-13">
+                                    <div className="w-150 h-20 m-tb-8 fs-13">
                                         {jobPipeline.started_on !== '0001-01-01T00:00:00Z' &&
                                             handleUTCTime(jobPipeline.started_on, true)}
                                     </div>
