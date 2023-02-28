d7c53ac80f5be9f734cdf689588d66939c039f27
milindk8 <milind@devtron.ai>
2023-02-28 22:20:01 +0530

    removed extra empty bracket

--- src/components/app/details/triggerView/workflow/nodes/triggerCINode.tsx
+++ src/components/app/details/triggerView/workflow/nodes/triggerCINode.tsx
@@ -60,12 +60,10 @@ export class TriggerCINode extends Component<TriggerCINodeProps> {
                     {this.props.status && this.props.status.toLowerCase() === 'cancelled'
                         ? 'ABORTED'
                         : this.props.status}
-                    <>
                         <span className="mr-5 ml-5">/</span>
                         <Link to={url} className="workflow-node__details-link">
                             Details
                         </Link>
-                    </>
                 </div>
             )
     }
