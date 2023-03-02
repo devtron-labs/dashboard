8cf2d196733fbfa3649449d7dcf2585869edcea9
Shivani Bhatt <61836271+shivani170@users.noreply.github.com>
2023-03-02 15:49:56 +0530

    eip added mock test

--- src/components/app/details/triggerView/__tests__/triggerview.test.tsx
+++ src/components/app/details/triggerView/__tests__/triggerview.test.tsx
@@ -3,7 +3,9 @@ import TriggerView from '../TriggerView'
 import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
-
+import { cdTriggerResponse } from '../__mocks__/trigger.view.mock'
+import { getCDMaterialList } from '../../../service'
+import { DeploymentNodeType } from '../types'
 describe('Trigger View', () => {
     it('Render Trigger View', () => {
         const { container } = render(<TriggerView />, {
@@ -11,4 +13,18 @@ describe('Trigger View', () => {
         })
         expect(container).toBeInTheDocument()
     })
+})
+
+describe('cd trigger view service tests', () => {
+  test('trigger view git material list response', () => {
+    console.log( getCDMaterialList(
+      "297", DeploymentNodeType.CD
+      ))
+      expect(
+        getCDMaterialList(
+          "297", DeploymentNodeType.CD
+          ),
+      ).toStrictEqual(cdTriggerResponse)
+
+  })
 })
