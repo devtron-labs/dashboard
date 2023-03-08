b560006610cdca094873a06e369e523ddcac4ac8
adi6859 <aditya.ar1909@gmail.com>
2023-03-08 22:57:09 +0530

    errorscreennotauthorized restructured

--- src/components/common/errorScreens/error.tsx
+++ src/components/common/errorScreens/error.tsx
@@ -20,9 +20,6 @@ export class ErrorScreenManager extends Component<{
             case 401:
                 return 'Unauthorized'
             case 403:
-                if (UserGroupRoute  ) {
-                    return <ErrorScreenNoAccess />
-                }
                 return <ErrorScreenNotAuthorized /> 
             case 404:
                 return <ErrorScreenNotFound />
@@ -56,26 +53,18 @@ export class ErrorScreenNotAuthorized extends Component<{}> {
                 <EmptyState.Title>
                     <h3 className="title">{ERROR_EMPTY_SCREEN.NOT_AUTHORIZED}</h3>
                 </EmptyState.Title>
-
-                <EmptyState.Subtitle>{ERROR_EMPTY_SCREEN.ONLY_FOR_SUPERADMIN}</EmptyState.Subtitle>
+                {UserGroupRoute ? (
+                     <EmptyState.Title>
+                     <h3 className="title">Access Denied</h3>
+                    <EmptyState.Subtitle>
+                        Looks like you don't have access to information on this page. Please contact your manager to
+                        request access.
+                    </EmptyState.Subtitle>
+                    </EmptyState.Title>
+                ) : (
+                    <EmptyState.Subtitle>{ERROR_EMPTY_SCREEN.ONLY_FOR_SUPERADMIN}</EmptyState.Subtitle>
+                )}
             </EmptyState>
         )
     }
 }
-
-export class ErrorScreenNoAccess extends Component<{}> {
-    render() {
-        return (
-            <EmptyState className="dc__align-reload-center">
-                <EmptyState.Image>
-                    <img src={notAuthorized} alt="Not Authorized" />
-                </EmptyState.Image>
-                <EmptyState.Title>
-                    <h3 className="title">Access Denied</h3>
-                </EmptyState.Title>
-                <EmptyState.Subtitle>Looks like you don't have access to information on this page. Please contact your manager to request access.</EmptyState.Subtitle>
-            </EmptyState>
-        )
-    }
-}
-
