db74e693c85311f6cb5764a768ea394782cba2b7
adi6859 <aditya.ar1909@gmail.com>
2023-03-08 21:49:06 +0530

    forbidden commented in api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -27,7 +27,7 @@ const responseMessages = {
     400: 'Bad Request',
     401: 'Unauthorized',
     402: 'Payment Required',
-    403: 'Forbidden',
+    //403: 'Forbidden',
     404: 'Not Found',
     405: 'Method Not Allowed',
     406: 'Not Acceptable',
