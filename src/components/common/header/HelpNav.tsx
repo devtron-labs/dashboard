57de852fd7aedc91bbae903992e76b5bc67a6fa0
rish2320 <rishabh.bindal@devtron.ai>
2023-03-14 12:16:16 +0530

    formating-help-options-into-single-function

--- src/components/common/header/HelpNav.tsx
+++ src/components/common/header/HelpNav.tsx
@@ -107,6 +107,31 @@ function HelpNav({
         )
     }
 
+    const renderHelpOptions = (helpOptionType): JSX.Element => {
+        return <> {CommonHelpOptions.concat(helpOptionType).map((option,index) => {
+                return (
+                    <Fragment key={option.name}>
+                        <a
+                            key={option.name}
+                            className="dc__no-decor help-card__option help-card__link flex left cn-9"
+                            href={option.link}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            onClick={() => {
+                                onClickHelpOptions(option)
+                            }}
+                        >
+                            <option.icon />
+                            <div className="help-card__option-name ml-12 cn-9 fs-14">{option.name}</div>
+                        </a>
+                        {isEnterprise && index===1 && <div className = "help__enterprise">Enterprise Support</div>}
+                        {option.showSeparator && <div className="help-card__option-separator" />}
+                    </Fragment>
+                )
+            })}
+        </>
+    }
+
     return (
         <div className="dc__transparent-div" onClick={toggleHelpCard}>
             <div className={`help-card pt-4 pb-4 ${className} ${isEnterprise ? `help-grid__feedback` : ''}`}>
@@ -119,49 +144,9 @@ function HelpNav({
                     <GettingStartedIcon />
                     <div className="help-card__option-name ml-12 cn-9 fs-14">Getting started</div>
                 </NavLink>
-                {isEnterprise && CommonHelpOptions.concat(EnterpriseHelpOptions).map((option,index) => {
-                    return (
-                        <Fragment key={option.name}>
-                            <a
-                                key={option.name}
-                                className="dc__no-decor help-card__option help-card__link flex left cn-9"
-                                href={option.link}
-                                target="_blank"
-                                rel="noreferrer noopener"
-                                onClick={() => {
-                                    onClickHelpOptions(option)
-                                }}
-                            >
-                                <option.icon />
-                                <div className="help-card__option-name ml-12 cn-9 fs-14">{option.name}</div>
-                            </a>
-                            {index===1 && <div className = "help__enterprise">Enterprise Support</div>}
-                            {option.showSeparator && <div className="help-card__option-separator" />}
-                        </Fragment>
-                    )
-                })}
+                {isEnterprise && renderHelpOptions(EnterpriseHelpOptions)}
+                {!isEnterprise && renderHelpOptions(NotEnterpriseHelpOptions)}
                 {isEnterprise && renderHelpFeedback()}
-                {!isEnterprise && CommonHelpOptions.concat(NotEnterpriseHelpOptions).map((option,index) => {
-                    return (
-                        <Fragment key={option.name}>
-                            <a
-                                key={option.name}
-                                className="dc__no-decor help-card__option help-card__link flex left cn-9"
-                                href={option.link}
-                                target="_blank"
-                                rel="noreferrer noopener"
-                                onClick={() => {
-                                    onClickHelpOptions(option)
-                                }}
-                            >
-                                <option.icon />
-                                <div className="help-card__option-name ml-12 cn-9 fs-14">{option.name}</div>
-                            </a>
-                            {option.showSeparator && <div className="help-card__option-separator" />}
-                        </Fragment>
-                    )
-                })}
-
                 {serverInfo?.installationType === InstallationType.OSS_HELM && (
                     <div className="help-card__update-option fs-11 fw-6 mt-4">
                         {fetchingServerInfo ? (
