ff09b1128dbdd9c441bf6feb1f8c893a9c9ced55
milindk8 <milind@devtron.ai>
2023-03-17 13:05:36 +0530

    Minor changes

--- src/components/ApplicationGroup/Details/EnvCIDetails/__mock__/EnvCIDetails.mock.ts
+++ src/components/ApplicationGroup/Details/EnvCIDetails/__mock__/EnvCIDetails.mock.ts
@@ -1,14 +1,98 @@
-// import { CIConfigListType } from "../../../AppGroup.types"
+import { MultiValue } from 'react-select'
+import { ResponseType } from '../../../../../services/service.types'
+import { OptionType } from '../../../../app/types'
+import { CIConfigListType } from '../../../AppGroup.types'
 
-// const result = () => {
-//     return 
-// }
+export const filteredData: MultiValue<OptionType> = [
+    {
+        value: '1',
+        label: 'test-1',
+    },
+    {
+        value: '2',
+        label: 'test-2',
+    },
+]
 
-// export async function mockCIList(): Promise<CIConfigListType> {
-//     const response = {
-//         code: 200,
-//         status: 'OK',
-//         result: result(),
-//     }
-//     return response
-// }
+export const ciResult: CIConfigListType = {
+    pipelineList: [
+        {
+            isManual: false,
+            dockerArgs: new Map(),
+            isExternal: false,
+            parentCiPipeline: 0,
+            parentAppId: 0,
+            appId: '1',
+            externalCiConfig: {
+                id: 1,
+                webhookUrl: '',
+                payload: '',
+                accessKey: 'string'
+            },
+            ciMaterial: [
+                {
+                    source: {
+                        type: 'SOURCE_TYPE_BRANCH_FIXED',
+                        value: 'main',
+                        regex: '',
+                    },
+                    gitMaterialId: 1,
+                    id: 1,
+                    gitMaterialName: 'getting-started-nodejs',
+                    isRegex: false,
+                },
+            ],
+            name: 'ci-1-zxju',
+            id: 1,
+            active: true,
+            linkedCount: 0,
+            scanEnabled: false,
+            isDockerConfigOverridden: false,
+            appName: 'test-1',
+        },
+        {
+            isManual: false,
+            isExternal: false,
+            parentCiPipeline: 0,
+            parentAppId: 0,
+            appId: '2',
+            externalCiConfig: {
+                id: 0,
+                webhookUrl: '',
+                payload: '',
+                accessKey: '',
+            },
+            ciMaterial: [
+                {
+                    source: {
+                        type: 'SOURCE_TYPE_BRANCH_FIXED',
+                        value: 'main',
+                        regex: '',
+                    },
+                    gitMaterialId: 2,
+                    id: 2,
+                    gitMaterialName: 'getting-started-nodejs',
+                    isRegex: false,
+                },
+            ],
+            name: 'ci-1-zxju',
+            id: 2,
+            active: true,
+            linkedCount: 0,
+            scanEnabled: false,
+            isDockerConfigOverridden: false,
+            appName: 'test-2',
+        },
+    ],
+    securityModuleInstalled: false,
+    blobStorageConfigured: false,
+}
+
+export async function mockCIList(): Promise<any> {
+    const response = {
+        code: 200,
+        status: 'OK',
+        result: ciResult,
+    }
+    return response
+}
--- src/components/ApplicationGroup/Details/EnvCIDetails/__test__/EnvCIDetails.test.tsx
+++ src/components/ApplicationGroup/Details/EnvCIDetails/__test__/EnvCIDetails.test.tsx
@@ -1,33 +1,36 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
+import { act, render } from '@testing-library/react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import EnvCIDetails from '../EnvCIDetails'
 import { renderWithRouter } from '../../EnvironmentConfig/__test__/ApplicationRoutes.test'
-import { filteredData } from '../../EnvironmentOverview/__mock__/EnvironmentOverview.mock'
+import { ciResult, filteredData, mockCIList } from '../__mock__/EnvCIDetails.mock'
+import * as configData from '../../../AppGroup.service'
 
 describe('EnvCIDetails', () => {
     it('EnvCIDetails render without error', () => {
         const { container } = renderWithRouter(
-            <Route path="application-group/:envId/ci-details/:pipelineId/:triggerId">
+            <Route path="application-group/:envId/ci-details/:pipelineId/:buildId">
                 <EnvCIDetails filteredApps={filteredData} />
             </Route>,
-            { route: 'application-group/4/ci-details/1' },
+            { route: 'application-group/1/ci-details/1' },
         )
         expect(container).toBeInTheDocument()
     })
 
-    // it('EnvCIDetails render without error', () => {
-    //     let component
-    //     jest.spyOn(configData, 'getCIConfigList').mockImplementation(mockCIList)
-    //     const { container } = renderWithRouter(
-    //         <Route path="application-group/:envId/ci-details/:pipelineId/:triggerId">
-    //             <EnvCIDetails filteredApps={filteredData} />
-    //         </Route>,
-    //         { route: 'application-group/4/ci-details/1' },
-    //     )
-    //     expect(container).toBeInTheDocument()
-    //     const loadState = container.querySelector('.ci-details__history')
-    //     expect(loadState).toBeInTheDocument()
-    // })
+    it('EnvCIDetails render without error', async () => {
+        let component
+        jest.spyOn(configData, 'getCIConfigList').mockImplementation(mockCIList)
+        await act(async () => {
+            component = renderWithRouter(
+            <Route path="application-group/:envId/ci-details/:pipelineId/:buildId">
+                <EnvCIDetails filteredApps={filteredData} />
+            </Route>,
+            { route: 'application-group/1/ci-details/1/1' },
+            )
+        })
+        expect(component.container).toBeInTheDocument()
+        const loadState = component.container.querySelector('.ci-details__history')
+        expect(loadState).toBeInTheDocument()
+    })
 })
