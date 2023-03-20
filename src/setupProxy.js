3591648aaee2a137cbd58b00ff31934f77f7fdb6
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2023-03-20 18:50:04 +0530

    fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/orchestrator',
         createProxyMiddleware({
-            target: 'http://20.241.140.189:30690/',
+            target: 'http://demo.devtron.info:32080/',
             changeOrigin: true,
             logLevel: 'info',
             secure: false,
