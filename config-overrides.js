e2f10e678c9bfbe888dce4b605509b34b66068b1
vivek-devtron <vivek@devtron.ai>
2023-03-23 20:01:34 +0530

    rollbacked configs overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,25 +1,12 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 module.exports = function override(config, env) {
     if (!config.plugins) {
-        config.plugins = []
-    }
-    // below lets it work in dev mode too
-    if (config.devServer === undefined) {
-        config.devServer = {
-            writeToDisk: true,
-        }
-    } else {
-        config.devServer.writeToDisk = true
+        config.plugins = [];
     }
     config.plugins.push(
         new MonacoWebpackPlugin({
-            languages: ['yaml', 'json', 'shell'],
-        }),
-        new CopyPlugin({
-            patterns: [{ from: 'node_modules/@devtron-labs/*/dist/assets/*', to: '[name].[ext]' }],
-        }),
-    )
-    return config
+            languages: ['yaml', 'json', 'shell']
+        })
+    );
+    return config;
 }
