78d2c73690bd8bf053daad2f609871b605966f87
vivek-devtron <vivek@devtron.ai>
2023-03-23 18:32:07 +0530

    config override to copy lib images

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,25 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = function override(config, env) {
     if (!config.plugins) {
-        config.plugins = [];
+        config.plugins = []
+    }
+    // below lets it work in dev mode too
+    if (config.devServer === undefined) {
+        config.devServer = {
+            writeToDisk: true,
+        }
+    } else {
+        config.devServer.writeToDisk = true
     }
     config.plugins.push(
         new MonacoWebpackPlugin({
-            languages: ['yaml', 'json', 'shell']
-        })
-    );
-    return config;
+            languages: ['yaml', 'json', 'shell'],
+        }),
+        new CopyPlugin({
+            patterns: [{ from: 'node_modules/@devtron-labs/*/dist/assets/*', to: '[name].[ext]' }],
+        }),
+    )
+    return config
 }
