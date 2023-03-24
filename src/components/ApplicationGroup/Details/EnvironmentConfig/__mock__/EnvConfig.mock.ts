a46d9aa1a15548fd24c4861ffcc937627e7fb995
milindk8 <milind@devtron.ai>
2023-03-24 16:35:00 +0530

    Minor fixes

--- src/components/ApplicationGroup/Details/EnvironmentConfig/__mock__/EnvConfig.mock.ts
+++ src/components/ApplicationGroup/Details/EnvironmentConfig/__mock__/EnvConfig.mock.ts
@@ -1,5 +1,32 @@
 import { ConfigAppListType } from "../../../AppGroup.service"
 
+export const filteredData = [
+    {
+        "value": '19',
+        "label": "testing-app"
+    },
+    {
+        "value": '23',
+        "label": "testing-4"
+    },
+    {
+        "value": '1',
+        "label": "ajay-app"
+    },
+    {
+        "value": '81',
+        "label": "docker-hub-test"
+    },
+    {
+        "value": '101',
+        "label": "aravind-child"
+    },
+    {
+        "value": '374',
+        "label": "prakash-1mar"
+    }
+]
+
 const result = () => {
     return [
         {
