b4690ba230bbcf7b9828f12ddb9be540478bb719
milindk8 <milind@devtron.ai>
2023-03-24 16:35:00 +0530

    Minor fixes

--- src/components/ApplicationGroup/Details/EnvironmentConfig/__test__/EnvConfig.test.tsx
+++ src/components/ApplicationGroup/Details/EnvironmentConfig/__test__/EnvConfig.test.tsx
@@ -3,10 +3,10 @@ import '@testing-library/jest-dom'
 import { act, render } from '@testing-library/react'
 import { BrowserRouter, Route, Router } from 'react-router-dom'
 import EnvConfig from '../EnvConfig'
-import { filteredData } from '../../EnvironmentOverview/__mock__/EnvironmentOverview.mock'
-import  * as configData from '../../../AppGroup.service'
+import * as configData from '../../../AppGroup.service'
 import { createMemoryHistory } from 'history'
-import { mockConfigAppList } from '../__mock__/EnvConfig.mock'
+import { filteredData, mockConfigAppList } from '../__mock__/EnvConfig.mock'
+import { AppContext } from '../../../../common/Contexts/AppContext'
 
 function renderWithRouter(ui, { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {}) {
     return {
@@ -15,10 +15,9 @@ function renderWithRouter(ui, { route = '/', history = createMemoryHistory({ ini
     }
 }
 
-
 describe('EnvConfig', () => {
     it('ApplicationRoutes render without error', () => {
-        const { container } = render(<EnvConfig filteredApps={[]}  />, {
+        const { container } = render(<EnvConfig filteredApps={[]} />, {
             wrapper: BrowserRouter,
         })
         expect(container).toBeInTheDocument()
@@ -27,19 +26,23 @@ describe('EnvConfig', () => {
     })
 
     it('Application render with data', async () => {
-        let component
+        let component,
+            environmentId = 0
+        const setEnvironmentId = jest.fn
         jest.spyOn(configData, 'getConfigAppList').mockImplementation(mockConfigAppList)
         await act(async () => {
             component = renderWithRouter(
-                <Route path="application-group/:envId/edit/:appId/">
-                   <EnvConfig filteredApps={filteredData} />
-                </Route>,
-                { route: 'application-group/4/edit/' },
+                <AppContext.Provider value={{ environmentId, setEnvironmentId }}>
+                    <Route path="application-group/:envId/edit/:appId">
+                        <EnvConfig filteredApps={filteredData} />
+                    </Route>
+                </AppContext.Provider>,
+                { route: 'application-group/4/edit/19' },
             )
         })
         expect(component.container).toBeInTheDocument()
-        // expect(component.container.querySelector('.env-compose')).toBeInTheDocument()
+        expect(component.container.querySelector('.env-compose')).toBeInTheDocument()
+        expect(component.getByText('testing-app')).toBeInTheDocument()
+        expect(component.container.querySelector('.env-compose__main')).toBeInTheDocument()
     })
 })
-
-
