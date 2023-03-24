c684517f682e14202bab401e85527b1052fc5efc
milindk8 <milind@devtron.ai>
2023-03-24 12:11:25 +0530

    Added more cases

--- src/components/ApplicationGroup/List/__tests__/EnvironmentListView.test.tsx
+++ src/components/ApplicationGroup/List/__tests__/EnvironmentListView.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { act, fireEvent, render } from '@testing-library/react'
+import { act, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import EnvironmentsListView from '../EnvironmentListView'
-import { json } from 'stream/consumers'
 import * as data from '../../AppGroup.service'
 import { mockEmptyFetch, mockFetch } from '../__mocks__/EnvironmentList.mock'
+import { renderWithRouter } from '../../Details/EnvironmentConfig/__test__/ApplicationRoutes.test'
 
 describe('EnvironmentList', () => {
 
@@ -34,6 +34,8 @@ describe('EnvironmentList', () => {
         expect(cont.container).toBeInTheDocument()
         const appGroupListContainer = cont.container.querySelector('.dc__overflow-scroll')
         expect(appGroupListContainer).toBeInTheDocument()
+        expect(cont.getAllByText('a')[0]).toBeInTheDocument()
+        expect(cont.getByText('shubham')).toBeInTheDocument()
     })
 
     it('EnvironmentList renders with empty state', async () => {
@@ -50,6 +52,27 @@ describe('EnvironmentList', () => {
         expect(emptyWrapper).toBeInTheDocument()
         const emptyComponent = emptyWrapper.querySelector('.flex.column.empty-state')
         expect(emptyComponent).toBeInTheDocument()
+        expect(cont.getByText('No matching env')).toBeInTheDocument()
+        
+    })
+
+    it('EnvironmentList renders with empty state when ClusterId is available', async () => {
+        let cont
+        jest.spyOn(data, 'getEnvAppList').mockImplementation(mockEmptyFetch)
+        await act(async () => {
+            cont = renderWithRouter(
+                <Route path="application-group/list">
+                    <EnvironmentsListView isSuperAdmin={true} removeAllFilters={jest.fn()} />
+                </Route>,
+                { route: 'application-group/list?cluster=3&offset=0' },
+                )
+            })
+        expect(cont.container).toBeInTheDocument()
+        const emptyWrapper = cont.container.querySelector('.flex.dc__border-top-n1')
+        expect(emptyWrapper).toBeInTheDocument()
+        const emptyComponent = emptyWrapper.querySelector('.flex.column.empty-state')
+        expect(emptyComponent).toBeInTheDocument()
+        expect(cont.getByText('No app groups found')).toBeInTheDocument()
         
     })
 })
