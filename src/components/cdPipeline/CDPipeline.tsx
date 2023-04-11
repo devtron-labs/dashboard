6286a8c2ff70a4b4e0d331754767b2a3aff7392d
dhananjay0601 <dhananjay@devtron.ai>
2023-04-11 11:20:51 +0530

    test-ids of build and deploy

--- src/components/cdPipeline/CDPipeline.tsx
+++ src/components/cdPipeline/CDPipeline.tsx
@@ -1149,6 +1149,7 @@ export default class CDPipeline extends Component<CDPipelineProps, CDPipelineSta
                     <div className="w-50 mr-8">
                         <div className="form__label">Environment*</div>
                         <ReactSelect
+                            menuIsOpen
                             menuPortalTarget={this.state.isAdvanced ? null : document.getElementById('visible-modal')}
                             closeMenuOnScroll={true}
                             isDisabled={!!this.props.match.params.cdPipelineId}
