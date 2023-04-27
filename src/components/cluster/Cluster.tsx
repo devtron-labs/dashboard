0d668d60c611812412e59d22e924b3c36c11863e
dhananjay0601 <dhananjay@devtron.ai>
2023-04-27 16:28:30 +0530

    "WIP"

--- src/components/cluster/Cluster.tsx
+++ src/components/cluster/Cluster.tsx
@@ -136,8 +136,6 @@ export default class ClusterList extends Component<ClusterListProps, any> {
         }
     }
 
-    
-
     initialise() {
         if (this.timerRef) clearInterval(this.timerRef)
         Promise.all([
@@ -557,6 +555,8 @@ function ClusterForm({
     const [saveYamlData, setSaveYamlState] = useState<string>('')
     const [clusterName, setClusterName] = useState([])
     const [errorMessage, setErrorMessage] = useState<string>('')
+    const [userName, setUserName] = useState([])
+    const [dataList, setDataList] = useState<Map<string, string[]>>(new Map())
 
     const { state, disable, handleOnChange, handleOnSubmit } = useForm(
         {
@@ -626,45 +626,53 @@ function ClusterForm({
     )
 
     const saveClustersDetails = (e) => {
-        try{
-            saveClusters(request).then((response) => {
-                console.log("response")
-
-            }, (err) => {
-                console.log("error")
-                if(err.response === 400){
-                    setErrorMessage("Bad request")
-                }
-                else if(err.response === 500){
-                    setErrorMessage("Internal Server error")
-                }
-            })
-        }
-        catch(err){
+        try {
+            saveClusters(request).then(
+                (response) => {
+                    console.log('response')
+                },
+                (err) => {
+                    console.log('error')
+                    if (err.response === 400) {
+                        setErrorMessage('Bad request')
+                    } else if (err.response === 500) {
+                        setErrorMessage('Internal Server error')
+                    }
+                },
+            )
+        } catch (err) {
             showError(err)
         }
     }
 
     const validateClusterDetail = () => {
-        try{
-            validateCluster(request, saveYamlData).then((response)=>{
-                let count = response.result.length()
-                if(response.result > 1){
-                    setClusterName(response.result)   
-                }
-                console.log("response")
-
-            }, (err)=>{
-                // console.log("error")
-                if(err.response === 400){
-                    setErrorMessage("Bad request")
-                }
-                else if(err.response === 500){
-                    setErrorMessage("Internal Server error")
-                }
-            })
-        }
-        catch(err){
+        try {
+            validateCluster(request, saveYamlData).then(
+                (response) => {
+                    const map = response.result
+                    map.forEach((cluster, userInfoObj) => {
+                        const listOfClusters = [...map.keys()]
+                        setClusterName(listOfClusters)
+                        const map1 = userInfoObj
+                        map1.forEach((userName, userNameObj) => {
+                            const map2 = userNameObj
+                            map2.forEach((key, value) => {
+                                const listOfUserName: string[] = [...map2.keys()]
+                                setDataList(new Map(dataList.set(cluster, listOfUserName)))
+                                setErrorMessage(map2.get('errorInConnecting'))
+                            })
+                        })
+                    })
+                },
+                (err) => {
+                    if (err.code === 400) {
+                        setErrorMessage('Bad request')
+                    } else if (err.code === 500) {
+                        setErrorMessage('Internal Server error')
+                    }
+                },
+            )
+        } catch (err) {
             showError(err)
         }
     }
@@ -680,7 +688,6 @@ function ClusterForm({
             e.target.value = DEFAULT_SECRET_PLACEHOLDER
         }
     }
-
 
     const getClusterPayload = () => {
         return {
@@ -1111,7 +1118,7 @@ function ClusterForm({
                     </div>
                     <div>
                         <div className="cluster-create-status__title mb-0">
-                            
+                            Select the cluster you want to update/add.
                         </div>
                     </div>
                     <div className="pb-8">
@@ -1122,12 +1129,12 @@ function ClusterForm({
                             <div>MESSAGE</div>
                             <div></div>
                         </div>
-
+                        
                     </div>
                 </div>
                 {isKubeConfigFile && (
                     <div className="w-100 dc__border-top flex right pb-8 pt-8 dc__position-fixed dc__position-abs dc__bottom-0">
-                        <button className="cta cancel" type="button" onClick={toggleGetCluster} >
+                        <button className="cta cancel" type="button" onClick={toggleGetCluster}>
                             Edit Kubeconfig
                         </button>
                         <button
