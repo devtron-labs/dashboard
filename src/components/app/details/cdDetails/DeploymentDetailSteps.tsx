3d9996ab2e4597322801d5f65f2a22435e8e2a9c
Shivani Bhatt <61836271+shivani170@users.noreply.github.com>
2023-05-04 14:14:15 +0530

    fix for deployment history api polling

--- src/components/app/details/cdDetails/DeploymentDetailSteps.tsx
+++ src/components/app/details/cdDetails/DeploymentDetailSteps.tsx
@@ -10,8 +10,9 @@ import CDEmptyState from './CDEmptyState'
 import mechanicalOperation from '../../../../assets/img/ic-mechanical-operation.svg'
 import { ReactComponent as Arrow } from '../../../../assets/icons/ic-arrow-forward.svg'
 import { DEPLOYMENT_STATUS, DEPLOYMENT_STATUS_QUERY_PARAM, TIMELINE_STATUS, URLS } from '../../../../config'
-import { DeploymentStatusDetailsBreakdownDataType } from '../appDetails/appDetails.type'
+import { DeploymentStatusDetailsBreakdownDataType, DeploymentStatusDetailsType } from '../appDetails/appDetails.type'
 
+let deploymentStatusTimer = null
 export default function DeploymentDetailSteps({
     deploymentStatus,
     deploymentAppType,
@@ -28,39 +29,54 @@ export default function DeploymentDetailSteps({
     const [deploymentStatusDetailsBreakdownData, setDeploymentStatusDetailsBreakdownData] =
         useState<DeploymentStatusDetailsBreakdownDataType>(processDeploymentStatusDetailsData())
 
-    let initTimer = null
-    const getDeploymentDetailStepsData = (): void => {
-        getDeploymentStatusDetail(appId, envId, triggerId, isHelmApps, installedAppVersionHistoryId)
-            .then((deploymentStatusDetailRes) => {
-                const processedDeploymentStatusDetailsData = processDeploymentStatusDetailsData(
-                    deploymentStatusDetailRes.result,
-                )
-                if (processedDeploymentStatusDetailsData.deploymentStatus === DEPLOYMENT_STATUS.INPROGRESS) {
-                    initTimer = setTimeout(() => {
-                        getDeploymentDetailStepsData()
-                    }, 10000)
-                }
-                setDeploymentStatusDetailsBreakdownData(processedDeploymentStatusDetailsData)
-                setDeploymentListLoader(false)
-            })
-            .catch((e) => {
-                setDeploymentListLoader(false)
-            })
-    }
-
     useEffect(() => {
         if (deploymentAppType === DeploymentAppType.helm) {
             history.replace(`${url.replace('deployment-steps', 'source-code')}`)
         }
-        if (deploymentStatus !== 'Aborted') {
+        if (isGitops) {
             getDeploymentDetailStepsData()
         }
+
         return (): void => {
-          if (initTimer) {
-              clearTimeout(initTimer)
-          }
-      }
+            clearDeploymentStatusTimer()
+        }
     }, [installedAppVersionHistoryId])
+
+    const getDeploymentDetailStepsData = (): void => {
+        getDeploymentStatusDetail(appId, envId, triggerId, isHelmApps, installedAppVersionHistoryId).then(
+            (deploymentStatusDetailRes) => {
+                deploymentStatus !== 'Aborted' && processDeploymentStatusData(deploymentStatusDetailRes.result)
+            }
+        )
+        .catch((e) => {
+          setDeploymentListLoader(false)
+      })
+      .finally(()=>{
+        setDeploymentListLoader(false)
+      })
+    }
+    const clearDeploymentStatusTimer = (): void => {
+        if (deploymentStatusTimer) {
+            clearTimeout(deploymentStatusTimer)
+        }
+    }
+
+    const processDeploymentStatusData = (deploymentStatusDetailRes: DeploymentStatusDetailsType): void => {
+        const processedDeploymentStatusDetailsData = processDeploymentStatusDetailsData(deploymentStatusDetailRes)
+        clearDeploymentStatusTimer()
+        // If deployment status is in progress then fetch data in every 10 seconds
+
+        if (processedDeploymentStatusDetailsData.deploymentStatus === DEPLOYMENT_STATUS.INPROGRESS) {
+            deploymentStatusTimer = setTimeout(() => {
+                getDeploymentDetailStepsData()
+            }, 10000)
+        } else {
+            deploymentStatusTimer = setTimeout(() => {
+                getDeploymentDetailStepsData()
+            }, 30000)
+        }
+        setDeploymentStatusDetailsBreakdownData(processedDeploymentStatusDetailsData)
+    }
 
     const redirectToDeploymentStatus = () => {
       isHelmApps
