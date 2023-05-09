1e8bcdec8486c2e7ff4804f7e299ddc8d6891709
Gireesh Naidu <gireesh@devtron.ai>
2023-05-09 19:17:43 +0530

    arrow direction change

--- src/components/ciConfig/CICreateDockerfileOption.tsx
+++ src/components/ciConfig/CICreateDockerfileOption.tsx
@@ -414,7 +414,7 @@ export default function CICreateDockerfileOption({
                             onClick={toggleCollapse}
                             className="icon-dim-26 rotate"
                             data-testid="set-build-context-button"
-                            style={{ ['--rotateBy' as any]: isCollapsed ? '0deg' : '90deg' }}
+                            style={{ ['--rotateBy' as any]: isCollapsed ? '0deg' : '270deg' }}
                         />
                         Set Build context
                     </span>
