00a88d30ec26780e183e590e2572f15aff954a22
Shivani Bhatt <61836271+shivani170@users.noreply.github.com>
2023-05-10 11:18:07 +0530

    dmeo set up proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/orchestrator',
         createProxyMiddleware({
-            target: 'http://172.173.233.198:31768/',
+            target: 'http://demo.devtron.info:32080/',
             changeOrigin: true,
             logLevel: 'info',
             secure: false,
