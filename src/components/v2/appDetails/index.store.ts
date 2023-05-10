99eff136d83ae3370e23d3ce297c1f1ec1cca3c9
Ashish-devtron <ashish.kumar@devtron.ai>
2023-05-10 21:16:00 +0530

    check-for-parent-refs

--- src/components/v2/appDetails/index.store.ts
+++ src/components/v2/appDetails/index.store.ts
@@ -370,14 +370,16 @@ export const reduceKindStatus = (aggregatedStatus: string, newStatus: string) =>
 }
 
 export const getPodsForRootNodeName = (_rootNode: string, _treeNodes: Array<Node>): Array<iNode> => {
-    let root = _treeNodes.filter(_tn => _tn.name.toLowerCase() == _rootNode.toLowerCase()).map(_tn => _tn as iNode)
+    let root = _treeNodes
+        .filter((_tn) => _tn.name.toLowerCase() == _rootNode.toLowerCase() && !_tn.parentRefs)
+        .map((_tn) => _tn as iNode)
     let rootNodes = getiNodesByRootNodeWithChildNodes(_treeNodes, root)
     let pods = [] as Array<iNode>
     while(rootNodes?.length > 0) {
         pods = pods.concat(rootNodes.filter(_n => _n.kind.toLowerCase() == NodeType.Pod.toLowerCase()))
         rootNodes = rootNodes.flatMap(_n => (_n.childNodes ?? []))
     }
-    return [...new Set(pods)]
+    return pods
 }
 
 export default IndexStore;
