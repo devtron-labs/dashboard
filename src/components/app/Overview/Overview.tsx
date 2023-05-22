f76b900391957f1aeeae0ff3ae0c0721f822eb5f
Aravind-2023 <aravind@devtron.ai>
2023-05-22 16:31:41 +0530

    removing codesmell2

--- src/components/app/Overview/Overview.tsx
+++ src/components/app/Overview/Overview.tsx
@@ -284,7 +284,7 @@ export default function AppOverview({ appMetaInfo, getAppMetaInfoRes, isJobOverv
     const renderDeploymentComponent = () => {
 
         if (otherEnvsResult?.[0]?.result?.length > 0) {
-            const sortedOtherEnvsResult=otherEnvsResult[0].result.sort((a,b)=>(a.environmentName>b.environmentName)?1:-1)
+            otherEnvsResult[0].result.sort((a,b)=>(a.environmentName>b.environmentName)?1:-1)
             return (
                 <div className="env-deployments-info-wrapper w-100">
                     <div
@@ -297,7 +297,7 @@ export default function AppOverview({ appMetaInfo, getAppMetaInfoRes, isJobOverv
                     </div>
 
                     <div className="env-deployments-info-body">
-                        {sortedOtherEnvsResult.map(
+                        {otherEnvsResult[0].result.map(
                             (_env, index) =>
                                 !_env.deploymentAppDeleteRequest && (
                                     <div
