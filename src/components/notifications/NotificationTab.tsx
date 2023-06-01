7c2b585174115ffbfb3da949552360b3d4909de0
dhananjay0601 <dhananjay@devtron.ai>
2023-06-01 16:12:29 +0530

    renderPagination call

--- src/components/notifications/NotificationTab.tsx
+++ src/components/notifications/NotificationTab.tsx
@@ -718,15 +718,7 @@ export class NotificationTab extends Component<any, NotificationTabState> {
                 </div>
                 {this.renderOptions()}
                 {this.renderPipelineList()}
-                {this.state.pagination.size > 0 ? (
-                    <Pagination
-                        offset={this.state.pagination.offset}
-                        pageSize={this.state.pagination.pageSize}
-                        size={this.state.pagination.size}
-                        changePage={this.changePage}
-                        changePageSize={this.changePageSize}
-                    />
-                ) : null}
+                {this.renderPagination()}
             </div>
         )
     }
