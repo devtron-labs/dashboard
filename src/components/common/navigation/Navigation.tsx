dc10cca05f6de3efd84a4b66583d7543e1f58f67
Aravind-2023 <aravind@devtron.ai>
2023-06-02 15:16:02 +0530

    changed styling

--- src/components/common/navigation/Navigation.tsx
+++ src/components/common/navigation/Navigation.tsx
@@ -328,8 +328,8 @@ export default class Navigation extends Component<
                                 >
                                     <use href={`${NavSprite}#nav-short-devtron-logo`}></use>
                                 </svg>
-                                <div className="pl-12 pt-10 pt-0">
-                                    <img src={TextLogo} alt="devtron" className="devtron-logo devtron-logo--text pt-10" />
+                                <div className="pl-12">
+                                    <img src={TextLogo} alt="devtron" className="devtron-logo devtron-logo--text" />
                                 </div>
                             </div>
                         </NavLink>
