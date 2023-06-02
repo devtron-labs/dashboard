66d5173590c75db9f5532aee97dde3f93002e7f2
rish2320 <rishabh.bindal@devtron.ai>
2023-06-02 13:46:32 +0530

    minor-change

--- src/components/material/UpdateMaterial.tsx
+++ src/components/material/UpdateMaterial.tsx
@@ -198,7 +198,7 @@ export class UpdateMaterial extends Component<UpdateMaterialProps, UpdateMateria
                         id: this.state.material.id,
                         url: this.state.material.url,
                         checkoutPath: this.state.material.checkoutPath,
-                        filterPattern: ( !window._env_.HIDE_EXCLUDE_INCLUDE_GIT_COMMITS && this.state.material.isExcludeRepoChecked ) ? this.state.material.includeExcludeFilePath 
+                        filterPattern: (!window._env_.HIDE_EXCLUDE_INCLUDE_GIT_COMMITS && this.state.material.isExcludeRepoChecked) ? this.state.material.includeExcludeFilePath 
                             .trim()
                             .split(/\r?\n/)
                             .filter((path) => path.trim()) : [],
