39d5210b6869a5941ede5929929bde3e225720e8
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2023-06-07 10:21:20 +0530

    extracted out the image tag conatiner in a new file

--- src/components/app/details/cicdHistory/Artifacts.tsx
+++ src/components/app/details/cicdHistory/Artifacts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { showError, EmptyState, GenericEmptyState } from '@devtron-labs/devtron-fe-common-lib'
 import { copyToClipboard, importComponentFromFELibrary } from '../../../common'
 import { useParams } from 'react-router'
@@ -9,17 +9,15 @@ import { ReactComponent as OpenInNew } from '../../../../assets/icons/ic-open-in
 import { ReactComponent as Question } from '../../../../assets/icons/ic-help.svg'
 import docker from '../../../../assets/icons/misc/docker.svg'
 import folder from '../../../../assets/icons/ic-folder.svg'
-import Creatable from 'react-select/creatable';
 import noartifact from '../../../../assets/img/no-artifact@2x.png'
-import { ReactComponent as QuestionIcon } from '../../../v2/assets/icons/ic-question.svg'
 import Tippy from '@tippyjs/react'
 import '../cIDetails/ciDetails.scss'
 import { ArtifactType, CIListItemType, CopyTippyWithTextType, HistoryComponentType } from './types'
-import { ReactComponent as EditIcon } from '../../../../assets/icons/ic-pencil.svg'
 import { DOCUMENTATION, TERMINAL_STATUS_MAP } from '../../../../config'
 import { extractImage } from '../../service'
 import { EMPTY_STATE_STATUS } from '../../../../config/constantMessaging'
-import { CustomInput } from '../../../globalConfigurations/GlobalConfiguration'
+import {ImageTagsContainer} from './ImageTags'
+
 
 const ApprovedArtifact = importComponentFromFELibrary && importComponentFromFELibrary('ApprovedArtifact')
 
@@ -177,99 +175,7 @@ const CIProgressView = (): JSX.Element => {
             </EmptyState.Subtitle>
         </EmptyState>
     )
-}
-
-const ImageTagsContainer = ({ description, tagTexts }) => {
-    const [newDescription, setNewDescription] = useState(description)
-    const [isEditing, setIsEditing] = useState(false)
-    const [selectedTags, setSelectedTags] = useState(
-        tagTexts.map((tag) => ({ text: tag.text, isSoftDeleted: tag.isSoftDeleted })),
-    )
-
-    const handleEditClick = () => {
-        setIsEditing(!isEditing)
-    }
-
-    const handleDescriptionChange = (e) => {
-        setNewDescription(e.target.value)
-    }
-
-    const handleCancel = () => {
-        setNewDescription(description)
-        setSelectedTags(tagTexts.map((tag) => ({ text: tag.text, isSoftDeleted: tag.isSoftDeleted })))
-        handleEditClick()
-    }
-
-    const handleTagCreate = (newValue) => {
-        setSelectedTags([...selectedTags, { text: newValue, isSoftDeleted: false }])
-    }
-
-    const creatableRef = useRef(null)
-
-    return (
-        <div>
-            {!isEditing ? (
-                <div className="top mt-8 br-4 image-tags-container" style={{ display: 'flex' }}>
-                    <div className="ml-10" style={{ width: '734px' }}>
-                        <div className="mb-8 mt-8">{description}</div>
-                        <div className="dc__flex-wrap flex left">
-                            {tagTexts.map((tag, index) => (
-                                <ImageTagButton key={index} text={tag.text} isSoftDeleted={tag.isSoftDeleted} />
-                            ))}
-                        </div>
-                    </div>
-                    <EditIcon
-                        className="icon-dim-16 mt-8 ml-10 image-tags-container-edit__icon"
-                        onClick={handleEditClick}
-                    />
-                </div>
-            ) : (
-                <div className="mt-12 dc__border-top-n1 ">
-                    <div className="cn-7 mt-12 flex left">Release tags (eg. v1.0)
-                    <QuestionIcon className="icon-dim-16 fcn-6 ml-4 cursor" />
-                    </div>
-                    <div className="mb-8 mt-6">
-                        <Creatable
-                            placeholder="Type a tag and press enter"
-                            onCreateOption={handleTagCreate}
-                            ref={creatableRef}  
-                        />
-                    </div>
-                    <div className="dc__flex-wrap flex left">
-                        {selectedTags.map((tag, index) => (
-                            <ImageTagButton key={index} text={tag.text} isSoftDeleted={tag.isSoftDeleted} />
-                        ))}
-                    </div>
-                    <div className="cn-7">Comment</div>
-                    <div className="flex left flex-wrap dc__gap-8 w-100 mt-6 mb-12 ">
-                        <textarea
-                            value={newDescription} 
-                            onChange={handleDescriptionChange}
-                            className="flex left flex-wrap dc__gap-8 dc__description-textarea h-90"
-                        />
-                    </div>
-                    <div className="w-100 flex right">
-                        <button className="cta cancel h-32 lh-32" type="button" onClick={handleCancel}>
-                            Cancel
-                        </button>
-                        <button className="cta h-32 lh-32 ml-12" type="button">
-                            Save
-                        </button>
-                    </div>
-                </div>
-            )}
-        </div>
-    )
-}
-
-const ImageTagButton = ({ text, isSoftDeleted }) => {
-    const containerClassName = isSoftDeleted ? 'image-tag-button-soft-deleted mb-8 mr-8' : 'image-tag-button mb-8 mr-8'
-    return (
-        <div className={containerClassName}>
-            <span className="mr-8 ml-8 mt-2 mb-2">{text}</span>
-        </div>
-    )
-}
+} 
 
 export const CIListItem = ({ type, userApprovalMetadata, triggeredBy, children }: CIListItemType) => {
     if (type === 'approved-artifact') {
@@ -295,7 +201,7 @@ export const CIListItem = ({ type, userApprovalMetadata, triggeredBy, children }
             </div>
             {type === 'artifact' && (
                 <ImageTagsContainer
-                    description="The photo shows a team of firefighters standing in front of a building they have just successfully saved."
+                    description="Hello"
                     tagTexts={[
                         { text: 'v4.9.0', isSoftDeleted: false },
                         { text: 'django', isSoftDeleted: true },
---
+++ src/components/app/details/cicdHistory/ImageTags.tsx
@@ -0,0 +1,164 @@
+import React, { useMemo, useRef, useState } from 'react'
+import { ReactComponent as Add } from '../../../../assets/icons/ic-add.svg'
+import Creatable from 'react-select/creatable'
+import { ReactComponent as Close } from '../../../../assets/icons/ic-close.svg'
+import { ReactComponent as QuestionIcon } from '../../../v2/assets/icons/ic-question.svg'
+import { ReactComponent as EditIcon } from '../../../../assets/icons/ic-pencil.svg'
+import { ReactComponent as Redo } from '../../../../assets/icons/ic-arrow-counter-clockwise.svg'
+import { ReactComponent as Minus } from '../../../../assets/icons/ic-minus.svg'
+
+export const ImageTagsContainer = ({ description, tagTexts }) => {
+    const [newDescription, setNewDescription] = useState(description)
+    const [isEditing, setIsEditing] = useState(false)
+    const [selectedTags, setSelectedTags] = useState(
+        tagTexts.map((tag) => ({ text: tag.text, isSoftDeleted: tag.isSoftDeleted })),
+    )
+
+    const CreatableComponents = useMemo(
+        () => ({
+            DropdownIndicator: () => null,
+            IndicatorSeparator: () => null,
+            Menu: () => null,
+        }),
+        [],
+    )
+
+    const handleEditClick = () => {
+        setIsEditing(!isEditing)
+    }
+
+    const handleDescriptionChange = (e) => {
+        setNewDescription(e.target.value)
+    }
+
+    const handleCancel = () => {
+        setNewDescription(description)
+        setSelectedTags(tagTexts.map((tag) => ({ text: tag.text, isSoftDeleted: tag.isSoftDeleted })))
+        handleEditClick()
+    }
+
+    const handleTagCreate = (newValue) => {
+        setSelectedTags([...selectedTags, { text: newValue, isSoftDeleted: false }])
+    }
+
+    const handleTagDelete = (index) => {
+        const updatedTags = [...selectedTags]
+        updatedTags[index].isSoftDeleted = !updatedTags[index].isSoftDeleted
+        setSelectedTags(updatedTags)
+    }
+
+    const creatableRef = useRef(null)
+
+    if (tagTexts.length === 0 && description === '' && !isEditing) {
+        return (
+            <div className="mt-8">
+                <AddImageButton onClick={handleEditClick} />
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            {!isEditing ? (
+                <div className="top mt-8 br-4 image-tags-container" style={{ display: 'flex' }}>
+                    <div className="ml-10" style={{ width: '734px' }}>
+                        <div className="mb-8 mt-8">{description}</div>
+                        <div className="dc__flex-wrap flex left">
+                            {tagTexts.map((tag, index) => (
+                                <ImageTagButton
+                                    key={index}
+                                    text={tag.text}
+                                    isSoftDeleted={tag.isSoftDeleted}
+                                    isEditing={isEditing}
+                                    onClick={() => handleTagDelete(index)}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                    <EditIcon
+                        className="icon-dim-16 mt-8 ml-10 image-tags-container-edit__icon"
+                        onClick={handleEditClick}
+                    />
+                </div>
+            ) : (
+                <div className="mt-12 dc__border-top-n1 ">
+                    <div className="cn-7 mt-12 flex left">
+                        Release tags (eg. v1.0)
+                        <QuestionIcon className="icon-dim-16 fcn-6 ml-4 cursor" />
+                    </div>
+                    <div className="mb-8 mt-6">
+                        <Creatable
+                            placeholder="Type a tag and press enter"
+                            onCreateOption={handleTagCreate}
+                            ref={creatableRef}
+                            components={CreatableComponents}
+                        />
+                    </div>
+                    <div className="dc__flex-wrap flex left">
+                        {selectedTags.map((tag, index) => (
+                            <ImageTagButton
+                                key={index}
+                                text={tag.text}
+                                isSoftDeleted={tag.isSoftDeleted}
+                                isEditing={isEditing}
+                                onClick={() => handleTagDelete(index)}
+                            />
+                        ))}
+                    </div>
+                    <div className="cn-7">Comment</div>
+                    <div className="flex left flex-wrap dc__gap-8 w-100 mt-6 mb-12 ">
+                        <textarea
+                            value={newDescription}
+                            onChange={handleDescriptionChange}
+                            className="flex left flex-wrap dc__gap-8 dc__description-textarea h-90"
+                        />
+                    </div>
+                    <div className="w-100 flex right">
+                        <button className="cta cancel h-32 lh-32" type="button" onClick={handleCancel}>
+                            Cancel
+                        </button>
+                        <button className="cta h-32 lh-32 ml-12" type="button">
+                            Save
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+const ImageTagButton = ({ text, isSoftDeleted, isEditing, onClick }) => {
+    const containerClassName = isSoftDeleted ? 'image-tag-button-soft-deleted mb-8 mr-8' : 'image-tag-button mb-8 mr-8'
+    const IconComponent = isSoftDeleted ? Redo : Minus
+    const [isHovered, setIsHovered] = useState(false)
+    const handleMouseEnter = () => {
+        if (isEditing) {
+            setIsHovered(true)
+        }
+    }
+    const handleMouseLeave = () => {
+        if (isEditing) {
+            setIsHovered(false)
+        }
+    }
+    return (
+        <div className={containerClassName} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <div className="mr-8 ml-8 mt-2 mb-2 flex">
+                {isHovered && isEditing && <IconComponent className="icon-dim-14 mr-2" onClick={onClick} />}
+                {text}
+                {isHovered && isEditing && <Close className="icon-dim-14 mr-2 cn-5" />}
+            </div>
+        </div>
+    )
+}
+
+const AddImageButton = ({ onClick }) => {
+    return (
+        <div className="add-tag-button flex" onClick={onClick}>
+            <div className="lh-16 flex">
+                <Add className="icon-dim-16 cn-6" />
+                <span className="cn-7">Add tags/comment</span>
+            </div>
+        </div>
+    )
+}
