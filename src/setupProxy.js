f465a1df8d05c069c3c750ec83442577cb1d19df
Gireesh Naidu <gireesh@devtron.ai>
2023-06-13 11:27:52 +0530

    revert target url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/orchestrator',
         createProxyMiddleware({
-            target: 'http://13.92.185.94:32081/',
+            target: 'http://demo.devtron.info:32080/',
             changeOrigin: true,
             logLevel: 'info',
             secure: false,
