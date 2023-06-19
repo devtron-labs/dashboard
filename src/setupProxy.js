b0164d3261febbf72ad52f5e33ac537a14cbce27
Gireesh Naidu <gireesh@devtron.ai>
2023-06-19 17:02:28 +0530

    migrated image tagging component to fe-common-lib

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/orchestrator',
         createProxyMiddleware({
-            target: 'http://20.228.137.37:30196/',
+            target: 'http://demo.devtron.info:32080/',
             changeOrigin: true,
             logLevel: 'info',
             secure: false,
