b5765bbb000fd385279d95a9d4c05eeab813d3a1
rish2320 <rishabh.bindal@devtron.ai>
2023-07-12 11:26:00 +0530

    target-url-fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/orchestrator',
         createProxyMiddleware({
-            target: 'http://20.163.244.230:31757/',
+            target: 'http://demo.devtron.info:32080//',
             changeOrigin: true,
             logLevel: 'info',
             secure: false,
