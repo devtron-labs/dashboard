aecfffb112900066e348d01db7b206be638360d9
milindk8 <milind@devtron.ai>
2023-07-19 18:33:09 +0530

    used datatestid

--- src/components/apiTokens/__tests__/ApiTokens.test.tsx
+++ src/components/apiTokens/__tests__/ApiTokens.test.tsx
@@ -1,36 +1,39 @@
-import React from 'react';
+import React from 'react'
 import '@testing-library/jest-dom'
-import { fireEvent, render, screen } from '@testing-library/react';
-import APITokenList from '../APITokenList';
-import { BrowserRouter } from 'react-router-dom';
+import { fireEvent, render, screen } from '@testing-library/react'
+import APITokenList from '../APITokenList'
+import { BrowserRouter } from 'react-router-dom'
 import { tokenList } from '../__mocks__/ApiTokens.mock'
 
 describe('APITokenList', () => {
-  
     it('renders the component', () => {
-      render(<APITokenList tokenList={[]} renderSearchToken={jest.fn()} reload={jest.fn()} />);
-    });
+        render(<APITokenList tokenList={[]} renderSearchToken={jest.fn()} reload={jest.fn()} />)
+    })
 
     it('renders the list of tokens', () => {
-        const { getByText , container} = render(<APITokenList tokenList={tokenList} renderSearchToken={jest.fn()} reload={jest.fn()} /> , {
-            wrapper:BrowserRouter});
-        expect(container).toBeInTheDocument();
-        expect(getByText('Token 1')).toBeInTheDocument();
-        expect(getByText('127.0.0.1')).toBeInTheDocument();
-        expect(getByText('190.160.0.0')).toBeInTheDocument();
-        expect(getByText('Token 2')).toBeInTheDocument();
-
-    });
+        const { getByText, container } = render(
+            <APITokenList tokenList={tokenList} renderSearchToken={jest.fn()} reload={jest.fn()} />,
+            {
+                wrapper: BrowserRouter,
+            },
+        )
+        expect(container).toBeInTheDocument()
+        expect(getByText('Token 1')).toBeInTheDocument()
+        expect(getByText('127.0.0.1')).toBeInTheDocument()
+        expect(getByText('190.160.0.0')).toBeInTheDocument()
+        expect(getByText('Token 2')).toBeInTheDocument()
+    })
 
     it('create token button trigger', () => {
-
-        const reload = jest.fn();
-        const { container , getByText } = render(<APITokenList tokenList={tokenList} renderSearchToken={jest.fn()} reload={reload} /> , {
-            wrapper:BrowserRouter});
-        const generateTokenButton = container.querySelector('.flex.cta.h-32.ml-10') as HTMLElement;
-        expect(generateTokenButton).toBeInTheDocument();
-        fireEvent.click(generateTokenButton);
-
-    });
-});
-
+        const reload = jest.fn()
+        const { getByTestId } = render(
+            <APITokenList tokenList={tokenList} renderSearchToken={jest.fn()} reload={reload} />,
+            {
+                wrapper: BrowserRouter,
+            },
+        )
+        const generateTokenButton = getByTestId('api-token-generate-button') as HTMLElement
+        expect(generateTokenButton).toBeInTheDocument()
+        fireEvent.click(generateTokenButton)
+    })
+})
