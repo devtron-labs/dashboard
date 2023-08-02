71cc76465314fb3e3a1553f5e5ade9298f4963f3
rish2320 <rishabh.bindal@devtron.ai>
2023-08-02 19:12:54 +0530

    minor-redirection-fix

--- src/components/ApplicationGroup/Details/TriggerView/BulkCDTrigger.tsx
+++ src/components/ApplicationGroup/Details/TriggerView/BulkCDTrigger.tsx
@@ -396,6 +396,7 @@ export default function BulkCDTrigger({
                             history={history}
                             location={location}
                             match={match}
+                            isApplicationGroupTrigger={true}
                         />
                     )}
                 </div>
