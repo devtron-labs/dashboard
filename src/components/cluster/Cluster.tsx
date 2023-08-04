0155e475a4fa5669913c430071c893952199dba5
Shivam-nagar23 <shivam@devtron.ai>
2023-08-04 14:38:35 +0530

    Revert "hyperion mode cluster list show"
    This reverts commit d2e76d6751d4c8714650a9272a806d6cd0c4dccc.

--- src/components/cluster/Cluster.tsx
+++ src/components/cluster/Cluster.tsx
@@ -79,7 +79,7 @@ export default class ClusterList extends Component<ClusterListProps, any> {
         if (this.timerRef) clearInterval(this.timerRef)
         Promise.all([
             getClusterList(),
-             window._env_.K8S_CLIENT
+            this.props.serverMode === SERVER_MODE.EA_ONLY || window._env_.K8S_CLIENT
                 ? { result: undefined }
                 : getEnvironmentList(),
         ])
@@ -636,7 +636,7 @@ function Cluster({
                             }}
                         />
                     )}
-                    {
+                    {serverMode !== SERVER_MODE.EA_ONLY &&
                     !window._env_.K8S_CLIENT &&
                     Array.isArray(newEnvs) &&
                     newEnvs.length > 1 ? (
