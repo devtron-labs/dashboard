c5eeb2ec59e4e4c81b0ea1e8fb3dfc6dfbc8930b
ashish sonam <ashishsonam@devtron.ai>
2023-08-17 18:59:07 +0530

    prevented unnecessary API calls to timelines and resourceTree on AppDetails page

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/orchestrator',
         createProxyMiddleware({
-            target: 'https://demo.devtron.info/',
+            target: 'http://127.0.0.1:8080/',
             changeOrigin: true,
             logLevel: 'info',
             secure: false,
