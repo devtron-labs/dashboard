b2335605884e8b2ae5473eae7c8c08ff726f6f8b
AbhishekA1509 <abhishek@devtron.ai>
2023-08-22 16:56:38 +0530

    chore: code smell for no key

--- src/components/scopedVariables/Table.tsx
+++ src/components/scopedVariables/Table.tsx
@@ -6,7 +6,7 @@ export const TableList = ({ children, width, headings }: TableListI) => {
         <div className={`scoped-variables-list-container`}>
             <div className="scoped-variables-list-header">
                 {headings?.map((heading, index) => (
-                    <div style={{ width: width[index] }}>
+                    <div style={{ width: width[index] }} key={heading}>
                         <div className="scoped-variables-list-item__heading">{heading}</div>
                     </div>
                 ))}
@@ -21,7 +21,7 @@ export const TableItem = ({ columnsData, width }: TableItemI) => {
     return (
         <div className="scoped-variables-list-item">
             {columnsData?.map((item, index) => (
-                <div style={{ width: width[index] }}>
+                <div style={{ width: width[index] }} key={`${item}-${width}`}>
                     <div className="scoped-variables-list-item__data">{item}</div>
                 </div>
             ))}
