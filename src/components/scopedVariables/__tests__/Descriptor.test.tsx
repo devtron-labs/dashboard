44c72c3c2c9cc9b16c38296e12e591e3a5e3fb2e
AbhishekA1509 <abhishek@devtron.ai>
2023-08-25 18:17:32 +0530

    chore: add test for descriptor

---
+++ src/components/scopedVariables/__tests__/Descriptor.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Descriptor from '../Descriptor'
+import { DEFAULT_TITLE } from '../constants'
+
+describe('Descriptor', () => {
+    it('should show the title', () => {
+        const { getByText } = render(<Descriptor />)
+        expect(getByText(DEFAULT_TITLE)).toBeTruthy()
+    })
+
+    it('should show customized tippy showing the description', () => {
+        const { container } = render(<Descriptor />)
+        const enableTippy = container.querySelector('.descriptor-help-button')
+        fireEvent.click(enableTippy!)
+        expect(container.querySelector('.tippy-box')).toBeTruthy()
+        expect(container.querySelector('.tippy-box .tippy-content')).toBeTruthy()
+    })
+
+    it('show show upload button when showUploadButton is true', () => {
+        const { container } = render(<Descriptor showUploadButton={true} />)
+        expect(container.querySelector('.descriptor-container__upload-button')).toBeTruthy()
+    })
+
+    it('should be able to upload a file', () => {
+        const readFile = jest.fn()
+        const { container } = render(<Descriptor showUploadButton={true} readFile={readFile} />)
+        const uploadButton = container.querySelector('.descriptor-container__upload-button')
+        fireEvent.click(uploadButton!)
+        const input = container.querySelector('input')
+        fireEvent.change(input!, { target: { files: [new File([''], 'filename.txt', { type: 'text/plain' })] } })
+        expect(readFile).toHaveBeenCalled()
+    })
+})
--- src/components/scopedVariables/__tests__/index.test.tsx
+++ src/components/scopedVariables/__tests__/index.test.tsx
@@ -14,31 +14,31 @@ jest.mock('../../CodeEditor/CodeEditor', () => jest.fn(() => null))
 
 describe('When ScopedVariables is mounted', () => {
     it('should show Not Authorized screen when user is not super admin', () => {
-        ;(useAsync as jest.Mock).mockReturnValue([null, null, null, null])
+        (useAsync as jest.Mock).mockReturnValue([null, null, null, null])
         render(<ScopedVariables isSuperAdmin={false} />)
         expect(screen.getByText('Not authorized')).toBeTruthy()
     })
 
     it('should show Progressing page loader when scoped variables are loading', () => {
-        ;(useAsync as jest.Mock).mockReturnValue([true, null, null, null])
+        (useAsync as jest.Mock).mockReturnValue([true, null, null, null])
         render(<ScopedVariables isSuperAdmin={true} />)
         expect(screen.getByTestId('progressing')).toBeTruthy()
     })
 
     it('should show Reload screen when scoped variables are not loaded', () => {
-        ;(useAsync as jest.Mock).mockReturnValue([false, null, null, null])
+        (useAsync as jest.Mock).mockReturnValue([false, null, null, null])
         render(<ScopedVariables isSuperAdmin={true} />)
         expect(screen.getByTestId('reload')).toBeTruthy()
     })
 
     it('should show UploadScopedVariables when scoped variables are not present', () => {
-        ;(useAsync as jest.Mock).mockReturnValue([false, noScopedVariablesData, null, null])
+        (useAsync as jest.Mock).mockReturnValue([false, noScopedVariablesData, null, null])
         const { container } = render(<ScopedVariables isSuperAdmin={true} />)
         expect(container.querySelector('.upload-scoped-variables-card')).toBeTruthy()
     })
 
     it('should show SavedVariablesView when scoped variables are present', () => {
-        ;(useAsync as jest.Mock).mockReturnValue([false, validScopedVariablesData, null, null])
+        (useAsync as jest.Mock).mockReturnValue([false, validScopedVariablesData, null, null])
         const { container } = render(<ScopedVariables isSuperAdmin={true} />)
         expect(container.querySelector('.saved-variables-card')).toBeFalsy()
         expect(container.querySelector('.saved-variables-editor-background')).toBeTruthy()
