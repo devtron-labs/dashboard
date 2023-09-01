3120610b5a894d2f4b6c0144edbad9100a6a3d89
shivani170 <shivani@devtron.ai>
2023-09-01 18:10:35 +0530

    fix for cm cs name selection issue

--- src/components/cdPipeline/cdPipeline.service.ts
+++ src/components/cdPipeline/cdPipeline.service.ts
@@ -105,7 +105,7 @@ export function getConfigMapAndSecrets(appId: string, envId) {
             options:  configmaps.map((configmap) => {
                 return {
                     label: configmap.name,
-                    value: configmap.name,
+                    value: `${configmap.name}-configmap`,
                     type: 'configmaps'
                 }
             })
@@ -114,7 +114,7 @@ export function getConfigMapAndSecrets(appId: string, envId) {
             options:  secrets.map((secret) => {
                 return {
                     label: secret.name,
-                    value: secret.name,
+                    value: `${secret.name}-secret`,
                     type: 'secrets'
                 }
             })
