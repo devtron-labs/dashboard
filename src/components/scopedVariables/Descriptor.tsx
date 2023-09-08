e0378ec47712e0af59e97c63e6897061002cb8ed
AbhishekA1509 <abhishek@devtron.ai>
2023-09-08 11:25:46 +0530

    fix: considering null case for tippy text in descriptor

--- src/components/scopedVariables/Descriptor.tsx
+++ src/components/scopedVariables/Descriptor.tsx
@@ -43,7 +43,7 @@ export default function Descriptor({ children, showUploadButton, readFile, onSea
                             showCloseButton={true}
                             trigger="click"
                             interactive={true}
-                            additionalContent={additonalTippyContent()}
+                            additionalContent={additonalTippyContent && additonalTippyContent()}
                         >
                             <button className="p-0 h-20 dc__no-background dc__no-border dc__outline-none-imp">
                                 <ICHelpOutline className="icon-dim-20" />
