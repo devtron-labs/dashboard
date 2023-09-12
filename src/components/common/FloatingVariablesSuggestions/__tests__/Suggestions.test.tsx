31f30ef64c6f9d2324232ea8f9b5de083c17b9a1
AbhishekA1509 <abhishek@devtron.ai>
2023-09-12 13:16:03 +0530

    test: suggestions from floating variables

---
+++ src/components/common/FloatingVariablesSuggestions/__tests__/Suggestions.test.tsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import Suggestions from '../Suggestions'
+
+// Mocking suggestions items since its already tested
+jest.mock(
+    '../SuggestionItem',
+    () =>
+        function SuggestionItem() {
+            return <div data-testid="suggestion-item" />
+        },
+)
+
+// mocking DebouncedSearch since its already tested
+jest.mock(
+    '../../DebouncedSearch/DebouncedSearch',
+    () =>
+        function DebouncedSearch({ onSearch }: { onSearch: (text: string) => void }) {
+            const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+                onSearch(e.target.value)
+            }
+            return <input data-testid="debounced-search" onChange={handleSearch} />
+        },
+)
+
+// mocking SuggestionsInfo since its already tested
+jest.mock(
+    '../SuggestionsInfo',
+    () =>
+        function SuggestionsInfo() {
+            return <div data-testid="suggestions-info" />
+        },
+)
+
+describe('When Suggestions mounts', () => {
+    beforeEach(() => {
+        jest.clearAllMocks()
+    })
+
+    it('should show all the variables', () => {
+        const { getByTestId } = render(
+            <Suggestions
+                handleDeActivation={null}
+                loading={false}
+                variables={[{ variableName: 'name', shortDescription: 'desc' }]}
+                reloadVariables={null}
+                error={false}
+            />,
+        )
+        expect(getByTestId('suggestion-item')).toBeTruthy()
+    })
+
+    it('should call handleDeActivation on click of deactivate-suggestions', () => {
+        const handleDeActivation = jest.fn()
+        const { getByTestId } = render(
+            <Suggestions
+                handleDeActivation={handleDeActivation}
+                loading={false}
+                variables={[{ variableName: 'name', shortDescription: 'desc' }]}
+                reloadVariables={null}
+                error={false}
+            />,
+        )
+        getByTestId('deactivate-suggestions').click()
+        expect(handleDeActivation).toBeCalled()
+    })
+
+    it('should show loader if loading is true', () => {
+        const { getByTestId } = render(
+            <Suggestions
+                handleDeActivation={null}
+                loading
+                variables={[{ variableName: 'name', shortDescription: 'desc' }]}
+                reloadVariables={null}
+                error={false}
+            />,
+        )
+
+        expect(getByTestId('progressing')).toBeTruthy()
+    })
+
+    it('should show No variables found if there are no variables', () => {
+        render(
+            <Suggestions
+                handleDeActivation={null}
+                loading={false}
+                variables={[]}
+                reloadVariables={null}
+                error={false}
+            />,
+        )
+        expect(screen.getByText('No variables found')).toBeTruthy()
+    })
+
+    it('should show no matching variable found if there are no matching variables', () => {
+        render(
+            <Suggestions
+                handleDeActivation={null}
+                loading={false}
+                variables={[{ variableName: 'name', shortDescription: 'desc' }]}
+                reloadVariables={null}
+                error={false}
+            />,
+        )
+        fireEvent.change(screen.getByTestId('debounced-search'), { target: { value: 'test' } })
+        expect(screen.getByText('No matching variable found')).toBeTruthy()
+    })
+})
