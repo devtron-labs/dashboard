2045265308b1964519ad7df5f6d5dbdb47e74774
dhananjay0601 <dhananjay@devtron.ai>
2023-09-15 14:54:47 +0530

    PR comments

--- src/components/login/SSOLogin.tsx
+++ src/components/login/SSOLogin.tsx
@@ -538,7 +538,7 @@ export default class SSOLogin extends Component<SSOLoginProps, SSOLoginState> {
                         <input
                             type="text"
                             className="form__input"
-                            value={this.state.ssoConfig.url.length === 0 ? `${window.location.origin}/orchestrator` : this.state.ssoConfig.url}
+                            value={this.state.ssoConfig.url || `${window.location.origin}/${process.env.REACT_APP_ORCHESTRATOR_ROOT}`}
                             onChange={this.handleURLChange}
                             data-testid="sso-url-input"
                         />
