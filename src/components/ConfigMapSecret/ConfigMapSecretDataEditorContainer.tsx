a7e38d53da29721392f7dd7ea5b8cbdec8e6127e
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2023-09-20 18:43:55 +0530

    bug fix

--- src/components/ConfigMapSecret/ConfigMapSecretDataEditorContainer.tsx
+++ src/components/ConfigMapSecret/ConfigMapSecretDataEditorContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Info, RadioGroup } from '../common'
 import { KeyValueInput, useKeyValueYaml } from './ConfigMapSecret.components'
 import CodeEditor from '../CodeEditor/CodeEditor'
