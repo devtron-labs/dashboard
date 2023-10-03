4626233dfd38f68d2bf065d7bca67acdf1584fe3
AbhishekA1509 <abhishek@devtron.ai>
2023-10-03 11:28:56 +0530

    chore: migrate ClipboardButton to common

--- src/components/common/ClipboardButton/types.ts
+++
@@ -1,7 +0,0 @@
-export default interface ClipboardProps {
-    content: string
-    copiedTippyText: string
-    duration: number
-    trigger: boolean
-    setTrigger: React.Dispatch<React.SetStateAction<boolean>>
-}
