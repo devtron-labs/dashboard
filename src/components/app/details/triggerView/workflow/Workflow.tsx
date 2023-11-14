bc3bb27bbb43bbc01fdfce7dce3550de73848bc3
AbhishekA1509 <abhishek@devtron.ai>
2023-11-14 22:53:56 +0530

    chore: update workflow bg color

--- src/components/app/details/triggerView/workflow/Workflow.tsx
+++ src/components/app/details/triggerView/workflow/Workflow.tsx
@@ -325,7 +325,7 @@ export class Workflow extends Component<WorkflowProps> {
                     )}
                 </div>
                 {isExternalCiWorkflow && <DeprecatedPipelineWarning />}
-                <div className="workflow__body">
+                <div className="workflow__body bc-n50">
                     <svg x={this.props.startX} y={0} height={this.props.height} width={this.props.width}>
                         {this.renderEdgeList()}
                         {this.renderNodes()}
