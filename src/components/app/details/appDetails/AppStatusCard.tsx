0df7da9725fd2edfd1d7debf2a06cc7c7c0bc40c
Prajjwal Singh <prajjwal@devtron.ai>
2023-11-17 00:01:11 +0530

    Remove unused css className

--- src/components/app/details/appDetails/AppStatusCard.tsx
+++ src/components/app/details/appDetails/AppStatusCard.tsx
@@ -54,7 +54,7 @@ const AppStatusCard = ({ appDetails, status, cardLoading, setDetailed, message }
                             <Question className="icon-dim-16 mt-2" />
                         </Tippy>
                     </div>
-                    <div className="app-details-info-card__top-container__content__commit-text-wrapper flex fs-12 fw-4">
+                    <div className="flex fs-12 fw-4">
                         <div className={`fs-13 fw-6 lh-20 f-${status.toLowerCase()}`}>
                             {isHibernated ? 'Hibernating' : status}
                         </div>
