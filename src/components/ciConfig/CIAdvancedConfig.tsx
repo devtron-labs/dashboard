654b3e4834920e9320922bfc1a4a1c14a9fa8ff0
AbhishekA1509 <abhishek@devtron.ai>
2024-01-02 12:55:55 +0530

    fix: remove duplication of docker args

--- src/components/ciConfig/CIAdvancedConfig.tsx
+++ src/components/ciConfig/CIAdvancedConfig.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react'
 import { ReactComponent as PluginIcon } from '../../assets/icons/ic-plugin.svg'
 import { ReactComponent as Dropdown } from '../../assets/icons/ic-chevron-down.svg'
-import { ReactComponent as Cross } from '../../assets/icons/ic-cross.svg'
-import { ReactComponent as QuestionIcon } from '../v2/assets/icons/ic-question.svg'
-import { ReactComponent as HelpIcon } from '../../assets/icons/ic-help.svg'
 import { CIAdvancedConfigProps } from './types'
-import { TippyCustomized, TippyTheme } from '@devtron-labs/devtron-fe-common-lib'
 import TargetPlatformSelector from './TargetPlatformSelector'
+import DockerArgs from '../CIPipelineN/DockerArgs'
+import { DockerArgsAction, HandleDockerArgsUpdateType } from '../CIPipelineN/types'
 
 export default function CIAdvancedConfig({
     configOverrideView,
@@ -27,113 +25,39 @@ export default function CIAdvancedConfig({
         setIsCollapsed(!isCollapsed)
     }
 
-    const addArg = (e) => {
+    const handleDockerArgsUpdate = ({ action, argData }: HandleDockerArgsUpdateType) => {
         if (updateNotAllowed) {
             return
         }
 
-        setArgs((args) => [{ k: '', v: '', keyError: '', valueError: '' }, ...args])
-    }
+        let _args = [...args]
 
-    const handleArgsChange = (e): void => {
-        const _target = e.currentTarget
-        if (updateNotAllowed || !_target) {
-            return
+        switch (action) {
+            case DockerArgsAction.ADD:
+                _args.unshift({ k: '', v: '', valueError: '', keyError: '' })
+                break
+
+            case DockerArgsAction.UPDATE_KEY:
+                _args[argData.index]['k'] = argData.value
+                break
+
+            case DockerArgsAction.UPDATE_VALUE:
+                _args[argData.index]['v'] = argData.value
+                break
+
+            case DockerArgsAction.DELETE:
+                _args = _args.filter((_, index) => index !== argData.index)
+                break
         }
 
-        const isKey = _target.name === 'arg-key',
-            id = _target.dataset.id,
-            k = isKey ? _target.value : _target.dataset.value,
-            v = isKey ? _target.dataset.value : _target.value
-
-        setArgs((arr) => {
-            arr[id] = { k: k, v: v, keyError: '', valueError: '' }
-            return Array.from(arr)
-        })
+        setArgs(_args)
     }
 
-    const deleteArgs = (e) => {
-        if (updateNotAllowed) {
-            return
-        }
-
-        const argsTemp = [...args]
-        argsTemp.splice(e.currentTarget.dataset.id, 1)
-        setArgs(argsTemp)
-    }
-
-    // FIXME: Another Duplication :/
-    const renderBuildArgs = (isDockerArgsSection?: boolean) => {
-        return (
-            <div>
-                <div className="flex left fs-13 fw-6 mb-8">
-                    {isDockerArgsSection ? 'Docker Build Arguments' : 'Build Env Arguments'}
-                    <TippyCustomized
-                        theme={TippyTheme.white}
-                        className="w-300"
-                        placement="top"
-                        Icon={HelpIcon}
-                        iconClass="fcv-5"
-                        heading={isDockerArgsSection ? 'Docker Build Arguments' : 'Build Env Arguments'}
-                        infoText={`Key/value pair will be appended as
-                                ${
-                                    isDockerArgsSection
-                                        ? ' docker build arguments (--build-args).'
-                                        : ' buildpack env arguments (--env).'
-                                }`}
-                        showCloseButton={true}
-                        trigger="click"
-                        interactive={true}
-                    >
-                        <QuestionIcon className="icon-dim-16 fcn-6 ml-4 cursor" />
-                    </TippyCustomized>
-                </div>
-                {!updateNotAllowed && (
-                    <div className="add-parameter fs-14 mb-8 cb-5 cursor dc__w-fit-content" onClick={addArg}>
-                        <span className="fa fa-plus mr-8"></span>Add{isDockerArgsSection ? ' parameter' : ' argument'}
-                    </div>
-                )}
-                {args &&
-                    args.map((arg, idx) => (
-                        <div className="flexbox justify-space" key={`build-${idx}`}>
-                            <div className="mt-8 w-100">
-                                <input
-                                    data-testid={`build-pack-build-env-key${idx}`}
-                                    name="arg-key"
-                                    className={`w-100 dc__top-radius-4 pl-10 pr-10 pt-6 pb-6 en-2 bw-1 ${
-                                        updateNotAllowed ? 'cursor-not-allowed' : ''
-                                    }`}
-                                    autoComplete="off"
-                                    placeholder="Key"
-                                    type="text"
-                                    value={arg.k}
-                                    data-id={idx}
-                                    data-value={arg.v}
-                                    onChange={handleArgsChange}
-                                    disabled={updateNotAllowed}
-                                />
-                                <textarea
-                                    data-testid={`build-pack-build-env-value${idx}`}
-                                    name="arg-value"
-                                    className={`build__value w-100 dc__bottom-radius-4 dc__no-top-border pl-10 pr-10 pt-6 pb-6 en-2 bw-1 form__textarea ${
-                                        updateNotAllowed ? 'cursor-not-allowed' : ''
-                                    }`}
-                                    value={arg.v}
-                                    data-id={idx}
-                                    data-value={arg.k}
-                                    onChange={handleArgsChange}
-                                    placeholder="Value"
-                                    disabled={updateNotAllowed}
-                                />
-                            </div>
-                            {!updateNotAllowed && (
-                                <Cross data-id={idx} className="icon-dim-24 mt-6 ml-6 cursor" onClick={deleteArgs} />
-                            )}
-                        </div>
-                    ))}
-            </div>
-        )
-    }
+    const mapArgsToDockerArgs = () =>
+        args.map((arg) => ({
+            key: arg.k,
+            value: arg.v,
+        }))
 
     const renderTargetPlatform = () => {
         return (
@@ -155,30 +79,44 @@ export default function CIAdvancedConfig({
     }
 
     return isBuildpackType ? (
-        renderBuildArgs()
+        <DockerArgs
+            args={mapArgsToDockerArgs()}
+            handleDockerArgsUpdate={handleDockerArgsUpdate}
+            fromBuildPack
+            readOnly={updateNotAllowed}
+        />
     ) : (
         <>
-                <div onClick={toggleCollapse} className="flex left cursor mb-20" data-testid="advanced-option-drop-down-button">
-                    <div className="icon-dim-40 mr-16">
-                        <PluginIcon />
-                    </div>
-                    <div>
-                        <div className="fs-14 fw-6 ">Advanced options</div>
-                        <div className="form-row__add-parameters">
-                            <span className="fs-13 fw-4 cn-7">Set target platform for build, Docker build arguments</span>
-                        </div>
-                    </div>
-                    <span className="ml-auto">
-                        <Dropdown
-                            className="icon-dim-32 rotate "
-                            style={{ ['--rotateBy' as any]: isCollapsed ? '180deg' : '0deg' }}
-                        />
-                    </span>
+            <div
+                onClick={toggleCollapse}
+                className="flex left cursor mb-20"
+                data-testid="advanced-option-drop-down-button"
+            >
+                <div className="icon-dim-40 mr-16">
+                    <PluginIcon />
                 </div>
+                <div>
+                    <div className="fs-14 fw-6 ">Advanced options</div>
+                    <div className="form-row__add-parameters">
+                        <span className="fs-13 fw-4 cn-7">Set target platform for build, Docker build arguments</span>
+                    </div>
+                </div>
+                <span className="ml-auto">
+                    <Dropdown
+                        className="icon-dim-32 rotate "
+                        style={{ ['--rotateBy' as any]: isCollapsed ? '180deg' : '0deg' }}
+                    />
+                </span>
+            </div>
             {isCollapsed && (
                 <>
                     {renderTargetPlatform()}
-                    {renderBuildArgs(true)}
+
+                    <DockerArgs
+                        args={mapArgsToDockerArgs()}
+                        handleDockerArgsUpdate={handleDockerArgsUpdate}
+                        readOnly={updateNotAllowed}
+                    />
                 </>
             )}
         </>
