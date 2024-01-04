416637a0d565d35163f626590a9a0aabd05f99b8
Eshank Vaish <eshank@devtron.ai>
2024-01-04 15:07:08 +0530

    add airbnb config to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
-const prettierConfig = require('./.prettierrc')
+// const prettierConfig = require('./.prettierrc')
 
 module.exports = {
     parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'react', 'prettier'],
     env: {
         commonjs: true,
         browser: true,
-        es6: true,
+        // ESLint 6 supports till ES2020 only
+        es2020: true,
     },
     extends: [
         'eslint:recommended',
@@ -13,14 +15,17 @@ module.exports = {
         'plugin:react/jsx-runtime',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/eslint-recommended',
+        'airbnb',
+        'airbnb/hooks',
         'plugin:prettier/recommended',
     ],
     parserOptions: {
-        ecmaVersion: 2018,
+        ecmaVersion: 2020,
         sourceType: 'module',
         ecmaFeatures: {
             jsx: true,
         },
+        project: ['./tsconfig.json'],
     },
     rules: {
         'no-console': 'warn',
@@ -28,14 +33,82 @@ module.exports = {
         'no-duplicate-imports': 'error',
         curly: ['error', 'all'],
         'keyword-spacing': 'error',
-        'prettier/prettier': ['error', prettierConfig],
+        'prettier/prettier': ['error'],
+        'linebreak-style': ['error', 'unix'],
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': 'warn',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        // Since we are using typescript, we can disable the no-unused-vars rule for enum,etc
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        'react/jsx-filename-extension': [
+            'error',
+            {
+                extensions: ['tsx'],
+            },
+        ],
+        'react/jsx-props-no-spreading': 'off',
+        'react/prefer-stateless-function': 'off',
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'jsx-a11y/no-static-element-interactions': 'off',
+        'jsx-a11y/no-noninteractive-element-interactions': 'off',
+        'no-underscore-dangle': 'off',
+        'import/no-extraneous-dependencies': [
+            'warn',
+            {
+                devDependencies: true,
+            },
+        ],
+        'no-plusplus': [
+            'error',
+            {
+                allowForLoopAfterthoughts: true,
+            },
+        ],
+        // Since we dont use prop-types, we can disable this rule
+        'react/require-default-props': 'off',
+        'react-hooks/exhaustive-deps': 'off',
+        'react/function-component-definition': [
+            'warn',
+            {
+                namedComponents: 'arrow-function',
+                unnamedComponents: 'arrow-function',
+            },
+        ],
         // FIXME: Turn this OFF once react-scripts is upgraded or removed
         'react/jsx-uses-react': 'warn',
         'react/react-in-jsx-scope': 'warn',
+        // additional rules:
+        '@typescript-eslint/no-floating-promises': 'error',
+        'import/extensions': [
+            'warn',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
     },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                'no-undef': 'off',
+            },
+        },
+    ],
     settings: {
         react: {
             version: 'detect',
+        },
+        'import/resolver': {
+            node: {
+                moduleDirectory: ['src', 'node_modules'],
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
         },
     },
 }
