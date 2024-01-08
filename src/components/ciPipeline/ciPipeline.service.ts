fb82c9cf1cc8695254bb2d41471b6f14032de114
AbhishekA1509 <abhishek@devtron.ai>
2024-01-08 19:40:06 +0530

    fix: derive triggerType from DEFAULT_CI_TRIGGER_TYPE_MANUAL

--- src/components/ciPipeline/ciPipeline.service.ts
+++ src/components/ciPipeline/ciPipeline.service.ts
@@ -54,7 +54,7 @@ export function getInitData(
                     webhookEvents: pipelineMetaConfig.result.webhookEvents,
                     ciPipelineSourceTypeOptions: pipelineMetaConfig.result.ciPipelineSourceTypeOptions,
                     webhookConditionList: pipelineMetaConfig.result.webhookConditionList,
-                    triggerType: TriggerType.Auto,
+                    triggerType: window._env_.DEFAULT_CI_TRIGGER_TYPE_MANUAL ? TriggerType.Manual : TriggerType.Auto,
                     beforeDockerBuildScripts: [],
                     afterDockerBuildScripts: [],
                     preBuildStage: emptyStepsData(),
