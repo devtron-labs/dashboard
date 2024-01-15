91eeab4e95c704f0a110e0447937302696206b13
AbhishekA1509 <abhishek@devtron.ai>
2024-01-15 12:29:36 +0530

    feat: add descriptor and footer for build-infra

--- src/pages/GlobalConfigurations/BuildInfra/BuildInfra.tsx
+++ src/pages/GlobalConfigurations/BuildInfra/BuildInfra.tsx
@@ -1,14 +1,22 @@
 import React, { FunctionComponent } from 'react'
+import {
+    BUILD_INFRA_BREADCRUMB,
+    BuildInfraDescriptor,
+    BuildInfraFooter,
+    useBreadcrumb,
+} from '@devtron-labs/devtron-fe-common-lib'
+import './styles.scss'
 
 export const BuildInfra: FunctionComponent = () => {
+    const { breadcrumbs } = useBreadcrumb(BUILD_INFRA_BREADCRUMB)
+
     return (
-        <div className="h-100 flexbox-col build-infra pl pr pt pb dc__content-space">
-            <div className="flexbox-col dc__gap-24">
-                {/* TODO: Replace with descriptor */}
-                <h1>Build Infra</h1>
+        <div className="h-100 flexbox-col build-infra pl pr pt pb dc__content-space bcn-0">
+            <div className="flexbox-col dc__gap-24 pt pr pb pl">
+                <BuildInfraDescriptor breadCrumbs={breadcrumbs} />
             </div>
 
-            {/* Import Footer */}
+            <BuildInfraFooter disabled={false} />
         </div>
     )
 }
