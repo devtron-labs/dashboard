ec6a0d3cc03ce81d1e154953df55e93b7d37f4ad
shivani170 <shivani@devtron.ai>
2024-01-17 10:28:59 +0530

    uI for deployment integration

--- src/components/cdPipeline/NewCDPipeline.tsx
+++ src/components/cdPipeline/NewCDPipeline.tsx
@@ -126,6 +126,8 @@ export default function NewCDPipeline({
         repoName: '',
         selectedRegistry: null,
         generatedHelmPushAction: GeneratedHelmPush.DO_NOT_PUSH,
+        isDigestEnforcedForPipeline: false,
+        isDigestEnforcedForEnv: false,
     })
     const [configMapAndSecrets, setConfigMapAndSecrets] = useState([])
     const [savedCustomTagPattern, setSavedCustomTagPattern] = useState<string>('')
@@ -444,6 +446,8 @@ export default function NewCDPipeline({
         form.customTag = pipelineConfigFromRes.customTag
         form.enableCustomTag = pipelineConfigFromRes.enableCustomTag
         form.customTagStage = pipelineConfigFromRes.customTagStage
+        form.isDigestEnforcedForEnv = pipelineConfigFromRes.isDigestEnforcedForEnv
+        form.isDigestEnforcedForPipeline = pipelineConfigFromRes.isDigestEnforcedForPipeline
 
         if (pipelineConfigFromRes?.preDeployStage) {
             if(pipelineConfigFromRes.preDeployStage.steps?.length > 0){
@@ -565,6 +569,8 @@ export default function NewCDPipeline({
             },
             enableCustomTag: formData.enableCustomTag,
             customTagStage: formData?.customTagStage ? formData.customTagStage : StageTypeEnums.PRE_CD,
+            isDigestEnforcedForPipeline: formData.isDigestEnforcedForPipeline,
+            isDigestEnforcedForEnv: formData.isDigestEnforcedForEnv,
         }
 
         if (isVirtualEnvironment) {
---
+++ src/components/cdPipeline/PullImageDigestToggle.tsx
@@ -0,0 +1,48 @@
+import { Toggle } from '@devtron-labs/devtron-fe-common-lib'
+import React from 'react'
+import { ReactComponent as DockerWithImage } from '../../assets/icons/ic-docker-with-image.svg'
+
+function PullImageDigestToggle({ formData, setFormData }) {
+    const handleCustomTagToggle = (): void => {
+        const _formData = { ...formData }
+        _formData.isDigestEnforcedForPipeline = !_formData.isDigestEnforcedForPipeline
+        setFormData(_formData)
+    }
+
+    const renderImageDigestBody = (): JSX.Element => {
+        return (
+            <div className="fs-13">
+                <div className="flex dc__content-space w-100 cursor flex top">
+                    <div
+                        className={`flex ${
+                            !formData.enableCustomTag && formData.customTag?.tagPattern?.length > 0 ? 'top' : ''
+                        }`}
+                    >
+                        <div className="pc-icon-container bcn-1 br-8 mr-16 flexbox">
+                            <DockerWithImage className="icon-dim-24" />
+                        </div>
+                        <div>
+                            <span className="fw-6">Pull container image with image digest</span>
+                            <div className="cn-7 ">
+                                When enabled, image will be pulled with image digest to ensure uniqueness of image.
+                                Learn more
+                            </div>
+                        </div>
+                    </div>
+                    <div className="" style={{ width: '32px', height: '20px' }}>
+                        <Toggle
+                            selected={formData.isDigestEnforcedForPipeline}
+                            onSelect={handleCustomTagToggle}
+                            dataTestId="create-build-pipeline-custom-tag-enabled-toggle"
+                        />
+                    </div>
+                </div>
+                <hr />
+            </div>
+        )
+    }
+
+    return renderImageDigestBody()
+}
+
+export default PullImageDigestToggle
