9563c8a640e9f16ea3b9817daca28d5ef3029469
shivani170 <shivani@devtron.ai>
2024-04-15 12:11:57 +0530

    css fixes

--- src/components/common/ClusterMetaDataBar/ClusterMetaDataBar.tsx
+++ src/components/common/ClusterMetaDataBar/ClusterMetaDataBar.tsx
@@ -8,17 +8,27 @@ import { AppDetailsTabs } from '../../v2/appDetails/appDetails.store'
 import { ReactComponent as ArrowLeft } from '../../../assets/icons/ic-arrow-left.svg'
 import { ReactComponent as AllResourcesIcon } from '../../../assets/icons/ic-resource.svg'
 import { ReactComponent as TerminalIcon } from '../../../assets/icons/ic-terminal-fill.svg'
+import './ClusterMetaData.scss'
 
 export const ClusterMetaDataBar = ({ clusterName, namespace, clusterId }: ClusterMetaDataBarProps) => {
     const { isSuperAdmin } = useMainContext()
     const history = useHistory()
+    const darkTheme =
+        history.location.pathname.includes('manifest') ||
+        history.location.pathname.includes('events') ||
+        history.location.pathname.includes('terminal') ||
+        history.location.pathname.includes('logs') ||
+        history.location.pathname.includes('log-analyzer')
 
     const renderNavigationToAllResources = () => {
         const navigateToAllResources = () => {
             history.push(`${URLS.RESOURCE_BROWSER}/${clusterId}/${namespace}/pod/${K8S_EMPTY_GROUP}`)
         }
         return (
-            <div className="cn-9 fw-6 flex left dc__gap-6 cursor" onClick={navigateToAllResources}>
+            <div
+                className={` ${darkTheme ? 'fcn-0' : ''} fw-6 flex left dc__gap-6 cursor`}
+                onClick={navigateToAllResources}
+            >
                 <AllResourcesIcon />
                 All resources in cluster
             </div>
@@ -32,7 +42,10 @@ export const ClusterMetaDataBar = ({ clusterName, namespace, clusterId }: Cluste
             )
         }
         return (
-            <div className="cn-9 fw-6 flex left dc__gap-6 cursor fcn-9" onClick={navigateToAClusterTerminal}>
+            <div
+                className={` ${darkTheme ? 'fcn-0' : 'fcn-9'} w-6 flex left dc__gap-6 cursor`}
+                onClick={navigateToAClusterTerminal}
+            >
                 <TerminalIcon className="icon-dim-16" />
                 Cluster terminal
             </div>
@@ -43,11 +56,16 @@ export const ClusterMetaDataBar = ({ clusterName, namespace, clusterId }: Cluste
         return null
     }
     return (
-        <div className="flex left dc__position-fixed dc__bottom-0 pl-16 w-100 fs-12 dc__border-top bcn-0 dc__gap-6 pt-4 pb-4 lh-20 cn-9">
+        <div
+            className={`cluster-meta-data-wrapper ${darkTheme ? 'dark-theme cn-0' : 'cn-9 bcn-0'} flex left dc__position-fixed dc__bottom-0 pl-16 w-100 fs-12 dc__border-top dc__gap-6 pt-4 pb-4 lh-20 cn-9`}
+        >
             <span className="dc__opacity-0_8"> Cluster: {clusterName}</span>
             <div className="dc__border-left h-12 dc__opacity-0_2" />
             <span className="pl-6 dc__opacity-0_8">Namespace: {namespace}</span>
-            <ArrowLeft className="rotate dc__gap-6 icon-dim-16 flex" style={{ ['--rotateBy' as string]: '180deg' }} />
+            <ArrowLeft
+                className={`${darkTheme ? 'fcn-0 dc__opacity-0_5' : ''} rotate dc__gap-6 icon-dim-16 flex`}
+                style={{ ['--rotateBy' as string]: '180deg' }}
+            />
             {renderNavigationToAllResources()}
             <div className="dc__border-left h-12 dc__opacity-0_2" />
             {renderNavigationToAClusterTerminal()}
