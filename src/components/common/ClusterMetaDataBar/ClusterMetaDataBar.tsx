ec1c033362966ecbf4650449412b2516be5c67d5
shivani170 <shivani@devtron.ai>
2024-04-15 11:24:36 +0530

    code feedback

--- src/components/common/ClusterMetaDataBar/ClusterMetaDataBar.tsx
+++ src/components/common/ClusterMetaDataBar/ClusterMetaDataBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useHistory } from 'react-router'
+import { useHistory } from 'react-router-dom'
 import { useMainContext } from '@devtron-labs/devtron-fe-common-lib'
 import { ClusterMetaDataBarProps } from './types'
 import { URLS } from '../../../config'
@@ -15,7 +15,7 @@ export const ClusterMetaDataBar = ({ clusterName, namespace, clusterId }: Cluste
 
     const renderNavigationToAllResources = () => {
         const navigateToAllResources = () => {
-            history.push(`${URLS.RESOURCE_BROWSER}/${clusterId}/all/node/${K8S_EMPTY_GROUP}`)
+            history.push(`${URLS.RESOURCE_BROWSER}/${clusterId}/${namespace}/pod/${K8S_EMPTY_GROUP}`)
         }
         return (
             <div className="cn-9 fw-6 flex left dc__gap-6 cursor" onClick={navigateToAllResources}>
@@ -27,12 +27,14 @@ export const ClusterMetaDataBar = ({ clusterName, namespace, clusterId }: Cluste
 
     const renderNavigationToAClusterTerminal = () => {
         const navigateToAClusterTerminal = () => {
-            history.push(`${URLS.RESOURCE_BROWSER}/${clusterId}/all/${AppDetailsTabs.terminal}/${K8S_EMPTY_GROUP}`)
+            history.push(
+                `${URLS.RESOURCE_BROWSER}/${clusterId}/${namespace}/${AppDetailsTabs.terminal}/${K8S_EMPTY_GROUP}`,
+            )
         }
         return (
-            <div className="cn-9 fw-6 flex left dc__gap-6 cursor" onClick={navigateToAClusterTerminal}>
-                <TerminalIcon />
-                Cluster Terminal
+            <div className="cn-9 fw-6 flex left dc__gap-6 cursor fcn-9" onClick={navigateToAClusterTerminal}>
+                <TerminalIcon className="icon-dim-16" />
+                Cluster terminal
             </div>
         )
     }
@@ -41,12 +43,14 @@ export const ClusterMetaDataBar = ({ clusterName, namespace, clusterId }: Cluste
         return null
     }
     return (
-        <div className="flex left dc__position-fixed dc__bottom-0 pl-16 w-100 fs-12 dc__border-top bcn-0 dc__gap-6 pt-4 pb-4 lh-20">
-            <div className="cn-9">
-                Cluster: {clusterName} | Namespace: {namespace}
-            </div>
+        <div className="flex left dc__position-fixed dc__bottom-0 pl-16 w-100 fs-12 dc__border-top bcn-0 dc__gap-6 pt-4 pb-4 lh-20 cn-9">
+            <span className="dc__opacity-0_8"> Cluster: {clusterName}</span>
+            <div className="dc__border-left h-12 dc__opacity-0_2" />
+            <span className="pl-6 dc__opacity-0_8">Namespace: {namespace}</span>
             <ArrowLeft className="rotate dc__gap-6 icon-dim-16 flex" style={{ ['--rotateBy' as string]: '180deg' }} />
-            {renderNavigationToAllResources()} | {renderNavigationToAClusterTerminal()}
+            {renderNavigationToAllResources()}
+            <div className="dc__border-left h-12 dc__opacity-0_2" />
+            {renderNavigationToAClusterTerminal()}
         </div>
     )
 }
