1d009443977b07a84add9a9f4b99018a7e29205d
kishan789dev <kishan.kumar@devtron.ai>
2024-05-10 13:22:54 +0530

    added some data-test-id and some classNamePrefix

--- src/Pages/GlobalConfigurations/Authorization/Shared/components/AppPermissions/DirectPermission.tsx
+++ src/Pages/GlobalConfigurations/Authorization/Shared/components/AppPermissions/DirectPermission.tsx
@@ -335,6 +335,7 @@ const DirectPermission = ({
     return (
         <>
             <Select
+                classNamePrefix="dropdown-for-project"
                 value={permission.team}
                 name={DirectPermissionFieldName.team}
                 isMulti={false}
@@ -366,6 +367,7 @@ const DirectPermission = ({
             {permission.accessType === ACCESS_TYPE_MAP.HELM_APPS ? (
                 <div>
                     <Select
+                        classNamePrefix="dropdown-for-environment"
                         value={permission.environment}
                         isMulti
                         closeMenuOnSelect={false}
@@ -404,6 +406,7 @@ const DirectPermission = ({
             ) : (
                 <div style={{ order: isAccessTypeJob ? 3 : 0 }}>
                     <Select
+                        classNamePrefix="dropdown-for-environment"
                         value={permission.environment}
                         isMulti
                         closeMenuOnSelect={false}
@@ -439,6 +442,7 @@ const DirectPermission = ({
             )}
             <div style={{ order: isAccessTypeJob ? 1 : 0 }}>
                 <Select
+                    classNamePrefix="dropdown-for-appOrJob"
                     value={permission.entityName}
                     isMulti
                     components={{
@@ -526,6 +530,7 @@ const DirectPermission = ({
             )}
             <div style={{ order: isAccessTypeJob ? 4 : 0 }}>
                 <Select
+                    classNamePrefix="dropdown-for-role"
                     value={primaryActionRole}
                     name="action"
                     placeholder="Select role"
