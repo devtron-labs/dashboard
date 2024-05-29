d0a910be20b61230501141d67b70d2a17a6e38e3
vivek-devtron <vivek@devtron.ai>
2024-05-29 12:43:57 +0530

    Added resource watcher flag

--- src/components/common/navigation/NavigationRoutes.tsx
+++ src/components/common/navigation/NavigationRoutes.tsx
@@ -410,7 +410,7 @@ export default function NavigationRoutes() {
                                             path={URLS.SECURITY}
                                             render={(props) => <Security {...props} serverMode={serverMode} />}
                                         />,
-                                        ...(ResourceWatcherRouter
+                                        ...(!window._env_.HIDE_RESOURCE_WATCHER && ResourceWatcherRouter
                                             ? [
                                                   <Route key={URLS.RESOURCE_WATCHER} path={URLS.RESOURCE_WATCHER}>
                                                       <ResourceWatcherRouter />
