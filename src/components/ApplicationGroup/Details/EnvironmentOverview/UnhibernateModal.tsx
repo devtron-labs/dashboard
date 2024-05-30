6da76e6fbc8ba6ece88449c5c4c002af5ca608a0
shivani170 <shivani@devtron.ai>
2024-05-30 18:20:32 +0530

    message fix

--- src/components/ApplicationGroup/Details/EnvironmentOverview/UnhibernateModal.tsx
+++ src/components/ApplicationGroup/Details/EnvironmentOverview/UnhibernateModal.tsx
@@ -62,7 +62,7 @@ export const UnhibernateModal = ({
                 <>
                     <span>
                         Pods for the selected applications will be &nbsp;
-                        <span className="fw-6">scaled down to 0 on {envName} environment.</span>
+                        <span className="fw-6">scaled up to its original count on {envName} environment.</span>
                     </span>
                     <span> Are you sure you want to continue?</span>
                 </>
@@ -70,7 +70,7 @@ export const UnhibernateModal = ({
         }
         return (
             <>
-                <div>Hibernating some applications is blocked due to deployment window</div>
+                <div>Unhibernating some applications is blocked due to deployment window</div>
                 {ResistantInput && (
                     <ResistantInput type={MODAL_TYPE.UNHIBERNATE} setActionButtonDisabled={setActionButtonDisabled} />
                 )}
