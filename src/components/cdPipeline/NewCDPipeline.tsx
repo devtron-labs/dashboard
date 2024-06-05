3b46b57c94b23faeefdaadbc17e32d6c47d3b0bc
Amrit Borah <elessar1802@gmail.com>
2024-06-05 16:32:22 +0530

    fix: cdPipeline modal on deletion fail

--- src/components/cdPipeline/NewCDPipeline.tsx
+++ src/components/cdPipeline/NewCDPipeline.tsx
@@ -927,7 +927,6 @@ export default function NewCDPipeline({
                         !response.result.deleteResponse?.clusterReachable &&
                         !response.result.deleteResponse?.deleteInitiated
                     ) {
-                        hideDeleteModal()
                         const form = { ...formData }
                         form.clusterName = response.result.deleteResponse?.clusterName
                         setFormData(form)
@@ -959,7 +958,6 @@ export default function NewCDPipeline({
                 // 422 is for deployment window
                 if (!force && error.code != 403 && error.code != 412 && error.code != 422) {
                     setForceDeleteDialogData(error)
-                    hideDeleteModal()
                     setDeleteDialog(DeleteDialogType.showForceDeleteDialog)
                 } else {
                     showError(error)
