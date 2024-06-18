708801b6fd6c1f58418a6fac7bcf2d750509038b
Eshank Vaish <eshank@devtron.ai>
2024-06-18 15:50:17 +0530

    refactor: remove unused code and add comments

--- src/components/app/service.ts
+++ src/components/app/service.ts
@@ -48,62 +48,6 @@ const stageMap = {
 export const getAppList = (request, options?) => {
     return post(Routes.APP_LIST, request, options)
 }
-//
-export function getCITriggerInfo(params: { envId: number | string; ciArtifactId: number | string }) {
-    const URL = `${Routes.APP}/material-info/${params.envId}/${params.ciArtifactId}`
-    return get(URL)
-}
-//
-export function getCITriggerInfoModal(params: { envId: number | string; ciArtifactId: number | string }) {
-    return getCITriggerInfo(params).then((response) => {
-        let materials = response?.result?.ciMaterials || []
-        materials = materials.map((mat) => {
-            return {
-                id: mat.id,
-                gitMaterialName: mat.gitMaterialName || '',
-                gitMaterialId: mat.gitMaterialId || 0,
-                gitURL: mat.url || '',
-                type: mat.type || '',
-                value: mat.value || '',
-                active: mat.active || false,
-                history: mat.history.map((hist, index) => {
-                    return {
-                        commitURL: mat.url ? createGitCommitUrl(mat.url, hist.Commit) : '',
-                        commit: hist.Commit || '',
-                        author: hist.Author || '',
-                        date: hist.Date ? handleUTCTime(hist.Date, false) : '',
-                        message: hist.Message || '',
-                        changes: hist.Changes || [],
-                        showChanges: index === 0,
-                        webhookData: hist.WebhookData,
-                    }
-                }),
-                lastFetchTime: mat.lastFetchTime || '',
-            }
-        })
-        if (materials.length > 0 && !materials.find((mat) => mat.isSelected)) {
-            materials[0].isSelected = true
-        }
-        return {
-            code: response.code,
-            result: {
-                materials,
-                triggeredByEmail: response.result.triggeredByEmail || '',
-                lastDeployedTime: response.result.lastDeployedTime
-                    ? handleUTCTime(response.result.lastDeployedTime, false)
-                    : '',
-                environmentName: response.result.environmentName || '',
-                environmentId: response.result.environmentId || 0,
-                appName: response.result.appName || '',
-                appReleaseTags: response?.result?.imageTaggingData?.appReleaseTags,
-                imageComment: response?.result?.imageTaggingData?.imageComment,
-                imageReleaseTags: response?.result?.imageTaggingData?.imageReleaseTags,
-                image: response?.result?.image,
-                tagsEditable: response?.result?.imageTaggingData?.tagsEditable,
-            },
-        }
-    })
-}
 
 export function deleteResource({ appName, env, name, kind, group, namespace, version, appId, envId }) {
     if (!group) {
