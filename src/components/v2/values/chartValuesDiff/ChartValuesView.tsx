70bf68823aeb523078019713c54fb6d99aa949d9
arunjaindev <arun.jain@devtron.ai>
2024-06-25 19:11:54 +0530

    feat: toggle security scan

--- src/components/v2/values/chartValuesDiff/ChartValuesView.tsx
+++ src/components/v2/values/chartValuesDiff/ChartValuesView.tsx
@@ -181,8 +181,7 @@ const ChartValuesView = ({
     const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
     const [showRepoSelector, setShowRepoSelector] = useState<boolean>(false)
     const [shouldShowPrompt, setShouldShowPrompt] = useState<boolean>(true)
-    const [scanChartImages, setScanChartImages] = useState(false)
-    const [scanKubernetesManifest, setScanKubernetesManifest] = useState(false)
+    const [securityScan, setSecurityScan] = useState(false)
 
     const [commonState, dispatch] = useReducer(
         chartValuesReducer,
@@ -1317,12 +1316,8 @@ const ChartValuesView = ({
         )
     }
 
-    const handleToggleScanChart = () => {
-        setScanChartImages(!scanChartImages)
-    }
-
-    const handleToggleScanManifest = () => {
-        setScanKubernetesManifest(!scanKubernetesManifest)
+    const handleToggleSecurityScan = () => {
+        setSecurityScan(!securityScan)
     }
 
     const handleProjectSelection = (selected: ChartValuesOptionType) => {
@@ -1769,18 +1764,10 @@ const ChartValuesView = ({
                                 <div className="dc__border-bottom-n1" />
                                 <div className="flexbox dc__content-space dc__align-items-center">
                                     <span className="fs-13 fw-6 lh-20 dc__border-bottom-dashed--n3">
-                                        Scan images in chart
+                                        Security scan
                                     </span>
                                     <div className="w-28 h-18">
-                                        <Toggle selected={scanChartImages} onSelect={handleToggleScanChart} />
-                                    </div>
-                                </div>
-                                <div className="flexbox dc__content-space dc__align-items-center">
-                                    <span className="fs-13 fw-6 lh-20 dc__border-bottom-dashed--n3">
-                                        Scan kubernetes manifest
-                                    </span>
-                                    <div className="w-28 h-18">
-                                        <Toggle selected={scanKubernetesManifest} onSelect={handleToggleScanManifest} />
+                                        <Toggle selected={securityScan} onSelect={handleToggleSecurityScan} />
                                     </div>
                                 </div>
                             </div>
