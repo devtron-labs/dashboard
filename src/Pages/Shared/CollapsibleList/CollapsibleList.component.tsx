4177533ac2afc1c5ca774003f9577e1c5c81b259
Rohit Raj <rohit.raj@devtron.ai>
2024-07-08 12:15:41 +0530

    refactor: collapsible list - logic to handle collapse state moved to props - handle externally

--- src/Pages/Shared/CollapsibleList/CollapsibleList.component.tsx
+++ src/Pages/Shared/CollapsibleList/CollapsibleList.component.tsx
@@ -1,21 +1,4 @@
-/*
- *   Copyright (c) 2024 Devtron Inc.
- *   All rights reserved.
-
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
-
- *   http://www.apache.org/licenses/LICENSE-2.0
-
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
-
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { Fragment } from 'react'
 import { NavLink } from 'react-router-dom'
 import Tippy, { TippyProps } from '@tippyjs/react'
 
@@ -32,97 +15,77 @@ const renderWithTippy = (tippyProps: TippyProps) => (children: React.ReactElemen
     </Tippy>
 )
 
-export const CollapsibleList = ({ config, expandedIds }: CollapsibleListProps) => {
-    const [isExpanded, setIsExpanded] = useState<Record<string, boolean>>({})
-
-    useEffect(() => {
-        // Expand the collapsible list items whenever the `expandedIds` is modified.
-        setIsExpanded((prevState) => ({
-            ...prevState,
-            ...expandedIds.reduce((acc, id) => ({ ...acc, [id]: true }), {}),
-        }))
-    }, [expandedIds])
-
-    const handleCollapseBtnClick = (id: string) => () =>
-        setIsExpanded((prev) => ({
-            ...prev,
-            [id]: !prev[id],
-        }))
-
+export const CollapsibleList = ({ config, onCollapseBtnClick }: CollapsibleListProps) => {
     return (
         <div className="mw-none bcn-0">
-            {config.map(
-                ({ id, header, headerIcon: HeaderIcon, headerIconProps, headerIconBtnProps, items, noItemsText }) => (
-                    <Fragment key={id}>
-                        <div className="flexbox dc__align-items-center dc__gap-4 py-6 px-8">
+            {config.map(({ id, header, headerIconConfig, items, noItemsText, isExpanded }) => (
+                <Fragment key={id}>
+                    <div className="flexbox dc__align-items-center dc__gap-4 py-6 px-8">
+                        <button
+                            type="button"
+                            className="dc__unset-button-styles mw-none flexbox dc__align-items-center flex-grow-1 p-0 cn-9 fs-12 lh-1-5 fw-6 dc__gap-4"
+                            onClick={(e) => onCollapseBtnClick(id, e)}
+                        >
+                            <ICExpand
+                                className="icon-dim-16 fcn-6 dc__no-shrink cursor rotate"
+                                style={{ ['--rotateBy' as string]: isExpanded ? '90deg' : '0deg' }}
+                            />
+                            <span className="flex-grow-1 dc__align-left dc__ellipsis-right">{header}</span>
+                        </button>
+                        {headerIconConfig && (
                             <button
+                                {...headerIconConfig.btnProps}
                                 type="button"
-                                className="dc__unset-button-styles mw-none flexbox dc__align-items-center flex-grow-1 p-0 cn-9 fs-12 lh-1-5 fw-6 dc__gap-4"
-                                onClick={handleCollapseBtnClick(id)}
+                                className={`dc__unset-button-styles dc__no-shrink cursor ${headerIconConfig.btnProps?.className || ''}`}
                             >
-                                <ICExpand
-                                    className="icon-dim-16 fcn-6 dc__no-shrink cursor rotate"
-                                    style={{ ['--rotateBy' as string]: isExpanded[id] ? '90deg' : '0deg' }}
+                                <headerIconConfig.Icon
+                                    {...headerIconConfig.props}
+                                    className={`icon-dim-16 ${headerIconConfig.props?.className || ''}`}
                                 />
-                                <span className="flex-grow-1 dc__align-left dc__ellipsis-right">{header}</span>
                             </button>
-                            {HeaderIcon && (
-                                <button
-                                    {...headerIconBtnProps}
-                                    type="button"
-                                    className={`dc__unset-button-styles dc__no-shrink cursor ${headerIconBtnProps?.className || ''}`}
-                                >
-                                    <HeaderIcon
-                                        {...headerIconProps}
-                                        className={`icon-dim-16 ${headerIconProps?.className || ''}`}
-                                    />
-                                </button>
+                        )}
+                    </div>
+                    {isExpanded && (
+                        <div className="collapsible ml-16 pl-4 dc__border-left">
+                            {!items.length ? (
+                                <div className="collapsible__item flexbox dc__gap-8 cn-7 dc__no-decor br-4 py-6 px-8">
+                                    <span className="collapsible__item__title dc__ellipsis-right fs-13 lh-20">
+                                        {noItemsText || 'No items found.'}
+                                    </span>
+                                </div>
+                            ) : (
+                                items.map(({ title, href, iconConfig, subtitle }) => (
+                                    <NavLink
+                                        key={title}
+                                        to={href}
+                                        className="collapsible__item flexbox dc__gap-8 cn-7 dc__no-decor br-4 py-6 px-8 cursor"
+                                    >
+                                        <div className="flexbox-col flex-grow-1 mw-none">
+                                            <span className="collapsible__item__title dc__ellipsis-right fs-13 lh-20">
+                                                {title}
+                                            </span>
+                                            {subtitle && (
+                                                <span className="dc__ellipsis-right fw-4 lh-1-5 cn-7">{subtitle}</span>
+                                            )}
+                                        </div>
+                                        {iconConfig && (
+                                            <ConditionalWrap
+                                                condition={!!iconConfig.tooltipProps}
+                                                wrap={renderWithTippy(iconConfig.tooltipProps)}
+                                            >
+                                                <iconConfig.Icon
+                                                    {...iconConfig.props}
+                                                    className={`icon-dim-16 dc__no-shrink cursor ${iconConfig.props?.className || ''}`}
+                                                />
+                                            </ConditionalWrap>
+                                        )}
+                                    </NavLink>
+                                ))
                             )}
                         </div>
-                        {isExpanded[id] && (
-                            <div className="collapsible ml-16 pl-4 dc__border-left">
-                                {!items.length ? (
-                                    <div className="collapsible__item flexbox dc__gap-8 cn-7 dc__no-decor br-4 py-6 px-8">
-                                        <span className="collapsible__item__title dc__ellipsis-right fs-13 lh-20">
-                                            {noItemsText || 'No items found.'}
-                                        </span>
-                                    </div>
-                                ) : (
-                                    items.map(({ title, href, icon: Icon, iconProps, iconTooltipProps, subtitle }) => (
-                                        <NavLink
-                                            key={title}
-                                            to={href}
-                                            className="collapsible__item flexbox dc__gap-8 cn-7 dc__no-decor br-4 py-6 px-8 cursor"
-                                        >
-                                            <div className="flexbox-col flex-grow-1 mw-none">
-                                                <span className="collapsible__item__title dc__ellipsis-right fs-13 lh-20">
-                                                    {title}
-                                                </span>
-                                                {subtitle && (
-                                                    <span className="dc__ellipsis-right fw-4 lh-1-5 cn-7">
-                                                        {subtitle}
-                                                    </span>
-                                                )}
-                                            </div>
-                                            {Icon && (
-                                                <ConditionalWrap
-                                                    condition={!!iconTooltipProps}
-                                                    wrap={renderWithTippy(iconTooltipProps)}
-                                                >
-                                                    <Icon
-                                                        {...iconProps}
-                                                        className={`icon-dim-16 dc__no-shrink cursor ${iconProps?.className || ''}`}
-                                                    />
-                                                </ConditionalWrap>
-                                            )}
-                                        </NavLink>
-                                    ))
-                                )}
-                            </div>
-                        )}
-                    </Fragment>
-                ),
-            )}
+                    )}
+                </Fragment>
+            ))}
         </div>
     )
 }
--- src/Pages/Shared/CollapsibleList/CollapsibleList.types.ts
+++ src/Pages/Shared/CollapsibleList/CollapsibleList.types.ts
@@ -1,20 +1,4 @@
-/*
- *   Copyright (c) 2024 Devtron Inc.
- *   All rights reserved.
-
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
-
- *   http://www.apache.org/licenses/LICENSE-2.0
-
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
-
+import React from 'react'
 import { TippyProps } from '@tippyjs/react'
 
 export interface CollapsibleListItem {
@@ -27,17 +11,22 @@ export interface CollapsibleListItem {
      */
     subtitle?: string
     /**
-     * A React component representing an icon to be displayed with the list item.
+     * Configuration for the icon.
      */
-    icon?: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
-    /**
-     * Properties for the icon component.
-     */
-    iconProps?: React.SVGProps<SVGSVGElement>
-    /**
-     * Properties for the tooltip component of the icon.
-     */
-    iconTooltipProps?: TippyProps
+    iconConfig?: {
+        /**
+         * A React component representing an icon to be displayed with the list item.
+         */
+        Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+        /**
+         * Properties for the icon component.
+         */
+        props?: React.SVGProps<SVGSVGElement>
+        /**
+         * Properties for the tooltip component of the icon.
+         */
+        tooltipProps?: TippyProps
+    }
     /**
      * The URL of the nav link.
      */
@@ -54,17 +43,22 @@ export interface CollapsibleListConfig {
      */
     header: string
     /**
-     * A React component representing an icon button to be displayed with the header.
+     * Configuration for the header icon.
      */
-    headerIcon?: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
-    /**
-     * Properties for the header icon component.
-     */
-    headerIconProps?: React.SVGProps<SVGSVGElement>
-    /**
-     * Properties for the header icon button component.
-     */
-    headerIconBtnProps?: React.ButtonHTMLAttributes<HTMLButtonElement>
+    headerIconConfig?: {
+        /**
+         * A React component representing an icon button to be displayed with the header.
+         */
+        Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+        /**
+         * Properties for the header icon component.
+         */
+        props?: React.SVGProps<SVGSVGElement>
+        /**
+         * Properties for the header icon button component.
+         */
+        btnProps?: React.ButtonHTMLAttributes<HTMLButtonElement>
+    }
     /**
      * Text to display when there are no items in the list.
      * @default 'No items found.'
@@ -74,6 +68,10 @@ export interface CollapsibleListConfig {
      * An array of items to be displayed in the collapsible list.
      */
     items: CollapsibleListItem[]
+    /**
+     * Boolean indicating whether the list is expanded or not.
+     */
+    isExpanded?: boolean
 }
 
 export interface CollapsibleListProps {
@@ -82,8 +80,11 @@ export interface CollapsibleListProps {
      */
     config: CollapsibleListConfig[]
     /**
-     * An array containing the IDs of the collapsible items that need to be expanded. \
-     * Whenever this array is modified, the expanded state is updated internally.
+     * Function to handle the collapse button click event.
+     *
+     * @param id - The unique identifier for the item to collapse.
+     * @param e - The mouse event triggered by the button click.
+     *
      */
-    expandedIds?: string[]
+    onCollapseBtnClick?: (id: string, e: React.MouseEvent<HTMLButtonElement>) => void
 }
