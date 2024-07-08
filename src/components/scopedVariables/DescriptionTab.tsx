447c5ae51cfffec7f8e1d4dcdad6c866b791671e
AbhishekA1509 <abhishek@devtron.ai>
2024-07-08 18:10:39 +0530

    fix: remove React import from scoped variables

--- src/components/scopedVariables/DescriptionTab.tsx
+++ src/components/scopedVariables/DescriptionTab.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { DescriptorTabProps } from './types'
 import { DESCRIPTOR_TABS } from './constants'
 
--- src/components/scopedVariables/SavedVariablesContent.tsx
+++ src/components/scopedVariables/SavedVariablesContent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { generatePath, useHistory, useParams, useRouteMatch } from 'react-router-dom'
 import {
     CodeEditor,
