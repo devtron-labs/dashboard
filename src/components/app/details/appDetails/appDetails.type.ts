2a16b68e4307b5b50f94da0d858ff039a6c52750
shivani170 <shivani@devtron.ai>
2024-07-29 10:57:50 +0530

    renaming constant ro isExternalCI

--- src/components/app/details/appDetails/appDetails.type.ts
+++ src/components/app/details/appDetails/appDetails.type.ts
@@ -228,7 +228,7 @@ export interface SecurityVulnerabilityCardType {
     appId?: string
     envId?: string
     installedAppId?: number
-    isExternalApp?: boolean
+    isExternalCI?: boolean
 }
 
 export interface DeployedCommitCardType {
