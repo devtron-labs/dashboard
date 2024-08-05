827b38ffdcb3e0154b57bd9555090a98aee7cf0e
Amrit Borah <elessar1802@gmail.com>
2024-08-05 16:40:46 +0530

    fix: replace with common search bar in resource filter

--- src/components/ResourceBrowser/ResourceList/ResourceFilterOptions.tsx
+++ src/components/ResourceBrowser/ResourceList/ResourceFilterOptions.tsx
@@ -18,11 +18,15 @@ import React, { useEffect, useRef, useState, useMemo } from 'react'
 import { useLocation, useParams, useHistory } from 'react-router-dom'
 import ReactSelect from 'react-select'
 import { withShortcut, IWithShortcut } from 'react-keybind'
-import { ConditionalWrap, useAsync, useRegisterShortcut, OptionType } from '@devtron-labs/devtron-fe-common-lib'
-import { Option } from '../../v2/common/ReactSelect.utils'
+import {
+    ConditionalWrap,
+    useAsync,
+    useRegisterShortcut,
+    OptionType,
+    SearchBar,
+    Option,
+} from '@devtron-labs/devtron-fe-common-lib'
 import { ResourceFilterOptionsProps, URLParams } from '../Types'
-import { ReactComponent as Search } from '../../../assets/icons/ic-search.svg'
-import { ReactComponent as Clear } from '../../../assets/icons/ic-error.svg'
 import { ResourceValueContainerWithIcon, tippyWrapper } from './ResourceList.component'
 import { ALL_NAMESPACE_OPTION, FILTER_SELECT_COMMON_STYLES, NAMESPACE_NOT_APPLICABLE_OPTION } from '../Constants'
 import { ShortcutKeyBadge } from '../../common/formFields/Widgets/Widgets'
@@ -86,14 +90,14 @@ const ResourceFilterOptions = ({
         }
     }, [registerShortcut, isOpen])
 
-    const handleFilterKeyPress = (e: React.KeyboardEvent): void => {
+    const handleFilterKeyUp = (e: React.KeyboardEvent): void => {
         if (e.key === 'Escape' || e.key === 'Esc') {
             searchInputRef.current?.blur()
         }
     }
 
-    const handleOnChangeSearchText: React.FormEventHandler<HTMLInputElement> = (event): void => {
-        setSearchText(event.currentTarget.value)
+    const handleOnChangeSearchText: React.ComponentProps<typeof SearchBar>['handleSearchChange'] = (text) => {
+        setSearchText(text)
     }
 
     const handleNamespaceChange = (selected: OptionType): void => {
@@ -118,40 +122,23 @@ const ResourceFilterOptions = ({
 
     const handleInputFocus = () => setIsInputFocused(true)
 
-    const clearSearchInput = () => {
-        setSearchText('')
-        searchInputRef.current?.focus()
-    }
-
     return (
         <>
             {typeof renderRefreshBar === 'function' && renderRefreshBar()}
             <div className="resource-filter-options-container flexbox dc__content-space pt-16 pr-20 pb-12 pl-20 w-100">
-                <div className="search dc__position-rel margin-right-0 en-2 bw-1 br-4 h-32 cursor-text">
-                    <Search className="search__icon icon-dim-16" onClick={handleInputShortcut} />
-                    <input
-                        ref={searchInputRef}
-                        type="text"
-                        placeholder={`Search ${selectedResource?.gvk?.Kind || ''}`}
-                        value={searchText}
-                        className={`search__input ${isSearchInputDisabled ? 'cursor-not-allowed' : ''}`}
-                        onChange={handleOnChangeSearchText}
-                        onKeyUp={handleFilterKeyPress}
-                        onFocus={handleInputFocus}
-                        onBlur={handleInputBlur}
-                        disabled={isSearchInputDisabled}
-                        data-testid="search-input-for-resource"
+                <div className="resource-filter-options-container__search-box dc__position-rel flex-grow-1">
+                    <SearchBar
+                        inputProps={{
+                            value: searchText,
+                            placeholder: `Search ${selectedResource?.gvk?.Kind || ''}`,
+                            disabled: isSearchInputDisabled,
+                            onBlur: handleInputBlur,
+                            onFocus: handleInputFocus,
+                            ref: searchInputRef,
+                            onKeyUp: handleFilterKeyUp,
+                        }}
+                        handleSearchChange={handleOnChangeSearchText}
                     />
-                    {!!searchText && (
-                        <button
-                            className="search__clear-button"
-                            type="button"
-                            onClick={clearSearchInput}
-                            aria-label="Search resources"
-                        >
-                            <Clear className="icon-dim-18 icon-n4 dc__vertical-align-middle" />
-                        </button>
-                    )}
                     {showShortcutKey && (
                         <ShortcutKeyBadge
                             shortcutKey="r"
