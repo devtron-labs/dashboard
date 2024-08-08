0a2c83fbe4bfc8d5943e73202570ea82d28604c8
vivek-devtron <vivek@devtron.ai>
2024-08-08 12:07:20 +0530

    fix: codeEditor worker issue

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,22 @@ import { URLS } from './config'
 import Hotjar from './components/Hotjar/Hotjar'
 import { validateToken } from './services/service'
 
+//Monaco Editor worker dependency
+import 'monaco-editor'
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import { MODES } from '@devtron-labs/devtron-fe-common-lib'
+import YamlWorker from './yaml.worker.js?worker'
+
+//Monaco Editor worker initialization
+self.MonacoEnvironment = {
+    getWorker(_, label) {
+        if (label === MODES.YAML) {
+            return new YamlWorker()
+        }
+        return new editorWorker()
+    },
+}
+
 const NavigationRoutes = lazy(() => import('./components/common/navigation/NavigationRoutes'))
 const Login = lazy(() => import('./components/login/Login'))
 const GenericDirectApprovalModal = importComponentFromFELibrary('GenericDirectApprovalModal')
