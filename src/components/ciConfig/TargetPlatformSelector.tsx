0d9e5dfd82be0d5fc48d95290604b6f35a47bf18
Eshank Vaish <eshank@devtron.ai>
2024-08-14 10:37:27 +0530

    fix: add info icon in the target platform selector footer

--- src/components/ciConfig/TargetPlatformSelector.tsx
+++ src/components/ciConfig/TargetPlatformSelector.tsx
@@ -16,13 +16,17 @@
 
 import { SelectPicker, SelectPickerProps } from '@devtron-labs/devtron-fe-common-lib'
 import { ReactComponent as WarningIcon } from '@Icons/ic-warning.svg'
+import { ReactComponent as ICInfoOutlineGrey } from '@Icons/ic-info-outline-grey.svg'
 import { DockerConfigOverrideKeys } from '../ciPipeline/types'
 import { TARGET_PLATFORM_LIST } from './CIConfig.utils'
 import { SelectorMessaging } from './ciConfigConstant'
 import { TargetPlatformSelectorType } from './types'
 
 const renderMenuListFooter = () => (
-    <div className="cn-5 px-12 py-4 dc__italic-font-style">{SelectorMessaging.TARGET_SELECTOR_MENU}</div>
+    <div className="cn-5 px-12 py-4 dc__italic-font-style flex left dc__gap-6">
+        <ICInfoOutlineGrey className="icon-dim-16 icon-n6" />
+        {SelectorMessaging.TARGET_SELECTOR_MENU}
+    </div>
 )
 
 const TargetPlatformSelector = ({
