101940e079371a44e663ee830c34b1b37169929a
Eshank Vaish <eshank@devtron.ai>
2024-09-03 11:45:14 +0530

    fix: update the styling for outline

--- src/components/common/navigation/ButtonPage.tsx
+++ src/components/common/navigation/ButtonPage.tsx
@@ -61,14 +61,6 @@ const ButtonPage = () => (
                                     disabled
                                 />
                                 <Button
-                                    text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-                                    variant={ButtonVariantType[buttonVariant]}
-                                    size={ComponentSizeType[size]}
-                                    startIcon={<ICCube />}
-                                    endIcon={<ICCube />}
-                                    style={ButtonStyleType[style]}
-                                />
-                                <Button
                                     text="With Tippy"
                                     variant={ButtonVariantType[buttonVariant]}
                                     size={ComponentSizeType[size]}
@@ -77,6 +69,14 @@ const ButtonPage = () => (
                                     style={ButtonStyleType[style]}
                                     showTippy
                                     tippyContent="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+                                />
+                                <Button
+                                    text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+                                    variant={ButtonVariantType[buttonVariant]}
+                                    size={ComponentSizeType[size]}
+                                    startIcon={<ICCube />}
+                                    endIcon={<ICCube />}
+                                    style={ButtonStyleType[style]}
                                 />
                             </div>
                         ))}
