945aab4a1c4a4e6c4fd1d9463f10135fc37724ef
Rohit Raj <rohit.raj@devtron.ai>
2024-09-13 16:16:04 +0530

    feat: AppDetails -> Resource Tree - UI update/fix

--- src/components/v2/appDetails/k8Resource/K8Resource.component.tsx
+++ src/components/v2/appDetails/k8Resource/K8Resource.component.tsx
@@ -14,20 +14,23 @@
  * limitations under the License.
  */
 
-import React, { useEffect } from 'react'
-import NodeTreeComponent from './nodeType/NodeTree.component'
-import FilterResource from './FilterResource'
-import AppDetailsStore, { AppDetailsTabs } from '../appDetails.store'
-import NodeComponent from './nodeType/Node.component'
-import { useSharedState } from '../../utils/useSharedState'
-import IndexStore from '../index.store'
-import { K8ResourceComponentProps } from '../appDetails.type'
-import { ReactComponent as K8ResourceIcon } from '../../../../assets/icons/ic-object.svg'
-import { ReactComponent as Info } from '../../../../assets/icons/ic-info-outline.svg'
-import './k8resources.scss'
-import { useMainContext } from '@devtron-labs/devtron-fe-common-lib'
+import { useEffect } from 'react'
 
-export default function K8ResourceComponent({
+import { StatusFilterButtonComponent, useMainContext } from '@devtron-labs/devtron-fe-common-lib'
+
+import { ReactComponent as K8ResourceIcon } from '@Icons/ic-object.svg'
+import { ReactComponent as Info } from '@Icons/ic-info-outline.svg'
+import { useSharedState } from '@Components/v2/utils/useSharedState'
+
+import IndexStore from '../index.store'
+import AppDetailsStore, { AppDetailsTabs } from '../appDetails.store'
+import { K8ResourceComponentProps } from '../appDetails.type'
+import NodeTreeComponent from './nodeType/NodeTree.component'
+import NodeComponent from './nodeType/Node.component'
+
+import './k8resources.scss'
+
+export const K8ResourceComponent = ({
     clickedNodes,
     registerNodeClick,
     handleFocusTabs,
@@ -37,7 +40,7 @@ export default function K8ResourceComponent({
     clusterId,
     isDeploymentBlocked,
     isExternalApp,
-}: K8ResourceComponentProps) {
+}: K8ResourceComponentProps) => {
     const [nodes] = useSharedState(IndexStore.getAppDetailsNodes(), IndexStore.getAppDetailsNodesObservable())
     const { isSuperAdmin } = useMainContext()
     useEffect(() => {
@@ -46,18 +49,15 @@ export default function K8ResourceComponent({
 
     return (
         <div className="bcn-0" style={{ justifyContent: 'space-between' }}>
-            <div className="pt-16 pl-20 pb-8">
-                <FilterResource nodes={nodes} />
-            </div>
             {nodes.length > 0 ? (
                 <div
                     className={`resource-node-wrapper flexbox ${isSuperAdmin ? 'pb-28' : ''}`}
                     data-testid="resource-node-wrapper"
                 >
-                    <div
-                        className="k8-resources-node-tree pt-8 pl-16 dc__border-right"
-                        data-testid="k8-resources-node-tree"
-                    >
+                    <div className="k8-resources-node-tree p-8 dc__border-right" data-testid="k8-resources-node-tree">
+                        <div className="flexbox mb-8 px-12">
+                            <StatusFilterButtonComponent nodes={nodes} />
+                        </div>
                         <NodeTreeComponent
                             clickedNodes={clickedNodes}
                             registerNodeClick={registerNodeClick}
@@ -83,39 +83,35 @@ export default function K8ResourceComponent({
     )
 }
 
-export const EmptyK8sResourceComponent = ({ emptyStateMessage }: { emptyStateMessage: string }) => {
-    return (
-        <>
-            <div
-                data-testid="resource-tree-wrapper"
-                className="resource-tree-wrapper flexbox pl-20 pr-20"
-                style={{ outline: 'none' }}
-            >
-                <ul className="tab-list">
-                    <li className="flex left dc__ellipsis-right">
-                        <div className="flex">
-                            <div className="resource-tree-tab bcn-0 cn-9 left pl-12 pt-8 pb-8 pr-12">
-                                <div className="resource-tree__tab-hover tab-list__tab resource-tab__node cursor cn-9 fw-6 dc__no-decor m-0-imp">
-                                    <div className="flex left cn-9">
-                                        <span className="icon-dim-16 resource-tree__tab-hover fcn-9">
-                                            <K8ResourceIcon />
-                                        </span>
-                                        <span className="ml-8 dc__capitalize fs-12">
-                                            {AppDetailsTabs.k8s_Resources}
-                                        </span>
-                                    </div>
+export const EmptyK8sResourceComponent = ({ emptyStateMessage }: { emptyStateMessage: string }) => (
+    <>
+        <div
+            data-testid="resource-tree-wrapper"
+            className="resource-tree-wrapper flexbox pl-20 pr-20"
+            style={{ outline: 'none' }}
+        >
+            <ul className="tab-list">
+                <li className="flex left dc__ellipsis-right">
+                    <div className="flex">
+                        <div className="resource-tree-tab bcn-0 cn-9 left pl-12 pt-8 pb-8 pr-12">
+                            <div className="resource-tree__tab-hover tab-list__tab resource-tab__node cursor cn-9 fw-6 dc__no-decor m-0-imp">
+                                <div className="flex left cn-9">
+                                    <span className="icon-dim-16 resource-tree__tab-hover fcn-9">
+                                        <K8ResourceIcon />
+                                    </span>
+                                    <span className="ml-8 dc__capitalize fs-12">{AppDetailsTabs.k8s_Resources}</span>
                                 </div>
                             </div>
                         </div>
-                    </li>
-                </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div className="bcn-0 flex h-100">
+            <div className="flex column h-100">
+                <Info className="icon-dim-20 icon-n5" />
+                <span className="mt-10">{emptyStateMessage}</span>
             </div>
-            <div className="bcn-0 flex h-100">
-                <div className="flex column h-100">
-                    <Info className="icon-dim-20 icon-n5" />
-                    <span className="mt-10">{emptyStateMessage}</span>
-                </div>
-            </div>
-        </>
-    )
-}
+        </div>
+    </>
+)
