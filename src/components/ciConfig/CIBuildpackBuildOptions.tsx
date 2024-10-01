d00f443cd76b1a5c2134f5133e2338d8812efdee
shivani170 <shivani@devtron.ai>
2024-10-01 12:41:21 +0530

    chore: REMOVED UNUSED CODE

--- src/components/ciConfig/CIBuildpackBuildOptions.tsx
+++ src/components/ciConfig/CIBuildpackBuildOptions.tsx
@@ -608,7 +608,6 @@ export default function CIBuildpackBuildOptions({
                             isSearchable={false}
                             onChange={handleVersionSelection}
                             size={ComponentSizeType.large}
-                            isCreatableSingleSelect
                         />
                     </div>
                 </div>
