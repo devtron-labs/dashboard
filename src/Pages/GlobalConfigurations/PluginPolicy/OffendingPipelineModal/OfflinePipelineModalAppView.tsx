46a31a54a1999e3a2847437e576bfd2ec2c05172
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2024-10-17 16:55:19 +0530

    feat: add null state for offending pipeline app view

--- src/Pages/GlobalConfigurations/PluginPolicy/OffendingPipelineModal/OfflinePipelineModalAppView.tsx
+++ src/Pages/GlobalConfigurations/PluginPolicy/OffendingPipelineModal/OfflinePipelineModalAppView.tsx
@@ -1,8 +1,9 @@
+import { useHistory, useLocation, useRouteMatch } from 'react-router-dom'
+import { APIResponseHandler, GenericEmptyState, noop, useAsync } from '@devtron-labs/devtron-fe-common-lib'
+import noOffendingPipelineImg from '@Images/no-offending-pipeline.png'
 import { WorkflowCreate } from '@Components/app/details/triggerView/config'
 import { getInitialWorkflows } from '@Components/app/details/triggerView/workflow.service'
 import { Workflow } from '@Components/workflowEditor/Workflow'
-import { APIResponseHandler, noop, useAsync } from '@devtron-labs/devtron-fe-common-lib'
-import { useHistory, useLocation, useRouteMatch } from 'react-router-dom'
 import { OfflinePipelineModalAppViewProps } from './types'
 
 const OfflinePipelineModalAppView = ({ appId }: OfflinePipelineModalAppViewProps) => {
@@ -28,44 +29,48 @@ const OfflinePipelineModalAppView = ({ appId }: OfflinePipelineModalAppViewProps
                 reload: refetchWorkflows,
             }}
         >
-            {!isWorkflowsLoading && !workflowsError && (
-                <div>
-                    {workflowsResponse.workflows.map((workflow) => (
-                        <Workflow
-                            key={workflow.id}
-                            id={Number(workflow.id)}
-                            name={workflow.name}
-                            startX={workflow.startX}
-                            startY={workflow.startY}
-                            width={workflow.width}
-                            height={workflow.height}
-                            nodes={workflow.nodes}
-                            history={history}
-                            location={location}
-                            match={match}
-                            handleCDSelect={noop}
-                            handleCISelect={noop}
-                            openEditWorkflow={noop}
-                            showDeleteDialog={noop}
-                            addCIPipeline={noop}
-                            addWebhookCD={noop}
-                            showWebhookTippy={workflow.showTippy}
-                            hideWebhookTippy={noop}
-                            isJobView={false}
-                            envList={[]}
-                            filteredCIPipelines={[]}
-                            addNewPipelineBlocked
-                            handleChangeCI={null}
-                            selectedNode={null}
-                            handleSelectedNodeChange={noop}
-                            appName={null}
-                            getWorkflows={refetchWorkflows}
-                            reloadEnvironments={noop}
-                            workflowPositionState={null}
-                            handleDisplayLoader={noop}
-                        />
-                    ))}
-                </div>
+            {!isWorkflowsLoading && !workflowsError && workflowsResponse.workflows.length > 0 ? (
+                workflowsResponse.workflows.map((workflow) => (
+                    <Workflow
+                        key={workflow.id}
+                        id={Number(workflow.id)}
+                        name={workflow.name}
+                        startX={workflow.startX}
+                        startY={workflow.startY}
+                        width={workflow.width}
+                        height={workflow.height}
+                        nodes={workflow.nodes}
+                        history={history}
+                        location={location}
+                        match={match}
+                        handleCDSelect={noop}
+                        handleCISelect={noop}
+                        openEditWorkflow={noop}
+                        showDeleteDialog={noop}
+                        addCIPipeline={noop}
+                        addWebhookCD={noop}
+                        showWebhookTippy={workflow.showTippy}
+                        hideWebhookTippy={noop}
+                        isJobView={false}
+                        envList={[]}
+                        filteredCIPipelines={[]}
+                        addNewPipelineBlocked
+                        handleChangeCI={null}
+                        selectedNode={null}
+                        handleSelectedNodeChange={noop}
+                        appName={null}
+                        getWorkflows={refetchWorkflows}
+                        reloadEnvironments={noop}
+                        workflowPositionState={null}
+                        handleDisplayLoader={noop}
+                    />
+                ))
+            ) : (
+                <GenericEmptyState
+                    title="All pipelines are compliant for this app"
+                    subTitle="All matching pipelines have required plugins configured."
+                    image={noOffendingPipelineImg}
+                />
             )}
         </APIResponseHandler>
     )
