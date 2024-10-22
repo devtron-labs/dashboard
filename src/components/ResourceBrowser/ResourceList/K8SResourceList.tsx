f2ad994b2aaf3c351b594c801176c2518831ab23
Amrit Kashyap Borah <amrit.borah@devtron.ai>
2024-10-22 11:39:17 +0530

    fix: remove background hover effect on resource browser list header

--- src/components/ResourceBrowser/ResourceList/K8SResourceList.tsx
+++ src/components/ResourceBrowser/ResourceList/K8SResourceList.tsx
@@ -430,7 +430,7 @@ export const K8SResourceList = ({
             )} dc__overflow-scroll`}
         >
             <div
-                className="scrollable-resource-list__row h-36 fw-6 cn-7 fs-12 dc__gap-16 dc__zi-2 dc__position-sticky dc__border-bottom dc__uppercase bcn-0 dc__top-0"
+                className="scrollable-resource-list__row no-hover-bg h-36 fw-6 cn-7 fs-12 dc__gap-16 dc__zi-2 dc__position-sticky dc__border-bottom dc__uppercase bcn-0 dc__top-0"
                 style={{ gridTemplateColumns }}
             >
                 {resourceList?.headers.map((columnName) => (
