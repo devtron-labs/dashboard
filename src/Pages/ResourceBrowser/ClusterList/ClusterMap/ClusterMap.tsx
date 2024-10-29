82bca1c36758680d83c473528cb7cdc69d2a53ee
Rohit Raj <rohit.raj@devtron.ai>
2024-10-29 17:26:21 +0530

    refactor: ClusterMap - scss update

--- src/Pages/ResourceBrowser/ClusterList/ClusterMap/ClusterMap.tsx
+++ src/Pages/ResourceBrowser/ClusterList/ClusterMap/ClusterMap.tsx
@@ -50,7 +50,7 @@ export const ClusterMap = ({ treeMapData = [] }: ClusterMapProps) =>
         <div className="cluster-map pb-16 px-20 bcn-50">
             <div className="w-100 p-12 bcn-0 dc__border br-4 flexbox dc__align-items-center dc__gap-6">
                 {treeMapData.map(({ id, label, data }) => (
-                    <div key={id} className="flexbox-col dc__gap-4" style={{ flexGrow: data.length }}>
+                    <div key={id} className="flexbox-col dc__gap-4" style={{ flexGrow: data.length, minWidth: '0' }}>
                         {label && (
                             <Tooltip content={label}>
                                 <p className="m-0 fs-13 lh-18 fw-6 cn-9">{label}</p>
