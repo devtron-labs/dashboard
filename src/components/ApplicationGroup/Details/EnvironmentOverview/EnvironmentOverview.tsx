148041e2daee5b3adf6b1a8090080cbe4e48936d
arunjaindev <arun.jain@devtron.ai>
2024-11-01 14:45:23 +0530

    fix: payload in app group for deployment window

--- src/components/ApplicationGroup/Details/EnvironmentOverview/EnvironmentOverview.tsx
+++ src/components/ApplicationGroup/Details/EnvironmentOverview/EnvironmentOverview.tsx
@@ -139,7 +139,7 @@ export default function EnvironmentOverview({
     async function getDeploymentWindowEnvOverrideMetaData() {
         const appEnvTuples = (selectedAppDetails ? [selectedAppDetails] : selectedAppDetailsList).map((appDetail) => ({
             appId: +appDetail.appId,
-            envId: +appDetail.envId,
+            envId: +envId,
         }))
         if (appEnvTuples.length) {
             setIsDeploymentLoading(true)
