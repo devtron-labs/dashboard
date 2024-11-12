287a22db30ccf5899285a441a643faba0f38a5e8
Amrit Kashyap Borah <amrit.borah@devtron.ai>
2024-11-12 15:09:00 +0530

    chore: move configurable gui view code to fe-lib

--- src/Pages/Applications/DevtronApps/Details/AppConfigurations/MainContent/DeploymentTemplate/GUIView/types.ts
+++ src/Pages/Applications/DevtronApps/Details/AppConfigurations/MainContent/DeploymentTemplate/GUIView/types.ts
@@ -1,55 +1,5 @@
-import { DeploymentChartVersionType, RJSFFormSchema } from '@devtron-labs/devtron-fe-common-lib'
+import { DeploymentChartVersionType } from '@devtron-labs/devtron-fe-common-lib'
 import { DeploymentTemplateFormProps } from '../types'
-
-export enum NodeEntityType {
-    ARRAY = 'ARRAY',
-    OBJECT = 'OBJECT',
-    LEAF = 'LEAF',
-}
-
-export type NodeType = {
-    key: string
-    title: string
-    path: string
-    type: NodeEntityType
-    fieldType: RJSFFormSchema['type']
-} & (
-    | {
-          selectionStatus: 'all-selected' | 'some-selected' | 'none-selected'
-          isChecked?: never
-          children: Array<NodeType>
-      }
-    | {
-          isChecked: boolean
-          selectionStatus?: never
-          children?: never
-      }
-)
-
-export type TraversalType = {
-    node: NodeType
-    wf: (node: NodeType, data: unknown) => void
-    data: unknown
-}
-
-export type GUIViewModelType = {
-    schema: object
-    json: object
-    totalCheckedCount: number
-    root: NodeType
-    updateNodeForPath: (data: UpdateNodeForPathDataType) => void
-    getUncheckedNodes: () => string[]
-    postOrder: (props: TraversalType) => void
-    inOrder: (props: TraversalType) => void
-    syncCheckedFieldsInJson: (json: object) => object
-}
-
-export type ViewErrorType = Record<'title' | 'subTitle', string>
-
-export type GUIViewCheckboxProps = {
-    node: NodeType
-    updateNodeForPath: (path: string) => void
-}
 
 export interface GUIViewProps
     extends Pick<
@@ -67,9 +17,4 @@ export interface GUIViewProps
     handleChangeToYAMLMode: () => void
     guiSchema: string
     selectedChart: DeploymentChartVersionType
-}
-
-export type UpdateNodeForPathDataType = {
-    path: string
-    json: object
 }
