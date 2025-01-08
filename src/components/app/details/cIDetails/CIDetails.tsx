0d80fb13dacd0a07f3acbf67a05ecda2e514d6b8
arunjaindev <arun.jain@devtron.ai>
2025-01-08 19:46:10 +0530

    feat: hide security card if no access

--- src/components/app/details/cIDetails/CIDetails.tsx
+++ src/components/app/details/cIDetails/CIDetails.tsx
@@ -22,7 +22,6 @@ import {
     GenericEmptyState,
     useAsync,
     PipelineType,
-    ScannedByToolModal,
     Sidebar,
     LogResizeButton,
     CICDSidebarFilterOptionType,
@@ -60,7 +59,6 @@ import './ciDetails.scss'
 import { getModuleInfo } from '../../../v2/devtronStackManager/DevtronStackManager.service'
 import { ModuleStatus } from '../../../v2/devtronStackManager/DevtronStackManager.type'
 import { getModuleConfigured } from '../appDetails/appDetails.service'
-import { ReactComponent as NoVulnerability } from '../../../../assets/img/ic-vulnerability-not-found.svg'
 import { CIPipelineBuildType } from '../../../ciPipeline/types'
 import { renderCIListHeader, renderDeploymentHistoryTriggerMetaText } from '../cdDetails/utils'
 import { importComponentFromFELibrary } from '@Components/common'
@@ -646,22 +644,6 @@ const HistoryLogs = ({
         </div>
     )
 }
-export const NoVulnerabilityViewWithTool = ({ scanToolId }: { scanToolId: number }) => {
-    return (
-        <div className="flex h-100 dc__position-rel">
-            <GenericEmptyState
-                SvgImage={NoVulnerability}
-                title={EMPTY_STATE_STATUS.CI_DEATILS_NO_VULNERABILITY_FOUND.TITLE}
-                subTitle={EMPTY_STATE_STATUS.CI_DEATILS_NO_VULNERABILITY_FOUND.SUBTITLE}
-                children={
-                    <span className="flex dc__border-radius-24 bcn-0 pl-16 pr-16 pt-8 pb-8 en-1 bw-1">
-                        <ScannedByToolModal scanToolId={scanToolId} />
-                    </span>
-                }
-            />
-        </div>
-    )
-}
 
 const SecurityTab = ({ ciPipelineId, artifactId, status, appIdFromParent }: SecurityTabType) => {
     const { appId } = useParams<{ appId: string }>()
@@ -699,7 +681,7 @@ const SecurityTab = ({ ciPipelineId, artifactId, status, appIdFromParent }: Secu
     }
 
     if (scanResultLoading) {
-        return <Progressing pageLoader />
+        return <div className='bcn-0 flex-grow-1'><Progressing pageLoader /></div>
     }
     if (scanResultError) {
         return <ErrorScreenManager code={scanResultError.code} reload={reloadScanResult} />
