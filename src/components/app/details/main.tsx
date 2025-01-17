4447156ea4bed4e850f7ac9cec2c1bcfd42db6e8
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-01-17 17:44:31 +0530

    refactor: remove test viewer

--- src/components/app/details/main.tsx
+++ src/components/app/details/main.tsx
@@ -57,7 +57,6 @@ const AppDetails = lazy(() => import('./appDetails/AppDetails'))
 const IndexComponent = lazy(() => import('../../v2/index'))
 
 const CDDetails = lazy(() => import('./cdDetails/CDDetails'))
-const TestRunList = lazy(() => import('./testViewer/TestRunList'))
 
 export default function AppDetailsPage({ isV2 }: AppDetailsProps) {
     const { path } = useRouteMatch()
@@ -426,10 +425,6 @@ export default function AppDetailsPage({ isV2 }: AppDetailsProps) {
                                 filteredEnvIds={_filteredEnvIds}
                             />
                         </Route>
-                        {/* commented for time being */}
-                        {/* <Route path={`${path}/tests/:pipelineId(\\d+)?/:triggerId(\\d+)?`}
-                            render={() => <TestRunList />}
-                        /> */}
                         <Redirect to={`${path}/${URLS.APP_DETAILS}/:envId(\\d+)?`} />
                     </Switch>
                 </Suspense>
