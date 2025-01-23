0c54fee95363e1eeb9cea0d2c6c3699d943a4ecd
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-01-23 16:54:49 +0530

    refactor: remove unused class

--- src/components/notifications/ModifyRecipientsModal.tsx
+++ src/components/notifications/ModifyRecipientsModal.tsx
@@ -232,8 +232,7 @@ export class ModifyRecipientsModal extends Component<ModifyRecipientsModalProps,
                         <span className="form__label mb-16">
                             Recipient will be removed from the respective notification.
                         </span>
-                        {/* TODO: Check form__input--textarea class and remove */}
-                        <div className="form__input form__input--textarea">
+                        <div className="form__input h-132">
                             {this.state.savedRecipients.map((p) => {
                                 return (
                                     <div className="dc__devtron-tag mr-5 mb-5">
