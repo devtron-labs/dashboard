94b8f5a1ae1164591e768323667a2d5d06a2768e
Rohit Raj <rohit.raj@devtron.ai>
2025-01-24 13:43:00 +0530

    feat: colorPalette - add codemirror color tokens

--- src/components/ClusterNodes/NodeDetails.tsx
+++ src/components/ClusterNodes/NodeDetails.tsx
@@ -935,7 +935,7 @@ const NodeDetails = ({ addTab, lowercaseKindToResourceGroupMap, updateTabUrl }: 
                     )}
                     {isReviewState && (
                         <CodeEditor.Header hideDefaultSplitHeader className="node-code-editor-header vertical-divider">
-                            <div className="h-32 lh-32 fs-12 fw-6 flexbox w-100 text-white">
+                            <div className="h-32 lh-32 fs-12 fw-6 flexbox w-100 text__white">
                                 <div className=" pl-10 w-49">Current node YAML </div>
                                 <div className="pl-25 w-51 flexbox">
                                     <Edit className="icon-dim-16 icon-fill-white mt-7 mr-5" />
