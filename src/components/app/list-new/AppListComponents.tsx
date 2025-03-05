d68aee2debb670fdca5daa5a6e0d66cff9dccce4
arunjaindev <arun.jain@devtron.ai>
2025-03-05 16:44:50 +0530

    chore: remove render button function

--- src/components/app/list-new/AppListComponents.tsx
+++ src/components/app/list-new/AppListComponents.tsx
@@ -1,43 +1,31 @@
-import { Button, ButtonVariantType, GenericFilterEmptyState } from '@devtron-labs/devtron-fe-common-lib'
+import { GenericFilterEmptyState } from '@devtron-labs/devtron-fe-common-lib'
 import { ReactComponent as InfoFill } from '@Icons/ic-info-filled.svg'
 import { APP_LIST_EMPTY_STATE_MESSAGING } from './Constants'
 import { AskToClearFiltersProps } from './AppListType'
 
-const AskToClearFilters = ({ clearAllFilters, showTipToSelectCluster = false }: AskToClearFiltersProps) => {
-    const renderButton = () => (
-        <Button
-            dataTestId="clear-filter-button"
-            text="Clear filters"
-            onClick={clearAllFilters}
-            variant={ButtonVariantType.secondary}
-        />
-    )
-
-    return (
-        <GenericFilterEmptyState
-            title={APP_LIST_EMPTY_STATE_MESSAGING.noAppsFound}
-            subTitle={APP_LIST_EMPTY_STATE_MESSAGING.noAppsFoundInfoText}
-            isButtonAvailable
-            renderButton={renderButton}
-        >
-            {showTipToSelectCluster && (
-                <div className="mt-18">
-                    <p
-                        className="bcb-1 cn-9 fs-13 pt-10 pb-10 pl-16 pr-16 eb-2 bw-1 br-4 cluster-tip flex left top"
-                        style={{ width: '300px' }}
-                    >
-                        <span>
-                            <InfoFill className="icon-dim-20" />
-                        </span>
-                        <div className="ml-12 cn-9" style={{ textAlign: 'start' }}>
-                            <span className="fw-6">Tip </span>
-                            <span>{APP_LIST_EMPTY_STATE_MESSAGING.selectCluster}</span>
-                        </div>
-                    </p>
-                </div>
-            )}
-        </GenericFilterEmptyState>
-    )
-}
+const AskToClearFilters = ({ clearAllFilters, showTipToSelectCluster = false }: AskToClearFiltersProps) => (
+    <GenericFilterEmptyState
+        title={APP_LIST_EMPTY_STATE_MESSAGING.noAppsFound}
+        subTitle={APP_LIST_EMPTY_STATE_MESSAGING.noAppsFoundInfoText}
+        handleClearFilters={clearAllFilters}
+    >
+        {showTipToSelectCluster && (
+            <div className="mt-18">
+                <p
+                    className="bcb-1 cn-9 fs-13 pt-10 pb-10 pl-16 pr-16 eb-2 bw-1 br-4 cluster-tip flex left top"
+                    style={{ width: '300px' }}
+                >
+                    <span>
+                        <InfoFill className="icon-dim-20" />
+                    </span>
+                    <div className="ml-12 cn-9" style={{ textAlign: 'start' }}>
+                        <span className="fw-6">Tip </span>
+                        <span>{APP_LIST_EMPTY_STATE_MESSAGING.selectCluster}</span>
+                    </div>
+                </p>
+            </div>
+        )}
+    </GenericFilterEmptyState>
+)
 
 export default AskToClearFilters
