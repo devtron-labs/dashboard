bdb6bf2a619a86e33b7092cda6898f1abb1138b7
arunjaindev <arun.jain@devtron.ai>
2025-03-25 16:04:24 +0530

    feat: update for for date time in local storage

--- src/utils.tsx
+++ src/utils.tsx
@@ -2,13 +2,13 @@ import moment from 'moment'
 import {
     CENTRAL_API_CONNECTIVITY_KEY,
     CentralAPILocalConfig,
-    FALLBACK_REQUEST_TIMEOUT,
+    DATE_TIME_FORMATS,
     getCentralAPIHealthObjectFromLocalStorage,
 } from '@devtron-labs/devtron-fe-common-lib'
 
 const setCentralAPIHealthInLocalStorage = (health: boolean, existingHealthObj: CentralAPILocalConfig) => {
     const updatedObject: CentralAPILocalConfig = {
-        lastUpdatedDate: moment().format('YYYY-MM-DD'),
+        lastUpdatedDate: moment().format(DATE_TIME_FORMATS['DD/MM/YYYY']),
         isConnected: health || existingHealthObj.isConnected,
         updateCount: existingHealthObj.updateCount + 1,
     }
@@ -19,17 +19,17 @@ export const getCentralAPIHealth = async (): Promise<void> => {
     const existingHealthObj = getCentralAPIHealthObjectFromLocalStorage()
     const { lastUpdatedDate, updateCount } = existingHealthObj
 
-    const currDate = moment().format('YYYY-MM-DD')
+    const currDate = moment().format(DATE_TIME_FORMATS['DD/MM/YYYY'])
 
     if (currDate === lastUpdatedDate || updateCount >= 3) {
         return
     }
 
     const controller = new AbortController()
-    const timeoutId = setTimeout(() => controller.abort(), window._env_.GLOBAL_API_TIMEOUT || FALLBACK_REQUEST_TIMEOUT)
+    const timeoutId = setTimeout(() => controller.abort(), window._env_.GLOBAL_API_TIMEOUT)
 
     try {
-        const response = await fetch(window?._env_?.CENTRAL_API_ENDPOINT || 'https://api.devtron.ai', {
+        const response = await fetch(window?._env_?.CENTRAL_API_ENDPOINT, {
             signal: controller.signal,
             method: 'GET',
             headers: {
