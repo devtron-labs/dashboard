b109e430841cbd403f4c06828d38b0e01800bf8f
shivani170 <shivani@devtron.ai>
2025-03-28 15:24:18 +0530

    chore: console remove

--- src/components/v2/values/chartValuesDiff/ChartValuesView.tsx
+++ src/components/v2/values/chartValuesDiff/ChartValuesView.tsx
@@ -1723,7 +1723,6 @@ const ChartValuesView = ({
                                     isVirtualEnvironment={appDetails?.isVirtualEnvironment}
                                 />
                             )}
-                           { console.log('allowedDeploymentTypes', allowedDeploymentTypes) }
                             {!window._env_.HIDE_GITOPS_OR_HELM_OPTION && showDeploymentTools && (
                                 <DeploymentAppSelector
                                     commonState={commonState}
