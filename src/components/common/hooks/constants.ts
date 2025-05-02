4e5834f2723a2d6bbeff6100c84b6bfc022acbe1
AbhishekA1509 <abhishek@devtron.ai>
2025-05-02 21:54:08 +0530

    fix: revert ONLINE_BANNER_TIMEOUT and INTERNET_CONNECTIVITY_INTERVAL to original values

--- src/components/common/hooks/constants.ts
+++ src/components/common/hooks/constants.ts
@@ -32,6 +32,5 @@ export const FILE_READING_FAILED_STATUS = {
     status: FileReaderStatus.FAILED,
 }
 
-// TODO: Revert to 3000 and 30000
-export const ONLINE_BANNER_TIMEOUT = 5000
-export const INTERNET_CONNECTIVITY_INTERVAL = 20000
+export const ONLINE_BANNER_TIMEOUT = 3000
+export const INTERNET_CONNECTIVITY_INTERVAL = 30000
