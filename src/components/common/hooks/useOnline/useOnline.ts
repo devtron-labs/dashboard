3a5a99842e4c6cc157491a39b840dc98c9f567c0
shivani170 <shivani@devtron.ai>
2025-06-12 17:48:06 +0530

    chore: ref removed

--- src/components/common/hooks/useOnline/useOnline.ts
+++ src/components/common/hooks/useOnline/useOnline.ts
@@ -1,16 +1,12 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { noop, useMainContext } from '@devtron-labs/devtron-fe-common-lib'
 
 export const useOnline = ({ onOnline = noop, onOffline = noop }: { onOnline?: () => void; onOffline?: () => void }) => {
     const [online, setOnline] = useState(structuredClone(navigator.onLine))
-    const timeoutRef = useRef<NodeJS.Timeout>(null)
     const { isAirgapped } = useMainContext()
 
     const handleOffline = () => {
-        if (timeoutRef.current) {
-            clearTimeout(timeoutRef.current)
-        }
         setOnline(false)
         if (onOffline) {
             onOffline()
@@ -28,9 +24,6 @@ export const useOnline = ({ onOnline = noop, onOffline = noop }: { onOnline?: ()
         window.addEventListener('offline', handleOffline)
 
         return () => {
-            if (timeoutRef.current) {
-                clearTimeout(timeoutRef.current)
-            }
             window.removeEventListener('offline', handleOffline)
             window.removeEventListener('online', handleOnline)
         }
