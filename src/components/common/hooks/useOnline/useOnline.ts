fc5717e1bcd1ac8d26b75926e67711a0266c7b63
shivani170 <shivani@devtron.ai>
2025-06-12 17:40:50 +0530

    chore: online & cluster selector fix

--- src/components/common/hooks/useOnline/useOnline.ts
+++ src/components/common/hooks/useOnline/useOnline.ts
@@ -2,52 +2,25 @@ import { useEffect, useRef, useState } from 'react'
 
 import { noop, useMainContext } from '@devtron-labs/devtron-fe-common-lib'
 
-import { getInternetConnectivity } from '@Services/service'
-
-import { INTERNET_CONNECTIVITY_INTERVAL } from '../constants'
-
 export const useOnline = ({ onOnline = noop, onOffline = noop }: { onOnline?: () => void; onOffline?: () => void }) => {
     const [online, setOnline] = useState(structuredClone(navigator.onLine))
-    const abortControllerRef = useRef<AbortController>(new AbortController())
     const timeoutRef = useRef<NodeJS.Timeout>(null)
     const { isAirgapped } = useMainContext()
 
-    const checkConnectivity = async () => {
-        if (isAirgapped) return
-        // Cancel any pending request
-        if (abortControllerRef.current) {
-            abortControllerRef.current.abort()
-        }
-
-        const controller = new AbortController()
-        abortControllerRef.current = controller
-
-        try {
-            await getInternetConnectivity(abortControllerRef.current)
-            setOnline(true)
-            if (online) {
-                onOnline()
-            }
-        } catch {
-            setOnline(false)
-        } finally {
-            timeoutRef.current = setTimeout(checkConnectivity, INTERNET_CONNECTIVITY_INTERVAL)
-        }
-    }
     const handleOffline = () => {
         if (timeoutRef.current) {
             clearTimeout(timeoutRef.current)
         }
-        abortControllerRef.current.abort()
         setOnline(false)
         if (onOffline) {
             onOffline()
         }
     }
 
-    const handleOnline = async () => {
-        // Verify connectivity when browser reports online
-        await checkConnectivity()
+    const handleOnline = () => {
+        if (!navigator.onLine) return
+        setOnline(navigator.onLine)
+        onOnline()
     }
 
     useEffect(() => {
@@ -61,7 +34,6 @@ export const useOnline = ({ onOnline = noop, onOffline = noop }: { onOnline?: ()
             }
             window.removeEventListener('offline', handleOffline)
             window.removeEventListener('online', handleOnline)
-            abortControllerRef.current.abort()
         }
     }, [isAirgapped])
 
