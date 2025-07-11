240f8ead4ffc75fdd974dce817abeab1a6a70ad2
AbhishekA1509 <abhishek@devtron.ai>
2025-07-11 13:51:57 +0530

    feat: add footer, trigger api, change branch modal

--- src/components/app/details/triggerView/BuildImageModal/BuildImageModal.tsx
+++ src/components/app/details/triggerView/BuildImageModal/BuildImageModal.tsx
@@ -6,8 +6,8 @@
  * - If !isLoading and filteredCIPipelines does not contain the selected pipeline, we will show error
  */
 
-import { useEffect, useRef, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { SyntheticEvent, useEffect, useRef, useState } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
 
 import {
     APIResponseHandler,
@@ -17,25 +17,46 @@ import {
     Checkbox,
     CIMaterialType,
     CIPipelineNodeType,
+    CommonNodeAttr,
     ComponentSizeType,
+    DocLink,
     Drawer,
     ErrorScreenManagerProps,
+    handleAnalyticsEvent,
     Icon,
     ModuleNameMap,
+    noop,
+    ServerErrors,
+    showError,
     stopPropagation,
+    TOAST_ACCESS_DENIED,
+    ToastManager,
+    ToastVariantType,
+    Tooltip,
     useAsync,
     WorkflowNodeType,
 } from '@devtron-labs/devtron-fe-common-lib'
 
-import { getCIMaterialList } from '@Components/app/service'
+import { getCIMaterialList, triggerCINode } from '@Components/app/service'
+import { CIPipelineBuildType } from '@Components/ciPipeline/types'
 import { EnvironmentList } from '@Components/CIPipelineN/EnvironmentList'
-import { importComponentFromFELibrary } from '@Components/common'
+import { getCIPipelineURL, importComponentFromFELibrary } from '@Components/common'
+import { CI_CONFIGURED_GIT_MATERIAL_ERROR, NO_TASKS_CONFIGURED_ERROR } from '@Config/constantMessaging'
+import { BUILD_STATUS, DEFAULT_GIT_BRANCH_VALUE, SOURCE_NOT_CONFIGURED } from '@Config/constants'
 
 import { getModuleConfigured } from '../../appDetails/appDetails.service'
 import BranchRegexModal from '../BranchRegexModal'
-import { BuildImageModalProps, CIMaterialRouterProps } from '../types'
+import { IGNORE_CACHE_INFO, TRIGGER_VIEW_GA_EVENTS } from '../Constants'
+import { BuildImageModalProps, CIMaterialRouterProps, CIPipelineMaterialDTO, RuntimeParamsErrorState } from '../types'
 
 const getRuntimeParams = importComponentFromFELibrary('getRuntimeParams', null, 'function')
+const AllowedWithWarningTippy = importComponentFromFELibrary('AllowedWithWarningTippy')
+const validateRuntimeParameters = importComponentFromFELibrary(
+    'validateRuntimeParameters',
+    () => ({ isValid: true, cellError: {} }),
+    'function',
+)
+const getRuntimeParamsPayload = importComponentFromFELibrary('getRuntimeParamsPayload', null, 'function')
 
 /**
  * TODO:
@@ -51,21 +72,27 @@ const BuildImageModal = ({
     appId,
     environmentLists,
 }: BuildImageModalProps) => {
+    const { push } = useHistory()
     const { ciNodeId } = useParams<Pick<CIMaterialRouterProps, 'ciNodeId'>>()
     const materialListAbortControllerRef = useRef<AbortController>(new AbortController())
 
     const [showRegexBranchChangeModal, setShowRegexBranchChangeModal] = useState<boolean>(false)
     const [selectedEnv, setSelectedEnv] = useState<BuildImageModalProps['environmentLists'][number] | null>(null)
+    const [invalidateCache, setInvalidateCache] = useState<boolean>(false)
+    const [runtimeParamsErrorState, setRuntimeParamsErrorState] = useState<RuntimeParamsErrorState>({
+        isValid: true,
+        cellError: {},
+    })
+    const [isBuildTriggerLoading, setIsBuildTriggerLoading] = useState<boolean>(false)
 
-    // Workflows will be present since this modal is only opened when workflows are loaded
-    const ciNode = (workflows || [])
-        .flatMap((workflow) => workflow.nodes)
-        .find((node) => node.type === CIPipelineNodeType.CI && node.id === ciNodeId)
     // TODO: Add as much type as possible to selectedCIPipeline
     const selectedCIPipeline = (filteredCIPipelines || []).find((_ci) => _ci.id === +ciNodeId)
-    const workflowId = workflows?.find((workflow) =>
+    const selectedWorkflow = workflows?.find((workflow) =>
         workflow.nodes.some((node) => node.type === WorkflowNodeType.CI && Number(node.id) === +ciNodeId),
-    )?.id
+    )
+    const workflowId = selectedWorkflow?.id
+    // Workflows will be present since this modal is only opened when workflows are loaded
+    const ciNode = selectedWorkflow?.nodes.find((node) => node.type === CIPipelineNodeType.CI && node.id === ciNodeId)
 
     const getMaterialList = async (): Promise<CIMaterialType[]> => {
         const { result: materialList } = await getCIMaterialList(
@@ -102,6 +129,16 @@ const BuildImageModal = ({
         getModuleConfigured(ModuleNameMap.BLOB_STORAGE),
     )
 
+    useEffect(() => {
+        if (isJobView && environmentLists?.length > 0) {
+            const envId = selectedCIPipeline?.environmentId || 0
+            const _selectedEnv = environmentLists.find((env) => +env.id === +envId)
+            setSelectedEnv(_selectedEnv)
+        }
+
+        return () => materialListAbortControllerRef.current.abort()
+    }, [])
+
     // TODO: Maybe extract component for storage module for bulk view as well
     const isBlobStorageConfigured = !!blobStorageModuleRes?.result?.enabled
 
@@ -120,17 +157,11 @@ const BuildImageModal = ({
         setShowRegexBranchChangeModal(false)
     }
 
-    useEffect(() => {
-        if (isJobView && environmentLists?.length > 0) {
-            const envId = selectedCIPipeline?.environmentId || 0
-            const _selectedEnv = environmentLists.find((env) => +env.id === +envId)
-            setSelectedEnv(_selectedEnv)
-        }
+    const toggleInvalidateCache = () => {
+        setInvalidateCache((prev) => !prev)
+    }
 
-        return () => materialListAbortControllerRef.current.abort()
-    }, [])
-
-    const showContentLoader = areRuntimeParamsLoading || isMaterialListLoading
+    const showContentLoader = areRuntimeParamsLoading || isMaterialListLoading || isLoadingBlobStorageModule
 
     const getErrorScreenManagerProps = (): ErrorScreenManagerProps => {
         if (materialListError) {
@@ -150,6 +181,192 @@ const BuildImageModal = ({
         return {}
     }
 
+    const handleRuntimeParamError = (updatedRuntimeParamsErrorState: typeof runtimeParamsErrorState) => {
+        setRuntimeParamsErrorState(updatedRuntimeParamsErrorState)
+    }
+
+    const getCIPipelineURLWrapper = (): string =>
+        getCIPipelineURL(String(appId), String(workflowId), true, ciNodeId, false, ciNode?.isJobCI, false)
+
+    const redirectToCIPipeline = () => {
+        push(getCIPipelineURLWrapper())
+    }
+
+    const onClickTriggerCINode = () => {
+        handleAnalyticsEvent(TRIGGER_VIEW_GA_EVENTS.CITriggered)
+        setIsBuildTriggerLoading(true)
+
+        const dockerfileConfiguredGitMaterialId = selectedWorkflow?.ciConfiguredGitMaterialId
+
+        const gitMaterials = new Map<number, string[]>()
+        const ciPipelineMaterials: CIPipelineMaterialDTO[] = []
+
+        materialList.forEach((material) => {
+            gitMaterials[material.gitMaterialId] = [material.gitMaterialName.toLowerCase(), material.value]
+
+            if (material.value === DEFAULT_GIT_BRANCH_VALUE) {
+                return
+            }
+
+            const history = material.history.filter((historyItem) => historyItem.isSelected)
+            if (!history.length) {
+                history.push(material.history[0])
+            }
+
+            history.forEach((element) => {
+                const historyItem: CIPipelineMaterialDTO = {
+                    Id: material.id,
+                    GitCommit: {
+                        Commit: element.commit,
+                    },
+                }
+                if (!element.commit) {
+                    historyItem.GitCommit.WebhookData = {
+                        id: element.webhookData.id,
+                    }
+                }
+                ciPipelineMaterials.push(historyItem)
+            })
+        })
+
+        if (gitMaterials[dockerfileConfiguredGitMaterialId][1] === DEFAULT_GIT_BRANCH_VALUE) {
+            ToastManager.showToast({
+                variant: ToastVariantType.error,
+                description: CI_CONFIGURED_GIT_MATERIAL_ERROR.replace(
+                    '$GIT_MATERIAL_ID',
+                    `"${gitMaterials[dockerfileConfiguredGitMaterialId][0]}"`,
+                ),
+            })
+            setIsBuildTriggerLoading(false)
+            return
+        }
+
+        const envId = selectedEnv && selectedEnv.id !== 0 ? selectedEnv.id : undefined
+        const runtimeParamsPayload = getRuntimeParamsPayload?.(runtimeParams ?? [])
+
+        const payload = {
+            pipelineId: +ciNodeId,
+            ciPipelineMaterials,
+            invalidateCache,
+            environmentId: envId,
+            pipelineType: ciNode?.isJobCI ? CIPipelineBuildType.CI_JOB : CIPipelineBuildType.CI_BUILD,
+            ...(getRuntimeParamsPayload ? runtimeParamsPayload : {}),
+        }
+
+        triggerCINode(payload)
+            .then((response) => {
+                if (response.result) {
+                    ToastManager.showToast({
+                        variant: ToastVariantType.success,
+                        description: 'Pipeline Triggered',
+                    })
+                    handleClose()
+                }
+            })
+            .catch((errors: ServerErrors) => {
+                if (errors.code === 403) {
+                    ToastManager.showToast({
+                        variant: ToastVariantType.notAuthorized,
+                        description: TOAST_ACCESS_DENIED.SUBTITLE,
+                    })
+                } else if (errors instanceof ServerErrors && Array.isArray(errors.errors) && errors.code === 409) {
+                    errors.errors.map((err) =>
+                        ToastManager.showToast({
+                            variant: ToastVariantType.error,
+                            description: err.internalMessage,
+                        }),
+                    )
+                } else {
+                    errors.errors.forEach((error) => {
+                        if (error.userMessage === NO_TASKS_CONFIGURED_ERROR) {
+                            ToastManager.showToast({
+                                variant: ToastVariantType.error,
+                                title: 'Nothing to execute',
+                                description: error.userMessage,
+                                buttonProps: {
+                                    text: 'Edit Pipeline',
+                                    dataTestId: 'edit-pipeline-btn',
+                                    onClick: redirectToCIPipeline,
+                                },
+                            })
+                        } else {
+                            showError([error])
+                        }
+                    })
+                }
+            })
+            .finally(() => {
+                setIsBuildTriggerLoading(false)
+            })
+    }
+
+    const handleStartBuildAction = (e: SyntheticEvent) => {
+        const runtimeParamsUpdatedErrorState = validateRuntimeParameters(runtimeParams)
+        handleRuntimeParamError(runtimeParamsUpdatedErrorState)
+
+        if (!runtimeParamsUpdatedErrorState.isValid) {
+            ToastManager.showToast({
+                variant: ToastVariantType.error,
+                description: 'Please resolve all the errors before starting the build',
+            })
+            return
+        }
+
+        e.stopPropagation()
+        onClickTriggerCINode()
+    }
+
+    const renderCTAButtonWithIcon = (isCTAActionable: boolean = true) => {
+        const isMaterialActive = materialList.some((material) => material.active)
+        const canTrigger = materialList.reduce(
+            (isValid, mat) =>
+                (isValid && !mat.isMaterialLoading && !!mat.history.find((history) => history.isSelected)) ||
+                (!mat.isDockerFileError && mat.branchErrorMsg === SOURCE_NOT_CONFIGURED && isMaterialActive),
+            true,
+        )
+
+        return (
+            <Button
+                dataTestId="ci-trigger-start-build-button"
+                text={isJobView || ciNode?.isJobCI ? 'Run Job' : 'Start Build'}
+                disabled={!canTrigger}
+                isLoading={isBuildTriggerLoading}
+                onClick={isCTAActionable ? handleStartBuildAction : noop}
+                size={ComponentSizeType.large}
+                startIcon={<Icon name="ic-play-outline" color={null} />}
+            />
+        )
+    }
+
+    const renderCTAButton = () => {
+        const nodeType: CommonNodeAttr['type'] = 'CI'
+
+        if (AllowedWithWarningTippy && ciNode?.pluginBlockState && !isJobView) {
+            return (
+                <AllowedWithWarningTippy
+                    consequence={ciNode.pluginBlockState}
+                    configurePluginURL={getCIPipelineURL(
+                        String(appId),
+                        String(workflowId),
+                        true,
+                        ciNodeId,
+                        false,
+                        ciNode.isJobCI,
+                        false,
+                    )}
+                    showTriggerButton
+                    onTrigger={handleStartBuildAction}
+                    nodeType={nodeType}
+                    isJobView={ciNode.isJobCI}
+                >
+                    {renderCTAButtonWithIcon(false)}
+                </AllowedWithWarningTippy>
+            )
+        }
+
+        return renderCTAButtonWithIcon()
+    }
+
     const renderEnvironments = () => (
         <EnvironmentList
             isBuildStage={false}
@@ -161,10 +378,10 @@ const BuildImageModal = ({
     )
 
     const renderCacheInfo = () => {
-        if (this.props.isFirstTrigger) {
+        if (ciNode?.status?.toLowerCase() === BUILD_STATUS.NOT_TRIGGERED) {
             return (
-                <div className="flexbox">
-                    <Info className="icon-dim-20 mr-8" />
+                <div className="flexbox dc__align-items-center dc__gap-8">
+                    <Icon name="ic-info-filled" color="B500" size={20} />
                     <div>
                         <div className="fw-6 fs-13">{IGNORE_CACHE_INFO.FirstTrigger.title}</div>
                         <div className="fw-4 fs-12">{IGNORE_CACHE_INFO.FirstTrigger.infoText}</div>
@@ -172,10 +389,10 @@ const BuildImageModal = ({
                 </div>
             )
         }
-        if (!this.state.isBlobStorageConfigured) {
+        if (!isBlobStorageConfigured) {
             return (
-                <div className="flexbox flex-align-center">
-                    <Storage className="icon-dim-24 mr-8" />
+                <div className="flexbox dc__align-items-center dc__gap-8">
+                    <Icon name="ic-storage" color={null} size={24} />
                     <div>
                         <div className="fw-6 fs-13">{IGNORE_CACHE_INFO.BlobStorageNotConfigured.title}</div>
                         <div className="fw-4 fs-12 flexbox">
@@ -194,10 +411,10 @@ const BuildImageModal = ({
                 </div>
             )
         }
-        if (!this.props.isCacheAvailable) {
+        if (!ciNode?.storageConfigured) {
             return (
-                <div className="flexbox">
-                    <Info className="icon-dim-20 mr-8" />
+                <div className="flexbox dc__align-items-center dc__gap-8">
+                    <Icon name="ic-info-filled" color="B500" size={20} />
                     <div>
                         <div className="fw-6 fs-13">{IGNORE_CACHE_INFO.CacheNotAvailable.title}</div>
                         <div className="fw-4 fs-12">{IGNORE_CACHE_INFO.CacheNotAvailable.infoText}</div>
@@ -207,23 +424,22 @@ const BuildImageModal = ({
         }
         return (
             <Checkbox
-                isChecked={this.context.invalidateCache}
-                onClick={this.onClickStopPropagation}
-                rootClassName="form__checkbox-label--ignore-cache mb-0 flex top"
+                isChecked={invalidateCache}
+                onClick={stopPropagation}
+                rootClassName="mb-0 flex top"
                 value="CHECKED"
-                onChange={this.context.toggleInvalidateCache}
+                onChange={toggleInvalidateCache}
                 data-testid="set-clone-directory"
             >
-                <div className="mr-5">
+                <div>
                     <div className="fs-13 fw-6 lh-20">{IGNORE_CACHE_INFO.IgnoreCache.title}</div>
 
                     <div className="flex dc__gap-4">
                         <span className="fs-12 fw-4 lh-16">{IGNORE_CACHE_INFO.IgnoreCache.infoText}</span>
 
                         <Tooltip content={IGNORE_CACHE_INFO.IgnoreCache.infoTooltipContent} alwaysShowTippyOnHover>
-                            {/* NOTE: need to wrap react elements with html elements when passing as children to tooltip */}
                             <div>
-                                <ICInfoOutline className="dc__no-shrink icon-dim-16 flex scn-6" />
+                                <Icon name="ic-info-outline" color="N600" size={16} />
                             </div>
                         </Tooltip>
                     </div>
@@ -248,8 +464,6 @@ const BuildImageModal = ({
                         material={materialList}
                         selectedCIPipeline={selectedCIPipeline}
                         title={ciNode?.title}
-                        // FIXME: Later add
-                        isChangeBranchClicked={false}
                         onCloseBranchRegexModal={handleCloseBranchRegexModal}
                         appId={appId}
                         workflowId={workflowId}
@@ -287,9 +501,12 @@ const BuildImageModal = ({
                     <div className="flexbox flex-grow-1 dc__overflow-auto w-100">{renderContent()}</div>
                 </div>
 
-                <div className="flexbox dc__content-end dc__gap-12 py-16 px-20 border__primary--top dc__no-shrink">
-                    {isJobView ? renderEnvironments() : renderCacheInfo()}
-                </div>
+                {ciNode?.isTriggerBlocked ? null : (
+                    <div className="flexbox dc__content-space dc__gap-12 py-16 px-20 border__primary--top dc__no-shrink">
+                        {isJobView ? renderEnvironments() : renderCacheInfo()}
+                        {renderCTAButton()}
+                    </div>
+                )}
             </div>
         </Drawer>
     )
