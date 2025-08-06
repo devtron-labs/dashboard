e523974f7a0524885879fa20deae2f49d3cff86d
Rohit Raj <rohit.raj@devtron.ai>
2025-08-06 12:41:30 +0530

    feat: implement ChartsList component and integrate with chart provider list

--- src/components/charts/list/types.ts
+++ src/components/charts/list/types.ts
@@ -1,3 +1,10 @@
+import { ChartListType } from '../charts.types'
+
 export interface ChartSkeletonRowType {
     isGridView: boolean
+}
+
+export interface ChartsListProps {
+    isLoading: boolean
+    chartsList: ChartListType[]
 }
