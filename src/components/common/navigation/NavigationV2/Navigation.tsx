6aa95dbbc3ef869605f85ef5d78ca13484ba632f
AbhishekA1509 <abhishek@devtron.ai>
2025-08-14 10:59:33 +0530

    feat: refactor navigation structure and integrate CommandBar for enhanced user experience

--- src/components/common/navigation/NavigationV2/Navigation.tsx
+++ src/components/common/navigation/NavigationV2/Navigation.tsx
@@ -19,6 +19,8 @@ import { useLocation } from 'react-router-dom'
 
 import { AnimatePresence, motion, SearchBar, URLS } from '@devtron-labs/devtron-fe-common-lib'
 
+import { CommandBar } from '@Pages/Shared/CommandBar'
+
 import { NAVIGATION_LIST } from './constants'
 import { NavGroup } from './NavGroup'
 import { NavigationLogo, NavigationLogoExpanded } from './NavigationLogo'
@@ -80,70 +82,74 @@ export const NavigationV2 = ({ showStackManager = false }: NavigationProps) => {
         }
 
     return (
-        <div className="navigation dc__position-rel">
-            <nav
-                className={`navigation__default dc__position-rel dc__grid dc__overflow-hidden h-100 ${isExpanded ? 'is-expanded' : ''}`}
-            >
-                <NavigationLogo />
-                <NavGroup title="Search" icon="ic-magnifying-glass" isExpanded={isExpanded} />
-                <NavGroup title="Overview" icon="ic-speedometer" to="/dummy-url" isExpanded={isExpanded} />
-                {NAVIGATION_LIST.map((item) => (
-                    <NavGroup
-                        key={item.id}
-                        title={item.title}
-                        icon={item.icon}
-                        isExpanded={isExpanded}
-                        isSelected={clickedNavGroup?.id === item.id || selectedNavGroup?.id === item.id}
-                        onClick={handleNavGroupClick(item)}
-                    />
-                ))}
-                {showStackManager && (
-                    <NavGroup
-                        title="Stack Manager"
-                        icon="ic-stack"
-                        to={URLS.STACK_MANAGER_ABOUT}
-                        isExpanded={isExpanded}
-                    />
-                )}
-            </nav>
-            <AnimatePresence>
-                {isExpanded && (
-                    <>
-                        <div
-                            className="navigation__expanded__backdrop dc__position-abs dc__top-0"
-                            onClick={closeExpandedNavigation(true)}
+        <>
+            <div className="navigation dc__position-rel">
+                <nav
+                    className={`navigation__default dc__position-rel dc__grid dc__overflow-hidden h-100 ${isExpanded ? 'is-expanded' : ''}`}
+                >
+                    <NavigationLogo />
+                    <NavGroup title="Search" icon="ic-magnifying-glass" isExpanded={isExpanded} />
+                    <NavGroup title="Overview" icon="ic-speedometer" to="/dummy-url" isExpanded={isExpanded} />
+                    {NAVIGATION_LIST.map((item) => (
+                        <NavGroup
+                            key={item.id}
+                            title={item.title}
+                            icon={item.icon}
+                            isExpanded={isExpanded}
+                            isSelected={clickedNavGroup?.id === item.id || selectedNavGroup?.id === item.id}
+                            onClick={handleNavGroupClick(item)}
                         />
-                        <motion.div
-                            initial={{ opacity: 0, x: 0 }}
-                            animate={{ opacity: 1, x: '100%' }}
-                            exit={{ opacity: 0, x: 0 }}
-                            transition={{ duration: 0.3, ease: 'easeOut' }}
-                            className="navigation__expanded h-100 dc__right-radius-8 flexbox-col dc__position-abs dc__top-0 dc__right-0"
-                            onMouseLeave={closeExpandedNavigation(false)}
-                        >
-                            <NavigationLogoExpanded />
-                            {currentNavGroup && (
-                                <div className="flex-grow-1 flexbox-col dc__gap-16 p-16 dc__overflow-auto">
-                                    <p className="m-0 fs-16 lh-1-5 fw-7 text__white font-merriweather">
-                                        {currentNavGroup.title}
-                                    </p>
-                                    <SearchBar
-                                        containerClassName="navigation__expanded__search-bar"
-                                        initialSearchText={searchText}
-                                        handleSearchChange={setSearchText}
-                                        inputProps={{ autoFocus: true }}
-                                    />
-                                    <div className="flex-grow-1 dc__overflow-auto">
-                                        {navItems.map((item) => (
-                                            <NavItem key={item.title} {...item} />
-                                        ))}
+                    ))}
+                    {showStackManager && (
+                        <NavGroup
+                            title="Stack Manager"
+                            icon="ic-stack"
+                            to={URLS.STACK_MANAGER_ABOUT}
+                            isExpanded={isExpanded}
+                        />
+                    )}
+                </nav>
+                <AnimatePresence>
+                    {isExpanded && (
+                        <>
+                            <div
+                                className="navigation__expanded__backdrop dc__position-abs dc__top-0"
+                                onClick={closeExpandedNavigation(true)}
+                            />
+                            <motion.div
+                                initial={{ opacity: 0, x: 0 }}
+                                animate={{ opacity: 1, x: '100%' }}
+                                exit={{ opacity: 0, x: 0 }}
+                                transition={{ duration: 0.3, ease: 'easeOut' }}
+                                className="navigation__expanded h-100 dc__right-radius-8 flexbox-col dc__position-abs dc__top-0 dc__right-0"
+                                onMouseLeave={closeExpandedNavigation(false)}
+                            >
+                                <NavigationLogoExpanded />
+                                {currentNavGroup && (
+                                    <div className="flex-grow-1 flexbox-col dc__gap-16 p-16 dc__overflow-auto">
+                                        <p className="m-0 fs-16 lh-1-5 fw-7 text__white font-merriweather">
+                                            {currentNavGroup.title}
+                                        </p>
+                                        <SearchBar
+                                            containerClassName="navigation__expanded__search-bar"
+                                            initialSearchText={searchText}
+                                            handleSearchChange={setSearchText}
+                                            inputProps={{ autoFocus: true }}
+                                        />
+                                        <div className="flex-grow-1 dc__overflow-auto">
+                                            {navItems.map((item) => (
+                                                <NavItem key={item.title} {...item} />
+                                            ))}
+                                        </div>
                                     </div>
-                                </div>
-                            )}
-                        </motion.div>
-                    </>
-                )}
-            </AnimatePresence>
-        </div>
+                                )}
+                            </motion.div>
+                        </>
+                    )}
+                </AnimatePresence>
+            </div>
+
+            <CommandBar />
+        </>
     )
 }
