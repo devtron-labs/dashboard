6361e529692142b726ec5dac97c30d6928966ee1
chithra <chithra.r@gadgeon.com>
2025-09-25 16:28:40 +0530

    changing the root

--- src/components/app/service.ts
+++ src/components/app/service.ts
@@ -39,11 +39,7 @@ import { BulkResponseStatus, BULK_VIRTUAL_RESPONSE_STATUS } from '../Application
 
 
 
-export const getAppList = (request, options?: APIOptions) => {
-    // Use ATHENA_ROOT instead of ORCHESTRATOR_ROOT for app list API
-    const athenaUrl = `${window.__ATHENA_ROOT__}/${Routes.APP_LIST}`
-    return post(athenaUrl, request, options)
-}
+export const getAppList = (request, options?: APIOptions) => post(Routes.APP_LIST, request, options)
 
 export function deleteResource({ appName, env, name, kind, group, namespace, version, appId, envId }) {
     if (!group) {
