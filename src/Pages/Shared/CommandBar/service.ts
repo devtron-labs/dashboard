eb2c89acbb4f58f98fcd5a1fd838437bd3c76e1d
Arun Jain <arun.jain@devtron.ai>
2025-10-16 16:03:20 +0530

    chore: misc fixes

--- src/Pages/Shared/CommandBar/service.ts
+++ src/Pages/Shared/CommandBar/service.ts
@@ -16,7 +16,7 @@ export const getCommandBarResourceLists = async (): Promise<CommandBarResourceLi
     const appList = promiseResponse[0].status === 'fulfilled' ? promiseResponse[0].value.result : []
     const chartList = promiseResponse[1].status === 'fulfilled' ? promiseResponse[1].value.result : []
     const clusterList = promiseResponse[2].status === 'fulfilled' ? promiseResponse[2].value : []
-    const helmAppList = promiseResponse[3].status === 'fulfilled' ? promiseResponse[3].value.result.helmApps : []
+    const helmAppList = promiseResponse[3].status === 'fulfilled' ? promiseResponse[3].value.result?.helmApps ?? [] : []
 
     return { appList, chartList, clusterList: (clusterList || []).filter((cluster) => !cluster.isVirtual), helmAppList }
 }
