b375a967f82f6b763ef5318db292a19c6f510673
Amrit Borah <elessar1802@gmail.com>
2026-01-14 18:15:33 +0530

    fix: onClick redirect url

--- src/components/app/list-new/GenericAppList.tsx
+++ src/components/app/list-new/GenericAppList.tsx
@@ -286,7 +286,7 @@ const GenericAppList = ({
     }, [isFluxCDAppList])
 
     const onRowClick = ({ data: app }) => {
-        push(buildAppDetailUrl(app))
+        push(buildAppDetailUrl(app.detail))
     }
 
     const filter = useCallback(({ data: app }) => {
