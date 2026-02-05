69dfd8d27116cf010201dec05bb2192b2db09f67
shivani170 <shivani@devtron.ai>
2026-02-05 10:31:19 +0530

    feat: app group table

--- src/components/ApplicationGroup/List/EnvironmentListView.tsx
+++ src/components/ApplicationGroup/List/EnvironmentListView.tsx
@@ -14,33 +14,33 @@
  * limitations under the License.
  */
 
-import { useEffect, useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { useMemo } from 'react'
+import { NavLink, useHistory } from 'react-router-dom'
 
 import {
-    DEFAULT_BASE_PAGE_SIZE,
-    Icon,
-    Pagination,
+    FiltersTypeEnum,
+    PaginationEnum,
+    preventDefault,
+    Table,
+    TableCellComponentProps,
     ToastManager,
     ToastVariantType,
+    URLS,
 } from '@devtron-labs/devtron-fe-common-lib'
 
-import { URLS } from '@Config/routes'
-
 import { useAppContext } from '../../common'
-import { EnvApp, EnvAppList, EnvironmentLinkProps, EnvironmentsListViewType } from '../AppGroup.types'
+import { EnvAppList, EnvironmentsListViewType } from '../AppGroup.types'
 import { EMPTY_LIST_MESSAGING, GROUP_LIST_HEADER, NO_ACCESS_TOAST_MESSAGE } from '../Constants'
-import { EnvEmptyStates } from '../EnvEmptyStates'
-import { LoadingShimmerList } from './LoadingShimmer'
 
-const EnvironmentLink = ({
-    namespace,
-    environmentId,
-    appCount,
-    handleClusterClick,
-    environmentName,
-}: EnvironmentLinkProps) => {
+const EnvironmentNameCellComponent = ({ row, value }: TableCellComponentProps<EnvAppList, FiltersTypeEnum.NONE>) => {
     const { setCurrentEnvironmentName } = useAppContext()
+    const { namespace, id: environmentId, appCount, environment_name: environmentName } = row.data
+
+    const handleClusterClick = (e: any): void => {
+        if (!appCount) {
+            preventDefault(e)
+        }
+    }
 
     const handleOnLinkRedirection = (e: any): void => {
         setCurrentEnvironmentName(environmentName)
@@ -48,14 +48,28 @@ const EnvironmentLink = ({
     }
 
     return (
-        <NavLink
-            data-testid={`${namespace}-click-on-env`}
-            to={`${URLS.APPLICATION_MANAGEMENT_APPLICATION_GROUP}/${environmentId}`}
-            data-noapp={!appCount}
-            onClick={handleOnLinkRedirection}
-        >
-            {environmentName}
-        </NavLink>
+        <div className="py-10">
+            <NavLink
+                data-testid={`${namespace}-click-on-env`}
+                to={`${URLS.APPLICATION_MANAGEMENT_APPLICATION_GROUP}/${environmentId}`}
+                data-noapp={!appCount}
+                onClick={handleOnLinkRedirection}
+                className="cb-5 dc__ellipsis-right dc__no-decor"
+            >
+                {value}
+            </NavLink>
+        </div>
+    )
+}
+
+// Cell component for application count
+const ApplicationCountCellComponent = ({ value }: TableCellComponentProps<EnvAppList, FiltersTypeEnum.NONE>) => {
+    const count = (value as number) || 0
+    return (
+        <div>
+            {count}&nbsp;
+            {count === 0 || count === 1 ? GROUP_LIST_HEADER.APPLICATION : GROUP_LIST_HEADER.APPLICATIONS}
+        </div>
     )
 }
 
@@ -63,45 +77,17 @@ const EnvironmentsListView = ({
     isSuperAdmin,
     filterConfig,
     clearFilters,
-    changePage,
-    changePageSize,
     appListLoading,
     appListResponse,
 }: EnvironmentsListViewType) => {
-    const [filteredEnvList, setFilteredEnvList] = useState<EnvAppList[]>([])
-    const [envCount, setEnvCount] = useState<number>(0)
-    const { cluster } = filterConfig
-    const emptyStateData = cluster.join()
-        ? { title: 'No app groups found', subTitle: "We couldn't find any matching app groups." }
-        : { title: '', subTitle: '' }
+    const history = useHistory()
+    const { setCurrentEnvironmentName } = useAppContext()
 
-    useEffect(() => {
-        const appListResult: EnvApp = appListResponse?.result?.envList?.length
-            ? appListResponse.result
-            : { envCount: 0, envList: [] }
-        setFilteredEnvList(appListResult.envList)
-        setEnvCount(appListResult.envCount)
-    }, [appListResponse?.result])
+    // Handle row click for navigation and toast notifications (also triggered by Enter key press)
+    const handleRowClick = (row: { data: EnvAppList }) => {
+        const { appCount, id: environmentId, environment_name: environmentName } = row.data
 
-    const renderPagination = () => {
-        if (envCount >= DEFAULT_BASE_PAGE_SIZE) {
-            return (
-                <Pagination
-                    rootClassName="flex dc__content-space px-20"
-                    size={envCount}
-                    pageSize={filterConfig.pageSize}
-                    offset={filterConfig.offset}
-                    changePage={changePage}
-                    changePageSize={changePageSize}
-                />
-            )
-        }
-        return null
-    }
-
-    const handleClusterClick = (e: any): void => {
-        if (e.currentTarget.dataset.noapp === 'true') {
-            e.preventDefault()
+        if (!appCount) {
             if (isSuperAdmin) {
                 ToastManager.showToast({
                     variant: ToastVariantType.info,
@@ -114,78 +100,82 @@ const EnvironmentsListView = ({
                     description: NO_ACCESS_TOAST_MESSAGE.NON_ADMIN,
                 })
             }
+        } else {
+            // Navigate to the environment detail page
+            setCurrentEnvironmentName(environmentName)
+            history.push(`${URLS.APPLICATION_MANAGEMENT_APPLICATION_GROUP}/${environmentId}`)
         }
     }
 
-    const renderEmptyView = () => (
-        <div className="dc__align-reload-center">
-            <EnvEmptyStates
-                title={emptyStateData.title}
-                subTitle={emptyStateData.subTitle}
-                actionHandler={clearFilters}
-            />
-        </div>
+    const hasClusterFilter = filterConfig?.cluster?.length > 0
+
+    // Use getRows for backend pagination
+    const getRows = useMemo(
+        () => async () => {
+            const envList = appListResponse?.result?.envList || []
+            const totalRows = appListResponse?.result?.envCount || 0
+
+            return {
+                rows: envList.map((env) => ({
+                    id: String(env.id),
+                    data: env,
+                })),
+                totalRows,
+            }
+        },
+        [appListResponse],
     )
 
-    const renderApplicationCount = (envData) => (
-        <div>
-            {envData.appCount || 0}&nbsp;
-            {envData.appCount === 0 || envData.appCount === 1
-                ? GROUP_LIST_HEADER.APPLICATION
-                : GROUP_LIST_HEADER.APPLICATIONS}
-        </div>
-    )
-    const renderAppGroupListContent = () =>
-        !filteredEnvList.length ? (
-            <LoadingShimmerList />
-        ) : (
-            <div className="dc__overflow-auto">
-                {filteredEnvList?.map((envData) => (
-                    <div
-                        key={envData.id}
-                        className="env-list-row fw-4 cn-9 fs-13 dc__border-bottom-n1 pt-12 pb-12 pr-20 pl-20"
-                        data-testid="env-list-row"
-                    >
-                        <span className="icon-dim-24 bcb-1 flex br-6">
-                            <Icon name="ic-app-group" size={16} color="B400" />
-                        </span>
-                        <div className="cb-5 dc__ellipsis-right">
-                            <EnvironmentLink
-                                namespace={envData.namespace}
-                                environmentId={envData.id}
-                                appCount={envData.appCount}
-                                handleClusterClick={handleClusterClick}
-                                environmentName={envData.environment_name}
-                            />
-                        </div>
-                        <div className="dc__truncate-text" data-testid={`${envData.namespace}-namespace`}>
-                            {envData.namespace}
-                        </div>
-                        <div data-testid={`${envData.cluster_name}-cluster`} className="dc__truncate-text">
-                            {envData.cluster_name}
-                        </div>
-                        {renderApplicationCount(envData)}
-                    </div>
-                ))}
-            </div>
-        )
-
-    return filteredEnvList.length === 0 && !appListLoading ? (
-        renderEmptyView()
-    ) : (
-        <>
-            <div data-testid="app-group-container">
-                <div className="env-list-row fw-6 cn-7 fs-12 py-8 px-20 dc__uppercase dc__position-sticky dc__top-48 bg__primary dc__border-bottom">
-                    <div />
-                    <div>{GROUP_LIST_HEADER.ENVIRONMENT}</div>
-                    <div>{GROUP_LIST_HEADER.NAMESPACE}</div>
-                    <div>{GROUP_LIST_HEADER.CLUSTER}</div>
-                    <div>{GROUP_LIST_HEADER.APPLICATIONS}</div>
-                </div>
-                {renderAppGroupListContent()}
-            </div>
-            {renderPagination()}
-        </>
+    return (
+        <Table<EnvAppList, FiltersTypeEnum.NONE>
+            id="table__application-group-environment-list"
+            columns={[
+                {
+                    field: 'environment_name',
+                    label: GROUP_LIST_HEADER.ENVIRONMENT,
+                    size: { fixed: 250 },
+                    CellComponent: EnvironmentNameCellComponent,
+                },
+                {
+                    field: 'namespace',
+                    label: GROUP_LIST_HEADER.NAMESPACE,
+                    size: null,
+                },
+                {
+                    field: 'cluster_name',
+                    label: GROUP_LIST_HEADER.CLUSTER,
+                    size: null,
+                },
+                {
+                    field: 'appCount',
+                    label: GROUP_LIST_HEADER.APPLICATIONS,
+                    size: { fixed: 150 },
+                    CellComponent: ApplicationCountCellComponent,
+                },
+            ]}
+            getRows={getRows}
+            filtersVariant={FiltersTypeEnum.NONE}
+            paginationVariant={PaginationEnum.NOT_PAGINATED}
+            loading={appListLoading}
+            filter={null}
+            emptyStateConfig={{
+                noRowsConfig: {
+                    title: hasClusterFilter ? 'No app groups found' : EMPTY_LIST_MESSAGING.TITLE,
+                    subTitle: hasClusterFilter ? "We couldn't find any matching app groups." : '',
+                },
+                noRowsForFilterConfig: hasClusterFilter
+                    ? {
+                          clearFilters,
+                      }
+                    : undefined,
+            }}
+            onRowClick={handleRowClick}
+            rowStartIconConfig={{
+                name: 'ic-app-group',
+                color: 'B400',
+                size: 24,
+            }}
+        />
     )
 }
 
