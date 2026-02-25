76725eb6e51e8b82a0875936cc3457dd9a5dd84c
AbhishekA1509 <abhishek@devtron.ai>
2026-02-25 11:50:19 +0530

    refactor: Remove LearnMoreTippy component from PermissionGroupInfoBar and UserPermissionsInfoBar descriptions

--- src/Pages/GlobalConfigurations/Authorization/SSOLoginServices/AutoAssign/common.tsx
+++ src/Pages/GlobalConfigurations/Authorization/SSOLoginServices/AutoAssign/common.tsx
@@ -1,20 +1,11 @@
 import { InfoBlock } from '@devtron-labs/devtron-fe-common-lib'
 
-import LearnMoreTippy from './LearnMoreTippy'
-
-const Tippy = () => (
-    <LearnMoreTippy
-        heading="Auto-assign permissions"
-        infoText="Upon user login, Devtron will check for Groups on SSO service provider associated with the user and assign them to Permission Groups with the same name in Devtron."
-    />
-)
-
 export const PermissionGroupInfoBar = () => (
     <InfoBlock
         description={
             <>
                 Users are auto-assigned a permission group upon SSO login. Ensure matching Permission Groups are created
-                on Devtron and SSO service provider. <Tippy />
+                on Devtron and SSO service provider.
             </>
         }
     />
@@ -25,7 +16,7 @@ export const UserPermissionsInfoBar = () => (
         description={
             <>
                 User permissions are being managed via SSO service provider. Direct permissions cannot be assigned to
-                users. <Tippy />
+                users.
             </>
         }
     />
