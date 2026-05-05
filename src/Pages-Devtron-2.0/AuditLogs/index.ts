71603abe192eb54704c858c6b85734f3c247c3d8
shivani170 <shivani@devtron.ai>
2026-05-05 10:15:42 +0530

    chore: move audit log code

--- src/Pages-Devtron-2.0/AuditLogs/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as AuditLogs } from './AuditLogsRouter'
--- src/components/common/navigation/NavigationRoutes.tsx
+++ src/components/common/navigation/NavigationRoutes.tsx
@@ -70,7 +70,6 @@ import { getUserRole } from '@Pages/GlobalConfigurations/Authorization/authoriza
 import EditClusterDrawerContent from '@Pages/GlobalConfigurations/ClustersAndEnvironments/EditClusterDrawerContent'
 import { ReleaseConfigurations } from '@Pages/Releases/Detail'
 import { ApplicationManagementRouter } from '@PagesDevtron2.0/ApplicationManagement'
-import AuditLogsRouter from '@PagesDevtron2.0/AuditLogs/AuditLogsList'
 import { InfrastructureManagementRouter } from '@PagesDevtron2.0/InfrastructureManagement'
 
 import { SERVER_MODE, ViewType } from '../../../config'
@@ -132,6 +131,8 @@ const CostVisibilityRenderProvider: FunctionComponent<CostVisibilityRenderProvid
 const CostVisibilityRouter = importComponentFromFELibrary('CostVisibilityRouter', null, 'function')
 const AIRecommendations = importComponentFromFELibrary('AIRecommendations', null, 'function')
 const AIChatProvider = importComponentFromFELibrary('AIChatProvider', null, 'function')
+
+const AuditLogsRouter = importComponentFromFELibrary('AuditLogsRouter', null, 'function')
 
 const NavigationRoutes = ({ reloadVersionConfig }: Readonly<NavigationRoutesTypes>) => {
     const navigate = useNavigate()
@@ -563,11 +564,13 @@ const NavigationRoutes = ({ reloadVersionConfig }: Readonly<NavigationRoutesType
                                         path={`${BASE_ROUTES.INFRASTRUCTURE_MANAGEMENT.ROOT}/*`}
                                         element={<InfrastructureManagementRouter isSuperAdmin={isSuperAdmin} />}
                                     />
-                                    <Route
-                                        key={BASE_ROUTES.AUDIT_LOGS.ROOT}
-                                        path={`${BASE_ROUTES.AUDIT_LOGS.ROOT}/*`}
-                                        element={<AuditLogsRouter />}
-                                    />
+                                    {serverMode === SERVER_MODE.FULL && AuditLogsRouter && (
+                                        <Route
+                                            key={BASE_ROUTES.AUDIT_LOGS.ROOT}
+                                            path={`${BASE_ROUTES.AUDIT_LOGS.ROOT}/*`}
+                                            element={<AuditLogsRouter />}
+                                        />
+                                    )}
                                     {!window._env_.K8S_CLIENT
                                         ? [
                                               ...(serverMode === SERVER_MODE.FULL
