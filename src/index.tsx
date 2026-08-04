12cf7965306ad91bfd7f96cd5cefdf4d69ffe64a
shivani170 <shivani@devtron.ai>
2026-08-04 15:42:03 +0530

    chore: error fix for  [0] Failed to fetch

--- src/index.tsx
+++ src/index.tsx
@@ -84,8 +84,10 @@ if (import.meta.env.VITE_NODE_ENV === 'production' && window._env_ && window._en
                     ((error.type &&
                         (error.type === '[401]' ||
                             error.type === '[403]' ||
+                            error.type === '[404]' ||
                             error.type === '[504]' ||
                             error.type === '[503]' ||
+                            error.type === '[0]' ||
                             error.type === 'ChunkLoadError')) ||
                         (error.value &&
                             (error.value.includes('write data discarded, use flow control to avoid losing data') ||
