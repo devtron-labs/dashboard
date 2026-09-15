4969d02c82c3f361323248d54093a1c914ae550d
Arun Jain <arun.jain@devtron.ai>
2026-09-15 18:03:13 +0530

    refactor: simplify error message rendering in VariablesInUseErrorModal

--- src/components/scopedVariables/VariablesInUseErrorModal.tsx
+++ src/components/scopedVariables/VariablesInUseErrorModal.tsx
@@ -32,15 +32,13 @@ const VariablesInUseErrorModal = ({ errors, handleClose }: VariablesInUseErrorMo
             <div className="flexbox-col dc__gap-16 p-20 dc__overflow-auto mxh-400">
                 <InfoBlock variant="error" description={VARIABLE_IN_USE_MODAL_DESCRIPTION} />
                 {errors.map((error, idx) => (
-                    <div className="flexbox-col dc__gap-8">
-                        <p
-                            // eslint-disable-next-line react/no-array-index-key
-                            key={`${idx}-${error.userMessage}`}
-                            className="m-0 fs-13 fw-6 cn-9 lh-1-5 dc__first-letter-capitalize"
-                        >
-                            {error.userMessage}
-                        </p>
-                    </div>
+                    <p
+                        // eslint-disable-next-line react/no-array-index-key
+                        key={`${idx}-${error.userMessage}`}
+                        className="m-0 fs-13 fw-6 cn-9 lh-1-5 dc__first-letter-capitalize"
+                    >
+                        {error.userMessage}
+                    </p>
                 ))}
             </div>
         </GenericModal.Body>
